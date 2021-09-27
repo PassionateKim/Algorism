bc8974faa26371f01715473b013447a351a2759d
codekim97 <kimtaeu4616@gmail.com>
2021-09-27 21:40:45 +0900

    '2021.09.27'

--- BJ1011.c
+++ BJ1011.c
@@ -8,17 +8,21 @@ int main() {
 	for (int i = 0; i < T; i++)
 	{
 		scanf("%lf %lf", &x, &y);
-		printf("y = %lf\n", y);
-		printf("x = %lf\n", x);
 		n = y - x;//45 50 이든 0 5이든 공간 이동 장치 작동 횟수
 				  //차이 없음 
-		printf("n = %lf\n", n);
 		a = (int)sqrt(n);
-		printf("a = %d\n", a);
 
 		if ((int)(n) == a * a)
 		{
 			printf("%d\n", 2 * a - 1);
+		}
+		else if ((int)(n) > a * a && (int)n <= a * a + a)//9<n<=12
+		{
+			printf("%d\n", 2 * a);
+		}
+		else
+		{
+			printf("%d\n", 2 * a + 1);
 		}
 	}
 	return 0;
