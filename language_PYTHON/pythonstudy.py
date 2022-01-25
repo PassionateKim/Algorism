151addc50b892b6de081f0b7b37f7ad4225e1a89
codekim97 <kimtaeu4616@gmail.com>
2022-01-25 17:43:38 +0900

    소인수 분해

--- language_PYTHON/pythonstudy.py
+++ language_PYTHON/pythonstudy.py
@@ -1,4 +1,10 @@
 num = int(input())
 i = 2
-for i in range(i,num):
-    print(i)
+if(num == 1):
+    print('')
+else:
+    for i in range(i,num+1):
+        if(num%i) == 0:
+         while num%i == 0:
+                print(i)
+                num = num/i
