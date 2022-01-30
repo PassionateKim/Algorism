6a9b19819b6931feceaa6fbf7e4c5888b1bd8ec5
codekim97 <kimtaeu4616@gmail.com>
2022-01-30 20:04:41 +0900

    for 문을 최대한 덜 사용하기 위해 저번 문제처럼 배열로 미리 소수를 구해놓고 시작함

--- BJ9020.py
+++ BJ9020.py
@@ -1,4 +1,5 @@
-#베르트랑 공준 
+#골드바흐의 추측
+
 def isPrime(n):
     if(n == 1):
         return False
@@ -10,20 +11,22 @@ def isPrime(n):
              return False
         return True
 
-all_list = list(range(2,246912))
+all_list = list(range(2,10000))
 prime_list = []
 
 for i in all_list:
     if isPrime(i):
         prime_list.append(i)
 
+print(prime_list)
 
-while True:
-    M = int(input())
-    if(M == 0):
-        break
-    N = 0       
-    for i in range(M+1,2*M+1):
-        if(isPrime(i)):
-            N = N+1
-    print(N)
+T = int(input())
+
+for i in range(1,T+1):
+    n = int(input())
+    print("예제입력: "+str(n))
+    
+
+
+
+
