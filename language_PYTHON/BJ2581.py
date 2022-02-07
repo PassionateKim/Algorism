d7b7f9284f0bf1e5227370709b89de90bd59dd1f
codekim97 <kimtaeu4616@gmail.com>
2022-02-07 11:20:59 +0900

    소수

---
+++ language_PYTHON/BJ2581.py
@@ -0,0 +1,35 @@
+#2581소수
+
+#소수 구하기 함수
+def isPrime(n):
+    if(n == 1):
+        return False
+    elif (n == 2):
+        return True
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i == 0:
+             return False
+        return True
+
+all_list = list(range(2,10000))
+prime_list = []
+
+#for 문을 계속 돌리면 시간초과 나므로, 미리 소수를 배열에 넣어두기
+for i in all_list:
+    if isPrime(i):
+        prime_list.append(i)
+
+M = int(input())
+N = int(input())
+
+specific_prime_list = []
+for i in range(M,N+1):
+    if(i in prime_list):
+        specific_prime_list.append(i)
+
+if len(specific_prime_list) == 0:
+    print(-1)
+else:
+    print(sum(specific_prime_list))
+    print(specific_prime_list[0])
--- language_PYTHON/BJ3053.py
+++ language_PYTHON/BJ3053.py
@@ -4,4 +4,3 @@ R = int(input())
 
 print("{:.6f}".format(math.pi*R*R))
 print("{:.6f}".format((2*R)**2//2))
-print("{:.6f}".format((2*R)**2/2))
