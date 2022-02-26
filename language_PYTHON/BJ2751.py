79480d82d52d809a72f59df2a340c5d45fd51c4e
codekim97 <kimtaeu4616@gmail.com>
2022-02-26 17:18:15 +0900

    병합 정렬 주석달기

--- language_PYTHON/BJ2751.py
+++ language_PYTHON/BJ2751.py
@@ -1,18 +1,23 @@
 #수 정렬하기2 
 
 #합병 정렬
-
 def mergemerge(arr):
     if len(arr) <= 1:
+        print("1자리수인 예외 경우 arr",arr)
         return arr
     
     mid = len(arr)//2
     mrg = []
+    print("merge함수 시작",arr)
     print("arr[:mid ",arr[:mid],"arr[mid:]",arr[mid:])
     left, right = mergemerge(arr[:mid]),mergemerge(arr[mid:])
+    
     print("--",left,right)
     i,j = 0,0
+    k =0
     while i < len(left) and j < len(right):
+        print("while문 시작")
+        k += 1
         if left[i] <right[j]:
             print("** left {0} < right {1}이므로 ".format(i,j))
             mrg.append(left[i])
@@ -23,19 +28,21 @@ def mergemerge(arr):
             mrg.append(right[j])
             print("mrg:"+str(mrg[:]))
             j += 1
+    print("while문 횟수,i,j",k,i,j)
     if i != len(left):
-        print("## {0} != "+str(len(left))+"이므로".format(i))
+        print("if 문의 i {0} != len(left) {1}".format(i,len(left)))
+        print(left[i:])
         mrg += left[i:]
         print("mrg:"+str(mrg[:]))
     if j != len(right):
-        print("## {0} != "+str(len(right))+"이므로".format(j))
+        print("if 문의 j {0} != len(left) {1}".format(j,len(right)))
         mrg += right[j:]
         print("mrg:"+str(mrg[:]))
+    
+    print(mrg)
+    print("left,right",left,right)
     print("mrg return")
-    return mrg
-
-
-
+    return mrg 
 
 N = int(input())
 
@@ -44,6 +51,5 @@ for i in range(N):
     arr.append(int(input()))
 
 answer = mergemerge(arr)
-for n in answer:
-    print(n)
+print("answer: ",answer)
 
