cca5f2b9264859e5938ebbdd1d81d1ace3f5d560
codekim97 <kimtaeu4616@gmail.com>
2022-03-05 17:02:55 +0900

    좌표압축과 dic활용

--- language_PYTHON/BJ18870.py
+++ language_PYTHON/BJ18870.py
@@ -4,28 +4,15 @@
 #Xi를 좌표 압축한 결과 X'i의 값은 Xi > Xj를 만족하는 서로 다른 좌표의 개수와 같아야 한다.
 #X1, X2, ..., XN에 좌표 압축을 적용한 결과 X'1, X'2, ..., X'N를 출력해보자.
 import sys
-#1. 정의
-N = int(input())
-xArray = []
-setArray = []
-answerArray = []
-#2. 입력값넣기
-xArray = list(map(int,sys.stdin.readline().rstrip().split()))
+N = int(sys.stdin.readline())
+arr = list(map(int,sys.stdin.readline().split()))
+arr2 = []
 
-#3. 비즈니스로직 -> 비교해서 array에 넣기
-#3-1 setArray를 set으로 중복 삭제하기
-setArray = set(xArray)
-setArray = list(setArray)
+arr2 = list(sorted(set(arr)))
 
-#3-2 비교해서 값에 넣기
-for i in range(len(xArray)):
-    press = 0
-    for j in range(len(setArray)):
-        if(xArray[i] > setArray[j]):
-            press += 1
-    answerArray.append(press)
+print("arr2:",arr2)
 
-for answer in answerArray:
-    print(answer,end=" ")
-    
-
+dic = {arr2[i]: i for i in range(len(arr2))}
+print(dic)
+for i in arr:
+    print(dic[i],end=" ")
