c2851b13a30b88dbf38f3287838e1cad41ccb0b8
codekim97 <kimtaeu4616@gmail.com>
2022-04-07 10:43:20 +0900

    복습완료

--- language_PYTHON/BJ17298.py
+++ language_PYTHON/BJ17298.py
@@ -1,34 +1,16 @@
 #오큰수
+from inspect import stack
+from turtle import st
+
+
 N = int(input())
 arr = list(map(int,input().split()))
-result = [-1] * N
+answer = [-1] * N
 stack = [0]
 
-for i in range(1,N):
+for i in range(N):
     while stack and arr[stack[-1]] < arr[i]:
-        result[stack.pop()] = arr[i]
+        answer[stack.pop()] = arr[i]
+    
     stack.append(i)
-
-print(*result)
-
-
-#기존풀이
-# N = int(input())
-# answer = []
-# su_yeol = list(map(int,input().split()))
-
-# for i in range(len(su_yeol)):
-#     flag = 0
-#     for j in range(len(su_yeol)):
-#         #중복제거
-#         if(i<j):
-#             if(su_yeol[i] < su_yeol[j]):
-#                 answer.append(su_yeol[j])
-#                 flag = 1
-#                 break
-#             elif(j == len(su_yeol) - 1 and flag == 0):
-#                 answer.append(-1)
-# answer.append(-1)
-
-# for i in answer:
-#     print(i,end=" ")
+print(answer)
