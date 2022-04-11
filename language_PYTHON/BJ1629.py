2c14b8191142be2c66b86bd33dd20a2e79d8a72d
codekim97 <kimtaeu4616@gmail.com>
2022-04-11 15:19:02 +0900

    복습완료

--- language_PYTHON/BJ1629.py
+++ language_PYTHON/BJ1629.py
@@ -1,14 +1,14 @@
-#곱셈
-A,B,C = map(int,input().split())
 
-def dac(A,B,C):
-    if B ==1:
+import sys
+
+A, B, C = map(int, sys.stdin.readline().rstrip().split())
+
+def f(A, B, C):
+    if B == 1:
+        print(A,B,C)
         return A % C
-    
-    if B % 2 == 0:
-        print("b%2")
-        return (dac(A,B//2,C)**2)%C
-    else:
-        print("b%2 != 0")
-        return ((dac(A,B//2,C)**2)*A)%C
-print(dac(A,B,C))
+    x = f(A, B//2, C)
+    print(A,B,C)
+    return x**2 % C if B%2 == 0 else (x**2 % C) * (A%C) % C
+
+print(f(A, B, C))
