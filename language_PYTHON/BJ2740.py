45e80f3d77b8d435f0f5269ee34f5e494745e49e
codekim97 <kimtaeu4616@gmail.com>
2022-04-13 19:56:16 +0900

    복습완료

--- language_PYTHON/BJ2740.py
+++ language_PYTHON/BJ2740.py
@@ -1,26 +1,38 @@
 #행렬 곱셈
 import sys
-N,M = map(int,input().split())
-A = []
-B = []
-for i in range(N):
-    A.append(list(map(int,sys.stdin.readline().rstrip().split())))
+# N,M = map(int,input().split())
+# A = []
+# B = []
+# for i in range(N):
+#     A.append(list(map(int,sys.stdin.readline().rstrip().split())))
 
-M,K = map(int,input().split())
+# M,K = map(int,input().split())
+# for i in range(M):
+#     B.append(list(map(int,sys.stdin.readline().rstrip().split())))
+ 
+# C = [[0 for _ in range(K)] for _ in range(N)]
 
-for i in range(M):
-    B.append(list(map(int,sys.stdin.readline().rstrip().split())))
 
-# 행렬 곱셈
+# for n in range(N):
+#     for k in range(K):
+#         for m in range(M):
+#             C[n][k] += A[n][m] * B[m][k]
 
-C = [[0 for _ in range(K)] for _ in range(N)]
+# for i in C:
+#     print(*i)
 
-for n in range(N):
-    for k in range(K):
-        for m in range(M):
-            C[n][k] += A[n][m] * B[m][k]
+c = [0] * 4
+C = []
+for _ in range(5):
+    C.append(c)
+
+C[0][1] = 1
 
 for i in C:
-    for j in i:
-        print(j,end=" ")
-    print()
+    print(i)
+#-------------------------------------------
+C = [[0]*4 for _ in range(5)]
+C[0][1] = 1
+
+for i in C:
+    print(i)
