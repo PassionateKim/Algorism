94a4c1c97616e1198bbef0229112f02c9b2ffb6d
codekim97 <kimtaeu4616@gmail.com>
2022-04-15 19:43:05 +0900

    복습완료

--- language_PYTHON/BJ10830.py
+++ language_PYTHON/BJ10830.py
@@ -1,35 +1,35 @@
 #행렬 제곱
 import sys
 
-def multiplyMatrix(m1, m2):
-    length = len(m1)
-    result = [[0] * length for _ in range(length)]
-    for i in range(length):
-        for j in range(length):
-            for k in range(length):
+
+N,B = map(int,sys.stdin.readline().split())
+
+initial_matrix = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+
+def multifly(m1,m2):
+    result = [[0 for _ in range(N)] for _ in range(N)]
+    
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
                 result[i][j] += m1[i][k] * m2[k][j]
-                # 1000으로 나눠주면서 큰 수 연산을 줄인다.
-                result[i][j] %= 1000
+                result[i][j] = result[i][j] % 1000
     return result
 
-def recursivelyMultiplying(matrix, b):
+def recursive(matrix,b):
     if b == 1:
         return matrix
+    elif b % 2 == 0:
+        matrix = recursive(matrix,b // 2)
+        return multifly(matrix, matrix)
     else:
-        if b % 2 == 0:
-            matrix = recursivelyMultiplying(matrix,b // 2)
-            return multiplyMatrix(matrix, matrix)
-        
-        else:
-            matrix = recursivelyMultiplying(matrix, b-1)
-            return multiplyMatrix(initial_matrix, matrix)
+        matrix = recursive(matrix, b-1)
+        return multifly(matrix, initial_matrix)
 
-n,b = map(int,sys.stdin.readline().split())
-initial_matrix = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
-result_matrix = recursivelyMultiplying(initial_matrix,b)
+result_matrix = recursive(initial_matrix,B)
 
-for i in range(n):
-    for j in range(n):
+for i in range(N):
+    for j in range(N):
         print(result_matrix[i][j] % 1000, end=" ")
     print()
 
@@ -37,28 +37,6 @@ for i in range(n):
 
 
 
-
-
-# graph = []
-# answer = [[0 for _ in range(N)] for _ in range(N)]
-# #행렬에 값넣기
-# for i in range(N):
-#     graph.append(list(map(int,sys.stdin.readline().rstrip().split())))
-
-
-# def double(n,b):
-#     for i in range(n):
-#         for j in range(n):
-#             for k in range(n):
-#                 answer[i][j] += graph[i][k] * graph[k][j]
-
-# double(N,B)
-# for i in answer:
-#     print(i)
-
-
-# import sys
- 
 # def multiplyMatrix(m1, m2):
 #     length = len(m1)
 #     result = [[0] * length for _ in range(length)]
@@ -69,23 +47,34 @@ for i in range(n):
 #                 # 1000으로 나눠주면서 큰 수 연산을 줄인다.
 #                 result[i][j] %= 1000
 #     return result
- 
+
 # def recursivelyMultiplying(matrix, b):
 #     if b == 1:
 #         return matrix
 #     else:
 #         if b % 2 == 0:
-#             matrix = recursivelyMultiplying(matrix, b // 2)
+#             matrix = recursivelyMultiplying(matrix,b // 2)
 #             return multiplyMatrix(matrix, matrix)
- 
+        
 #         else:
 #             matrix = recursivelyMultiplying(matrix, b-1)
 #             return multiplyMatrix(initial_matrix, matrix)
- 
-# n, b = map(int, sys.stdin.readline().split())
-# initial_matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-# result_matrix = recursivelyMultiplying(initial_matrix, b)
+
+# n,b = map(int,sys.stdin.readline().split())
+# initial_matrix = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+# result_matrix = recursivelyMultiplying(initial_matrix,b)
+
 # for i in range(n):
 #     for j in range(n):
-#         print(result_matrix[i][j] % 1000, end = ' ')
+#         print(result_matrix[i][j] % 1000, end=" ")
 #     print()
+
+
+
+
+
+
+
+
+
+
