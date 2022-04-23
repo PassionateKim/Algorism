1880a7901285359dde26339958761db3edd6aca0
codekim97 <kimtaeu4616@gmail.com>
2022-04-23 14:00:11 +0900

    가운데를 말해요 복습완료

--- language_PYTHON/BJ1655.py
+++ language_PYTHON/BJ1655.py
@@ -8,24 +8,27 @@ left_heap = []
 right_heap = []
 
 for i in range(N):
-    s = int(input())
-    
-    #왼쪽힙부터 오른쪽 순차적으로 값을 넣어보기
+    num = int(input())
+
+    #왼쪽부터 넣기
     if len(left_heap) == len(right_heap):
-        heapq.heappush(left_heap,(-s, s))
+        heapq.heappush(left_heap, (-num, num))
     else:
-        heapq.heappush(right_heap,(s, s))
-    #비교해서 넣기
-    if i > 0 and left_heap[0][1] > right_heap[0][1]:
-        #동시에 빼기
+        heapq.heappush(right_heap, (num, num))
+    
+    #조건 비교
+    if right_heap and left_heap[0][1] > right_heap[0][1]:
         left = heapq.heappop(left_heap)[1]
         right = heapq.heappop(right_heap)[1]
 
-        heapq.heappush(left_heap, (-right, right)) 
-        heapq.heappush(left_heap, (-left, left))
+        heapq.heappush(left_heap, (-right, right))
+        heapq.heappush(right_heap, (left, left))
     
     #출력
     print(left_heap[0][1])
+
+
+    
     
 
     
