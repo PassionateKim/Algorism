5e82ecc5f3e32e7e64c485457f24fe478aaceb13
codekim97 <kimtaeu4616@gmail.com>
2022-04-29 13:22:26 +0900

    이분탐색 난이도 있는 문제 복습 완료

--- language_PYTHON/BJ12015.py
+++ language_PYTHON/BJ12015.py
@@ -4,28 +4,50 @@ A = list(map(int,input().split()))
 
 LIS = [0]
 arr = [0] + A
-for item in arr:  
+
+for item in arr:
     if LIS[-1] < item:
         LIS.append(item)
-
     else:
-        left = 0
-        right = len(LIS)
-        while left < right:
-            mid = (left + right) // 2
-            
-            if LIS[mid] < item:
-                left = mid + 1
+        start = 0
+        end = len(LIS)
+        while start <= end:
+            mid = (start + end) // 2
+
+            if LIS[mid] < item: # 작으면 절대 안되니까.
+                start = mid + 1  
             else:
-                right = mid
+                end = mid - 1
+        LIS[start] = item
 
-        LIS[right] = item
+print(LIS)
 
-print(len(LIS) - 1)
+# 가장 긴 증가하는 부분 수열 2
+# N = int(input())
+# A = list(map(int,input().split()))
+
+# LIS = [0]
+# A_array = [0] +  A
+
+# for item in A_array:
+#     if LIS[-1] < item:
+#         LIS.append(item)
+#     else: # LIS 의 원소들을 비교해서 그 안에 값을 넣어주어야한다.
+#         start = 0
+#         end = len(LIS) - 1
+        
+#         while start <= end:
+#             mid = (start + end) // 2
+
+#             if LIS[mid] < item:
+#                 start = mid + 1
+#             else:
+#                 end = mid - 1
+#         LIS[start] = item
+
+# print(LIS)
 
 
-    
 
 
-    
 
--- language_PYTHON/BJ1300.py
+++ language_PYTHON/BJ1300.py
@@ -3,17 +3,20 @@ N = int(input())
 K = int(input())
 
 def Binary(start, end):
-    while start <= end:     
+    while start <= end:
         mid = (start + end) // 2
-        
-        cnt = 0
+        count = 0
         for i in range(1, N+1):
-            cnt += min(N, mid // i)
-                
-        print(start, end, mid, cnt)
-        if cnt >= K: # 줄여야하므로
+            count += min(N, mid // i)
+
+        if count < K:
+            start = mid + 1
+        else:
             end = mid - 1
-        else: # 키워야 하므로
-            start = mid + 1 
-    return start
-print(Binary(1, 10** 9))
+        print(start, end) 
+    
+
+
+
+
+Binary(1, 10**9)
