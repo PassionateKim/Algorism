4abaef01b821bb2c9666b9f0c528404bc5688d2b
codekim97 <kimtaeu4616@gmail.com>
2022-05-03 23:24:05 +0900

    백트레킹으로 풀이

--- language_PYTHON/BJ6603.py
+++ language_PYTHON/BJ6603.py
@@ -1,6 +1,19 @@
 # 로또
 from itertools import combinations
 
+def dfs(start, depth):
+    if depth == 6:
+        for i in range(6):
+            print(combi[i], end=' ')
+        print()
+        return
+    
+    for i in range(start, len(set_S)):
+        combi[depth] = set_S[i]
+        dfs(i + 1, depth + 1)
+
+combi = [0 for i in range(13)]
+
 while True:
     # 입력
     set_S = list(map(int, input().split()))
@@ -9,8 +22,5 @@ while True:
         break
 
     del set_S[0]
-    s = list(combinations(set_S, 6))
-    
-    for i in s:
-        print(*i)
+    dfs(0,0)
     print()
