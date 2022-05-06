62e66c0ea83857b06c4eee149f09697547f2c180
codekim97 <kimtaeu4616@gmail.com>
2022-05-06 23:33:48 +0900

    1day복습완료

--- language_PYTHON/BJ2609.py
+++ language_PYTHON/BJ2609.py
@@ -1,13 +1,34 @@
 # 최대 공약수와 최소 공배수
 
 N, M = map(int, input().split())
-def GCD(x, y):
+
+def GCD(x,y):
     while y:
         x, y = y, x % y
     return x
 
-def LCM(x, y):
-    return (x * y) // GCD(x, y)
+def LCM(x,y):
+    return (x * y) // GCD(x,y)
 
-print(GCD(N,M))    
+
+print(GCD(N,M))
 print(LCM(N,M))
+
+
+
+
+
+
+
+
+# N, M = map(int, input().split())
+# def GCD(x, y):
+#     while y:
+#         x, y = y, x % y
+#     return x
+
+# def LCM(x, y):
+#     return (x * y) // GCD(x, y)
+
+# print(GCD(N,M))    
+# print(LCM(N,M))
