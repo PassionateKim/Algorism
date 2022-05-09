cb541132dfc01224ded149976067c287b0942bf3
codekim97 <kimtaeu4616@gmail.com>
2022-05-09 15:13:45 +0900

    00:42:18, 복습필요O 사유: 백트레킹, dp로 풀기 등

--- language_PYTHON/BJ14501.py
+++ language_PYTHON/BJ14501.py
@@ -1,18 +1,17 @@
 # 퇴사
-N = int(input())
-schedule_list = [list(map(int, input().split())) for _ in range(N)]
-answer_list = []
-# 백트레킹으로 풀어야할 거같은데
-
-def dfs(sum, day):
-    answer_list.append(sum) 
-  
-    
-
-    for i in range(day, N):
-        tmp = i + schedule_list[i][0] # 다음회차에 가능한지 체크
-        if i + schedule_list[i][0] <= N: 
-            dfs(sum + schedule_list[i][1] , i + schedule_list[i][0])
-        
-dfs(0,0)
-print(max(answer_list))
+n = int(input())
+t = []
+p = []
+dp = []
+for i in range(n):
+    a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+    dp.append(b)
+dp.append(0)
+for i in range(n - 1, -1, -1):
+    if t[i] + i > n:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
+print(dp[0])
