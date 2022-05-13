b2c5e3ddf8ac93984d6ce48503a6f76dd05caf7b
codekim97 <kimtaeu4616@gmail.com>
2022-05-14 00:22:42 +0900

    01:09:05, 복습필요O

--- language_PYTHON/BJ2580.py
+++ language_PYTHON/BJ2580.py
@@ -1,9 +1,70 @@
 #스도쿠
 import sys
-sdoku = []
-blank = []
-for i in range(9):
-    sdoku.append(list(map(int,sys.stdin.readline().rstrip().split())))
+sdoku = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
+blank_list = []
+
+# 빈칸 체크
+for x in range(9):
+    for y in range(9):
+        if sdoku[x][y] == 0:
+            blank_list.append((x,y))
+
+# Promising한지? 가로 체크 한번이라도 같으면 안된다.
+def isRowPossible(x,value):
+    for i in range(9):
+        if sdoku[x][i] == value:
+            return False
+    return True
+
+def isColPossible(y,value):
+    for i in range(9):
+        if sdoku[i][y] == value:
+            return False
+    return True
+
+
+
+def isRecPossible(x,y,value):
+    nx = (x // 3) * 3
+    ny = (y // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if sdoku[nx+i][ny+j] == value:
+                return False
+    return True
+
+
+
+def dfs(depth):
+    if depth == len(blank_list):
+        for item in sdoku:
+            print(*item)
+        exit()
+
+    for blank in blank_list:
+        if blank in sdoku:
+            continue
+        
+        x,y = blank[0], blank[1] 
+        
+        for i in range(1, 9+1):
+            if isRowPossible(x,i) and isColPossible(y,i) and isRecPossible(x,y,i):
+                sdoku[x][y] = i
+                dfs(depth+1)
+                sdoku[x][y] = 0
+
+    # for i in range(1, 9+1): # 접근법 복습 필요
+    #     x, y = blank_list[depth][0], blank_list[depth][1]
+    #     if isRowPossible(x,i) and isColPossible(y,i) and isRecPossible(x,y,i):
+    #         sdoku[x][y] = i
+    #         dfs(depth+1)
+    #         sdoku[x][y] = 0 # 초기화 for 다음턴
+
+dfs(0)
+
+
+
+
 
 # # 행 -> 열
 # for row in range(9):
@@ -50,5 +111,5 @@ for i in range(9):
 #             sdoku[x][y] = 0
 
 # dfs(0)
-print(sdoku)
-print(sdoku[1][2])
+# print(sdoku)
+# print(sdoku[1][2])
