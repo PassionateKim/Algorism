bdf7e22c7b203164af6bf97b745955cea6bd8a0e
codekim97 <kimtaeu4616@gmail.com>
2022-05-27 23:20:57 +0900

    01:57:44, 복습필요O 틀린풀이commmit

---
+++ language_PYTHON/KakaoBlind2019(1).py
@@ -0,0 +1,31 @@
+# 오픈채팅방
+from collections import defaultdict, deque
+
+def solution(record):
+    answer = []
+    command_dict = defaultdict(deque) # id: command
+    nickName_dict = dict() # id: nickName
+    # record 순서대로 체크
+    for i in record:
+        command = i.split()[0]
+        if command == "Leave":
+            id = i.split()[1]
+            # nickName_dict[id] = nickName // 이 땐 바뀌지 않는다. 
+        else:
+            id, nickName = i.split()[1], i.split()[2]
+            command_dict[id].append(command)
+            nickName_dict[id] = nickName
+        
+    print(command_dict)
+    print(nickName_dict)
+    for i in record:
+        id = i.split()[1]
+        if command_dict[id][0] == "Enter":
+            answer.append(str(nickName_dict[id]) + "님이 들어왔습니다.")
+        elif command_dict[id][0] == "Leave":
+            answer.append(str(nickName_dict[id]) + "님이 나갔습니다.")        
+        command_dict[id].popleft() 
+    print(answer)
+    return answer
+
+solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
