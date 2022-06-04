580f4924641d4f5258cfec8aeb1073e005458bc1
codekim97 <kimtaeu4616@gmail.com>
2022-06-04 13:39:22 +0900

    00:31:28, 복습필요X

--- language_PYTHON/PG(1).py
+++ language_PYTHON/PG(1).py
@@ -1,44 +1,20 @@
-N = int(input())
+n = 9 
+n //= 3
+print(n)
 
-def solution(n):
-    # 자리수 구하기
-    cnt = 1
-    a = pow(3, cnt)
-    while True:
-        if a >= n:
-            break
-
-        cnt += 1
-        a += pow(3, cnt)
+# def solution(n):
+#     answer = ''
+#     a_list = ["1","2","4"]
     
-    differ = a - n 
-    
-    answer = ''
-    tmp_change = ''
+#     while n > 0:
+#         n = n-1
+#         answer = a_list[n % 3] + answer
+#         n //= 3
 
-    # 각 자리수 정하기
-    # 바뀔 수 있는 최대 자리수 pow(3, cnt-1)
-    while True:
-        if cnt == 0:
-            break
-        
-        if differ >= pow(3, cnt-1): # 25 >= 9
-            change_degree = differ // pow(3, cnt-1) # 2
-            differ -= pow(3, cnt-1) * change_degree
-            tmp_change += str(change_degree)
-            cnt -= 1
-        else:
-            cnt -= 1
-            tmp_change += str(0)
+#     return answer
+# solution(10)
 
-    for i in tmp_change:
-        if i == '0':
-            answer += '4'
-        elif i == '1':
-            answer += '2'
-        else:
-            answer += '1'
-    print(answer)
-    return answer
 
-solution(N)
+# n = n // 3 으로 하면 효율성 TC 틀리는데
+# n //= 3 은 효율성 TC 틀리지 않음...?? 
+# 이유가 무엇인지 
