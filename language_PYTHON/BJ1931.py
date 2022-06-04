c870ec1c3b934ebb0d0c812fed5589cf00c0f0be
codekim97 <kimtaeu4616@gmail.com>
2022-06-04 17:04:36 +0900

    00:20:04, 복습필요X

--- language_PYTHON/BJ1931.py
+++ language_PYTHON/BJ1931.py
@@ -1,47 +1,13 @@
 # #회의실 배정
-# import sys
-# N = int(input())
-# convention = []
-
-# for i in range(N):
-#     convention.append(list(map(int,sys.stdin.readline().rstrip().split())))
-
-# convention.sort(key= lambda x:(x[1],x[0]))
-
-# cnt = 1
-# end_convention = convention[0][1]
-
-# for i in range(1,N):
-#     if convention[i][0] >= end_convention:
-#         cnt += 1
-#         end_convention = convention[i][1]
-
-# print(cnt)
-
-
-
-
-
-
-
-
 import sys
 N = int(input())
-convention=[]
-for i in range(N):
-    convention.append(list(map(int,sys.stdin.readline().rstrip().split())))
-
-convention.sort(key=lambda x:(x[1],x[0]))
-
-cnt = 1 
-values = []
-values.append(convention[0])
-end_convention = convention[0][1]
-for i in range(N):
-    if(convention[i][0] >= end_convention):
+convention = [tuple(map(int, input().split())) for _ in range(N)]
+convention.sort(key = lambda x: (x[1], x[0]))
+cnt = 1
+current = convention[0][1]
+for i in range(1, len(convention)):
+    if convention[i][0] >= current:
+        current = convention[i][1] 
         cnt += 1
-        end_convention = convention[i][1]
-        values.append(convention[i])
 
-print(cnt,values)
-
+print(cnt)
