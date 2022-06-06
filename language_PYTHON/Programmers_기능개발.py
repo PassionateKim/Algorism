21d38fcbc39e3797e9c0a7f1187716ed0befcacc
codekim97 <kimtaeu4616@gmail.com>
2022-06-06 19:34:18 +0900

    00:21:01, 복습필요O

---
+++ language_PYTHON/Programmers_기능개발.py
@@ -0,0 +1,22 @@
+from math import ceil
+
+def solution(progresses, speeds):
+    daysLeft = list(map(lambda x: (ceil((100 - progresses[x]) / speeds[x])), range(len(progresses))))
+    print(daysLeft)
+    count = 1
+    retList = []
+
+    for i in range(len(daysLeft)):
+        try:
+            if daysLeft[i] < daysLeft[i + 1]:
+                retList.append(count)
+                count = 1
+            else:
+                daysLeft[i + 1] = daysLeft[i]
+                count += 1
+        except IndexError:
+            retList.append(count)
+
+    return retList
+
+solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])
