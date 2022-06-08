5df930b7f41d9f8f3e4254d5d2896f9ae437e07b
codekim97 <kimtaeu4616@gmail.com>
2022-06-08 17:31:37 +0900

    복습 횟수:1, 00:24:54, 복습필요O

--- language_PYTHON/Programmers/Programmers_행렬_테두리_회전하기.py
+++ language_PYTHON/Programmers/Programmers_행렬_테두리_회전하기.py
@@ -1,73 +1,41 @@
-
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
+# 행렬 테두리 회전하기
 def solution(rows, columns, queries):
-    answer = []
-
-    board = [[i+(j)*columns for i in range(1,columns+1)] for j in range(rows)]
-    # print(board)
-
-    for a,b,c,d in queries:
+    result = []
+    graph = [[i + j * columns for i in range(1, columns+1)] for j in range(rows)]
+    for i in graph:
+        print(i)
+    for x1, y1, x2, y2 in queries:
         stack = []
-        r1, c1, r2, c2 = a-1, b-1, c-1, d-1
-
-
+        r1, c1, r2, c2 = x1-1, y1-1, x2-1, y2-1
+        # 위 가로
         for i in range(c1, c2+1):
-
-            stack.append(board[r1][i])
-            if len(stack) == 1:
-                continue
+            if i == c1:
+                stack.append(graph[r1][i])
             else:
-                board[r1][i] = stack[-2]
+                stack.append(graph[r1][i])
+                graph[r1][i] = stack[-2]
+        # 오른 세로
+        for i in range(r1+1,r2+1):
+            stack.append(graph[i][c2])
+            graph[i][c2] = stack[-2]
+        # 아래 왼쪽
+        for i in range(c2-1, c1-1, -1):
+            stack.append(graph[r2][i])
+            graph[r2][i] = stack[-2]
+        # 위로
+        for i in range(r2-1, r1-1, -1):
+            stack.append(graph[i][c1])
+            graph[i][c1] = stack[-2]
+        for i in graph:
+            print(i)
+
+        
 
 
-        for j in range(r1+1, r2+1):
-            stack.append(board[j][i])
-            board[j][i] = stack[-2]
 
-        for k in range(c2-1, c1-1, -1):
-            stack.append(board[j][k])
-            board[j][k] = stack[-2]
 
-        for l in range(r2-1, r1-1, -1):
-            stack.append(board[l][k])
-            board[l][k] = stack[-2]
 
-        answer.append(min(stack))
 
-    return answer
+    return result
 
-solution(6,6,[[2,2,5,4],[3,3,6,6],[5,1,6,3]])
+solution(6, 6, [[2,2,5,4]])
