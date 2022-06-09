626953e74ec925474d480330ba5d30eab32e8bd1
codekim97 <kimtaeu4616@gmail.com>
2022-06-09 15:25:31 +0900

    복습 횟수:0, 01:15:55, 복습필요O

---
+++ language_PYTHON/Programmers/Programmers_짝지어_제거하기.py
@@ -0,0 +1,19 @@
+# 짝지어 제거하기
+from collections import deque
+
+def solution(s):
+    answer = -1
+    check_list = []
+    for i in s:
+        print(i)
+
+    return answer
+
+solution("aab")
+
+# for i in s:
+# a = []
+# if a[-1] == i:
+#      a.pop()
+# else:
+#      a.append(i)
