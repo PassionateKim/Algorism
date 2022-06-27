a2486818c4a2cc0463abc2b2873d1b2aa78779f8
codekim97 <kimtaeu4616@gmail.com>
2022-06-27 20:54:20 +0900

    BJ1637, 복습 횟수:1, 00:35:35, 복습필요O

--- language_PYTHON/백준/BJ1637.py
+++ language_PYTHON/백준/BJ1637.py
@@ -1,41 +1,37 @@
 import sys
- 
-def check(x):
-    global total
- 
-    # n줄에 주어진 구간에서 x보다 작거나 같은 수의 개수 구하기 - 누적합
-    total = 0
-    for i in li:
-        # 계산하기 쉽게
-        a, b, c = i[0], i[1], i[2]
-        check = min(b, x)
-        if check < a:
-            total += 0
+si = sys.stdin.readline
+
+N = int(si())
+A = list()
+for i in range(N):
+    A.append(list(map(int, si().split())))
+
+def determination(array, mid):
+
+    sumi = 0 # mid 이하의 수의 개수 
+    for i in array:
+        a, c, b = i[0], i[1], i[2]
+        checki = min(mid, c) # 최대
+        if a > mid: # 최소가 기준값보다 크다면
+            sumi += 0 # 기준값 이하의 수가 아무것도 없다.
         else:
-            total += ((check - a) // c) + 1
-    return total
- 
-n = int(sys.stdin.readline().strip())
-li = []
-for i in range(n):
-    a, c, b = map(int, sys.stdin.readline().strip().split())
-    li.append([a, c, b])
- 
-s = 1
-e = 10
- 
-# 이진 탐색
-ans = -1
-while s <= e:
-    mid = (s + e) // 2
- 
-    if check(mid) % 2:
-        ans = mid
-        e = mid - 1
-    else:
-        s = mid + 1
- 
-if ans == -1:
+            sumi += (checki - a) // b + 1 # 이하의 개수 버림 + 1 
+
+    return sumi # 홀수인지 체크
+
+def Binary(array, start, end):
+    ans = -1
+    while start<=end:
+        mid = (start+end)//2
+        if determination(array, mid) % 2:
+            ans = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    return ans
+
+tmp = Binary(A, 1, 2147483637)
+if tmp == -1:
     print("NOTHING")
 else:
-    print(ans, check(ans) - check(ans - 1))
+    print(tmp, determination(A, tmp) - determination(A, tmp-1))
