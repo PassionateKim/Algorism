0e74cf20651c13145226d60daf17d65ed57287e5
codekim97 <kimtaeu4616@gmail.com>
2022-06-27 17:01:06 +0900

    BJ1300.py, 복습 횟수:2, 00:34:17, 복습필요O

--- language_PYTHON/백준/BJ1300.py
+++ language_PYTHON/백준/BJ1300.py
@@ -5,30 +5,25 @@ si = sys.stdin.readline
 N = int(si())
 k = int(si())
 
-def determination(num, target):
-    # num 보다 작거나 같은것의 개수 ex) num = 4
+def determination(mid, num):
     sumi = 0
-    # 1*1 1*2 1*3
-    # 2*1 2*2 2*3
-    # 3*1 3*2 3*3
-    for i in range(1, N+1):
-        tmp = num // i
+    for i in range(1, N+1): # 1,2...N
+        tmp = mid // i
         if tmp > N:
             tmp = N
-        sumi += tmp  
-    # sumi = 6
-    return sumi >= target
+        sumi += tmp
+    return sumi >= num
 
 def Binary(start, end, target):
     ans = 0
     while start<=end:
-        mid = (start+end)//2
-        if determination(mid, target):
-            ans = mid
+        mid = (start + end) // 2
+        if determination(mid, target): # 크거나 같아서 되는 범위
+            ans = mid 
             end = mid - 1
-        else:
-            start = mid + 1   
+        else: # 작아서 안되는 범위
+            start = mid + 1
+
     return ans
 
-answer = Binary(1, N*N, k)
-print(answer)
+print(Binary(1, N*N, k))
