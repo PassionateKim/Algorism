b61e981f7145dd78776bb1184612a80e1394f635
codekim97 <kimtaeu4616@gmail.com>
2022-06-27 15:46:01 +0900

    BJ13702, 복습 횟수:1, 00:13:47, 복습필요X

--- language_PYTHON/백준/BJ13702.py
+++ language_PYTHON/백준/BJ13702.py
@@ -3,22 +3,20 @@ import sys
 si = sys.stdin.readline
 N, K = map(int, si().split())
 drinks = []
-
 for i in range(N):
     drinks.append(int(si()))
 
-def determination(array, mid, target):
-    res = 0
-    for i in array:
-        res += i//mid
-    # 예를들어 200일 때 target보다 크다면 가능한 경우이다.
-    return res >= target
+def determination(drinks, mg, num):
+    sum = 0
+    for dr in drinks:
+        sum += dr // mg
+
+    return sum >= num
 
 def Binary(array, start, end, target):
     ans = 0
     while start<=end:
-        mid = (start+end)//2
-
+        mid = (start + end) // 2
         if determination(array, mid, target):
             ans = mid
             start = mid + 1
