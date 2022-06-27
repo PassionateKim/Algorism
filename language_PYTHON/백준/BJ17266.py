23e6d33404dd89c1dee7fd4bde0aa4fc9bd63c52
codekim97 <kimtaeu4616@gmail.com>
2022-06-27 16:00:43 +0900

    BJ17266.py, 복습 횟수:2, 00:12:30, 복습 필요X

--- language_PYTHON/백준/BJ17266.py
+++ language_PYTHON/백준/BJ17266.py
@@ -6,33 +6,26 @@ N = int(si())
 M = int(si())
 location = list(map(int, si().split())) # 오름차순이라 sorted 안해도됨
 
-def determination(distance, height):
+def determination(garosu, height):
     end_point = 0
-
-    for i in range(len(location)):
-        # 왼쪽
-        if location[i] - height <= end_point:
-            end_point = location[i] + height
+    for ga in garosu:
+        if ga - height <= end_point: # 좌
+            end_point = ga + height # 우
         else:
             return False
+    return end_point >= N
 
-    # distance를 모두 덮는 경우
-    if end_point >= distance:
-        return True    
-    else:
-        return False
-
-def Binary(start, end):
-    ans = N
+def Binary(start, end, garosu):
+    ans = 0
     while start<=end:
-        mid = (start+end)//2
-
-        if determination(N, mid):
+        mid = (start + end) // 2
+        if determination(garosu, mid):
             ans = mid
             end = mid - 1
         else:
             start = mid + 1
+
+
     return ans
 
-answer = Binary(1, int(1e8))
-print(answer)
+print(Binary(1, N, location))
