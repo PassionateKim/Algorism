7e2148b66f5b2d7b29682809dfdc54d75e489b0b
codekim97 <kimtaeu4616@gmail.com>
2022-07-10 11:17:48 +0900

    BJ12015, 복습 횟수:1, 01:30:18, 복습필요O

---
+++ language_PYTHON/백준/BJ12015.py
@@ -0,0 +1,25 @@
+# 가장 긴 증가하는 부분 수열 2
+
+N = int(input())
+A = list(map(int,input().split()))
+
+LIS = [0]
+A_array = [0] +  A
+
+for item in A_array:
+    if LIS[-1] < item:
+        LIS.append(item)
+    else: # LIS 의 원소들을 비교해서 그 안에 값을 넣어주어야한다.
+        start = 0
+        end = len(LIS) - 1
+        
+        while start <= end:
+            mid = (start + end) // 2
+
+            if LIS[mid] < item:
+                start = mid + 1
+            else:
+                end = mid - 1
+        LIS[start] = item
+
+print(len(LIS) - 1)
