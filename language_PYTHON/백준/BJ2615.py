279d6057fbf27065ead31d1ea87211021fc63e09
codekim97 <kimtaeu4616@gmail.com>
2022-07-13 20:51:09 +0900

    BJ2615, 복습 횟수:1, 00:23:06, 복습필요O

--- language_PYTHON/백준/BJ2615.py
+++ language_PYTHON/백준/BJ2615.py
@@ -1,4 +1,5 @@
 # 오목
+from calendar import c
 import sys
 si = sys.stdin.readline
 
@@ -10,9 +11,9 @@ dir = [(0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1)]
 answer = 0
 omok = []
 
-def checkWhite(x, y):
+def checkOmok(x, y):
     global answer
-    
+    color = graph[x][y]
     for i in range(8):
         cnt = 1
         omok.append((x, y))
@@ -20,9 +21,11 @@ def checkWhite(x, y):
         while True:
             nx += dir[i][0]
             ny += dir[i][1]
-            if not (0<=nx<19 and 0<=ny<19): break
+            if not (0<=nx<19 and 0<=ny<19): 
+                omok.clear()
+                break
 
-            if graph[nx][ny] == 1: # 흰 오목이라면
+            if graph[nx][ny] == color: # 흰 오목이라면
                 omok.append((nx, ny))
                 cnt += 1
             else:
@@ -33,56 +36,18 @@ def checkWhite(x, y):
             if cnt == 5:
                 fnx, fny = nx + dir[i][0], ny + dir[i][1] 
                 bnx, bny = x - dir[i][0], y - dir[i][1] 
-
-                if (0<= fnx <19 and 0<= fny <19) and graph[fnx][fny] != 1:
-                    if ((0<=bnx<19 and 0<=bny<19) and graph[bnx][bny] != 1) or not (0<=bnx<19 and 0<=bny<19):
-                        answer = 1 # 1로 지정
-                        return  
-                elif not (0<= fnx <19 and 0<= fny < 19): # 앞에 인덱스 아웃인 경우
-                    if ((0<=bnx<19 and 0<=bny<19) and graph[bnx][bny] != 1) or not (0<=bnx<19 and 0<=bny<19):
-                        answer = 1 # 1로 지정
-                        return
-                else:
-                    omok.clear()
+                if (0<=fnx<19 and 0<=fny<19 and graph[fnx][fny] == color):
                     break
-                
-    return
-
-def checkBlack(x, y):
-    global answer
-    
-    for i in range(8):
-        cnt = 1
-        omok.append((x, y))
-        nx, ny = x, y
-        while True:
-            nx += dir[i][0]
-            ny += dir[i][1]
-            if not (0<=nx<19 and 0<=ny<19): break
-
-            if graph[nx][ny] == 2: # 검은 오목이라면
-                omok.append((nx, ny))
-                cnt += 1
-            else:
-                omok.clear()
-                break
-
-            # 탈출 조건 딱 5개라면
-            if cnt == 5:
-                fnx, fny = nx + dir[i][0], ny + dir[i][1] 
-                bnx, bny = x - dir[i][0], y - dir[i][1] 
-
-                if (0<= fnx <19 and 0<= fny <19) and graph[fnx][fny] != 2:
-                    if ((0<=bnx<19 and 0<=bny<19) and graph[bnx][bny] != 2) or not (0<=bnx<19 and 0<=bny<19):
-                        answer = 2 # 2로 지정
-                        return  
-                elif not (0<= fnx <19 and 0<= fny < 19): # 앞에 인덱스 아웃인 경우
-                    if ((0<=bnx<19 and 0<=bny<19) and graph[bnx][bny] != 2) or not (0<=bnx<19 and 0<=bny<19):
-                        answer = 2 # 2로 지정
-                        return
-                else:
-                    omok.clear()
+                if (0<=bnx<19 and 0<=bny<19 and graph[bnx][bny] == color):
                     break
+                answer = color
+                print(answer)
+                omok.sort(key=lambda x: (x[1], x[0]))
+                print(omok[0][0]+1, omok[0][1]+1)
+                sys.exit(0)          
+                # else:
+                #     omok.clear()
+                #     break
                 
     return
 
@@ -91,19 +56,9 @@ def check():
     global answer
     for i in range(19):
         for j in range(19):
+            if graph[i][j] != 0: 
+                checkOmok(i, j)
             
-            if graph[i][j] == 1: # 흰색
-                checkWhite(i, j)
-            elif graph[i][j] == 2: # 검은색
-                checkBlack(i, j)
-            
-            if answer != 0:
-                return
     return
-
 check()
-print(answer)
-omok.sort(key=lambda x: (x[1], x[0]))
-# 만약 승부가 결정된 경우
-if answer != 0:
-    print(omok[0][0] + 1, omok[0][1] + 1)
+print(0)
