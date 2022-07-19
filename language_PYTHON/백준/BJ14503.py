c5c4b38bb9c7bb0560db2f2486043d67a594d46f
codekim97 <kimtaeu4616@gmail.com>
2022-07-19 23:38:20 +0900

    BJ14503, 복습 횟수:4, 01:30:00, 복습필요O

--- language_PYTHON/백준/BJ14503.py
+++ language_PYTHON/백준/BJ14503.py
@@ -1,71 +1,62 @@
 #로봇 청소기
 import sys
 si = sys.stdin.readline
-N, M = map(int, si().split())
-r, c, d = map(int, si().split())
+
 # 북, 동, 남, 서
 dir = [(-1,0), (0,1), (1,0), (0,-1)]
-robot = [r-1,c-1,d]
-graph = []
-cnt = 0
-answer = 0
-for i in range(N):
-    graph.append(list(map(int, si().split())))
-visited = [[0 for _ in range(N)] for _ in range(N)]
+N, M = map(int, si().split())
+r, c, d = map(int, si().split())
+visited = [[0 for _ in range(M)] for _ in range(N)]
+graph = [list(map(int, si().split())) for _ in range(N)]
 
-def turnLeft(d):
-    # 방향
-    if d == 0: # 북 -> 서
-        tmp = 3
-    elif d == 1: # 동 -> 북
-        tmp = 0
-    elif d == 2: # 남 -> 동
-        tmp = 1
-    else:        # 서 -> 남
-        tmp = 2
-
-    return tmp
-def checkBack(robot):
-    if robot[2] == 0:
-        return robot[0] + 1, robot[1]
-    elif robot[2] == 1:
-        return robot[0], robot[1] - 1
-    elif robot[2] == 2:
-        return robot[0] - 1, robot[1]
+def checkLeft():
+    if d == 0:
+        return 3
+    elif d == 1:
+        return 0
+    elif d == 2:
+        return 1
     else:
-        return robot[0], robot[1] + 1
+        return 2
 
+def checkBehind():
+    if d == 0:
+        return r+1, c
+    elif d == 1:
+        return r, c-1
+    elif d == 2:
+        return r-1, c
+    else:
+        return r, c+1
+
+
+answer = 0
 while True:
-    # 현재 위치를 청소한다.
-    answer += 1
-    visited[robot[0]][robot[1]] = 1 # 방문처리
+    
+    # 1. 현재 위치를 청소한다.
+    answer += 1 # 추가
+    visited[r][c] = 1 # 청소처리
+    # 2. 현재 위치에서 현재 방향을 기준으로 왼쪽 방향부터 차례로 탐색
     cnt = 0
     while True:
+        print(r, c)
+        leftDir = checkLeft()
+        nx,  ny = r + dir[leftDir][0], c + dir[leftDir][1] # 왼쪽
+        if visited[nx][ny] == 0 and graph[nx][ny] == 0: # 청소가능하다면
+            r, c, d = nx, ny, leftDir
+            break # 1번부터 진행한다.
+        else:
+            cnt += 1 # 회전 개수 세주기
+            d = leftDir # 회전만
+        
         if cnt == 4:
-            rx, ry = checkBack(robot)
-            if graph[rx][ry] == 1:
+            x, y = checkBehind()
+            if graph[x][y] == 1: # 벽인 경우
                 print(answer)
-                
                 exit()
             else:
-                robot[0] = rx
-                robot[1] = ry
+                r, c = x, y # 한칸 후진
                 cnt = 0
-                continue
-
-        x, y = robot[0], robot[1]
-        # 왼쪽
-        d = turnLeft(robot[2])
-        cnt += 1
-        nx = x + dir[d][0]
-        ny = y + dir[d][1]
-        # 아직 청소하지 않은 공간이 존재한다면
-        if visited[nx][ny] == 0 and graph[nx][ny] == 0:
-            robot[0] = nx
-            robot[1] = ny
-            robot[2] = d
-            # 1번으로 돌아간다.
-            break
-        else:
-            robot[2] = d
-
+                continue # 2번으로 돌아간다.
+        
+        
