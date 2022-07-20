97f466328ca614555fd2fd93b67eefa1469a2b49
codekim97 <kimtaeu4616@gmail.com>
2022-07-20 09:54:22 +0900

    BJ20056, 복습횟수:1, 01:00:00, 복습필요O

--- language_PYTHON/백준/BJ20056.py
+++ language_PYTHON/백준/BJ20056.py
@@ -1,70 +1,71 @@
 # 마법사 상어와 파이어볼
 import sys
-import copy
 si = sys.stdin.readline
-
+#크기 # 개수 # 명령 수
 N, M, K = map(int, si().split())
-# 그래프 생성
-graph = [[[] for _ in range(N)] for _ in range(N)]
+graph = [[[] for _ in range(N)] for _ in range(N)] # 원소로 배열을 담을 것이다. 
 dir = [(-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1)]
-# 처음 파이어볼 넣기 초기화
+
+# 파이어볼 위치 처음 초기화
 for i in range(M):
     r, c, m, s, d = map(int, si().split())
     graph[r-1][c-1].append([m, s, d])
 
-# 시작
+
 for i in range(K):
+    # 1. 파이어볼 위치 체크 반드시 계속 초기화 해주어야함
     tmp_list = [[[] for _ in range(N)] for _ in range(N)]
-    # 1. 그래프 돌기
     for i in range(N):
         for j in range(N):
-            # 하나 이상이면 탐색하면서, 이동시키기
-            if len(graph[i][j]) >= 1:
-                while graph[i][j]: 
-                    m, s, d = graph[i][j].pop()
-                    nx, ny = (i + dir[d][0]*s) % N, (j + dir[d][1]*s) % N # 열, 행이 연결되어 있으므로
-                    # graph에 하면 영향을 줘서 안된다.
-                    tmp_list[nx][ny].append([m, s, d]) # 위치 이동
+            if len(graph[i][j]) >= 1: # 하나이상 있는지 확인하기
+                # 원소를 완전탐색하고, 이동시키기
+                for fireball in graph[i][j]:
+                    m, s, d = fireball[0], fireball[1], fireball[2]
+                    nx, ny = (i + dir[d][0]*s)%N, (j + dir[d][1]*s)%N
+                    tmp_list[nx][ny].append([m, s, d]) 
     
-    #2. 이동 후 2개 이상의 파이어볼이 있는지 체크
+    # for i in tmp_list:
+    #     print(i)
+    #2. 이동이 끝 난뒤~ 
     for i in range(N):
         for j in range(N):
-            amount = 0
-            speed = 0
-            num = len(tmp_list[i][j])
-            a_odd, a_even = True, True
-            if len(tmp_list[i][j]) >= 2:
-                for p in tmp_list[i][j]:
-                    m, s, d = p
-                    # 홀짝 체크
-                    if d%2 == 1:
-                        a_even = False
-                    else:
-                        a_odd = False
-                    # 질량 합, 속력 합
-                    amount += m
+            # 2개 이상에서는 다음과 같은 일이 일어난다.
+            if len(tmp_list[i][j])>= 2:  
+                isEven, isOdd = True, True
+                amount, speed, count = 0, 0, len(tmp_list[i][j])
+
+                while tmp_list[i][j]: # 원소에 있는거 완전탐색
+                    m, s, d = tmp_list[i][j].pop()
+                    # 질량, 속력 더하기
+                    amount += m 
                     speed += s
+                    # 홀짝 구분
+                    if d%2 == 0: # 짝
+                        isOdd = False
+                    else: # 홀
+                        isEven = False
+                # 질량이 0인 파이어볼은 소멸되어 없어진다.
+                if amount//5 == 0: continue
                 
-                tmp_list[i][j] = [] # 초기화
-                
-                if amount // 5 == 0: continue # 질량이 0인 파이어볼은 소멸된다.
-                
-                # 방향
-                if a_odd or a_even:
-                    for q in [0, 2, 4, 6]:
-                        tmp_list[i][j].append([amount//5, speed//num, q])
+                if isOdd or isEven: #모두 홀수이거나 모두 짝수이면
+                    for ddd in [0, 2, 4, 6]:
+                        tmp_list[i][j].append([amount//5, speed//count, ddd])
                 else:
-                    for q in [1, 3, 5, 7]:
-                        tmp_list[i][j].append([amount//5, speed//num, q])
+                    for ddd in [1, 3, 5, 7]:
+                        tmp_list[i][j].append([amount//5, speed//count, ddd])
+
     
-    # 그래프 초기화
     for i in range(N):
         for j in range(N):
-            graph[i][j] = copy.deepcopy(tmp_list[i][j])
-    
-answer = 0                
-for hang in graph: # 3 
-    for i in hang: # 2 
-        for j in i: # 1
-            answer += j[0]
-print(answer)
+            graph[i][j] = tmp_list[i][j]
+answer = 0
+for i in graph:
+    for j in i:
+        for k in j:
+            answer += k[0]
+print(answer)                            
+                    
+                    
+
+
+                    
