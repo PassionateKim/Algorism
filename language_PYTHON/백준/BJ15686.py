20fafdbe355c0fae9f8d96c8443eecd0569a0fff
codekim97 <kimtaeu4616@gmail.com>
2022-07-27 21:51:59 +0900

    BJ15686, 복습 횟수:2, 00:30:00, 복습필요X

--- language_PYTHON/백준/BJ15686.py
+++ language_PYTHON/백준/BJ15686.py
@@ -1,46 +1,48 @@
+# 2022-05-13
+# 2022-06-05
+# 2022-07-27
 # 치킨 배달
+from re import L
 import sys
 si = sys.stdin.readline
-
 N, M = map(int, si().split())
-graph = []
+graph = [list(map(int, si().split())) for _ in range(N)]
+
 chicken_list = []
 house_list = []
-for i in range(N):
-    graph.append(list(map(int, si().split())))
 
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 1: # 집이면
-            house_list.append((i,j))
-        elif graph[i][j] == 2: # 치킨집이면
-            chicken_list.append((i,j))
+        if graph[i][j] == 1:
+            house_list.append([i,j])
+        elif graph[i][j] == 2:
+            chicken_list.append([i,j])
+
 answer = 1e10
-def dfs(depth, l : list, idx):
-    # global answer
-    # # 탈출 조건
-    # if len(l) == M: 
-    #     sumi_dis = 0 # 도시의 치킨 거리
-    #     for house in house_list:
-    #         dis = 1e9
-    #         h_x, h_y = house[0], house[1]
-    #         for chicken in l: # 치킨거리
-    #             c_x, c_y = chicken[0], chicken[1]
-    #             differ = abs(h_x - c_x) + abs(h_y - c_y)
-    #             dis = min(dis, differ)
-    #         sumi_dis += dis
-    #     # 최소 비교
-    #     answer = min(answer, sumi_dis)
-    #     return
-    if len(l) == M:
-        print(l)
+def dfs(idx, arr : list):
+    global answer
+    if len(arr) == M:
+        city_d = 0
+        # 집 in 집_list:
+        for house in house_list:
+            hx, hy = house[0], house[1]
+            chicken_d = 1e10
+            for chicken in arr:
+                cx, cy = chicken[0], chicken[1]
+                tmp = abs(hx- cx) + abs(hy - cy)
+                
+                # 치킨 거리 구하기
+                chicken_d = min(chicken_d, tmp)
+            # 치킨 거리 더하기
+            city_d += chicken_d
+        # / 집 list
+        answer = min(city_d, answer)
         return
 
     for i in range(idx, len(chicken_list)):
-        chicken = chicken_list[i]
-        l.append(chicken)
-        dfs(depth+1, l, idx+1)
-        l.pop()
+        arr.append(chicken_list[i])
+        dfs(i+1, arr)
+        arr.pop()
 
-dfs(0, [], 0)
-
+dfs(0, [])
+print(answer)
