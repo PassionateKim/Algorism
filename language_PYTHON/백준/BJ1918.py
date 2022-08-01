3581654d77aa3a3bbedc284d352f8dbcf356601e
codekim97 <kimtaeu4616@gmail.com>
2022-08-01 19:44:38 +0900

    BJ1918, 복습 횟수:3, 00:30:00, 복습필요O

--- language_PYTHON/백준/BJ1918.py
+++ language_PYTHON/백준/BJ1918.py
@@ -1,34 +1,37 @@
 # 2022-07-25
 # 2022-07-26
 # 2022-07-28
+# 2022-08-01
 # 후위 표기식
 import sys
 si = sys.stdin.readline
-chars = list(map(str, si().strip()))
+
 op_stack = []
 answer = ""
-for ch in chars:
-    if ch.isalpha():
-        answer += ch
-    elif ch == '(':
-        op_stack.append(ch)
-    elif ch == '+' or ch == '-':
+
+input = si().strip()
+
+for s in input:
+    if s == '(':
+        op_stack.append(s)
+    elif s.isalpha():
+        answer += s
+    elif s == '+' or  s == '-':
         while op_stack and op_stack[-1] != '(':
             answer += op_stack.pop()
         
-        op_stack.append(ch)
-    elif ch == '*' or ch == '/':
+        op_stack.append(s)
+    elif s == '*' or s == '/':
         while op_stack and op_stack[-1] in ['*', '/']:
             answer += op_stack.pop()
         
-        op_stack.append(ch)
+        op_stack.append(s)
     else:
         while op_stack and op_stack[-1] != '(':
             answer += op_stack.pop()
         
         op_stack.pop()
-        
+
 while op_stack:
     answer += op_stack.pop()
-
 print(answer)
