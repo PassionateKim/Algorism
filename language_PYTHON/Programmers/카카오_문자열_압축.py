47911be8a7a2cd3639ae9c630ef16c4390581a9e
codekim97 <kimtaeu4616@gmail.com>
2022-08-01 17:39:55 +0900

    복습 횟수:3, 00:30:00, 복습필요O

--- language_PYTHON/Programmers/카카오_문자열_압축.py
+++ language_PYTHON/Programmers/카카오_문자열_압축.py
@@ -3,38 +3,11 @@
 # 2022-07-30
 # 문자열 압축
 def solution(s):
-    result = 1000
-    # 1. 문자열 자르는 개수
-    for i in range(1, len(s) + 1):
-        tmp = s[0:i]
-        cnt = 1
-        answer = ""
-        # 2. 문자열 탐색
-        for j in range(i, len(s), i):
-            string = s[j:j+i]
-            
-            # 다른 경우
-            if tmp != string:
-                if cnt > 1:
-                    answer += str(cnt)
-                answer += tmp
+    answer = 0
 
-                # 초기화
-                tmp = string
-                cnt = 1
-            # 같은 경우 
-            else:
-                cnt += 1
+    for i in range(1, len(s)+1):
+        print(i)
 
-
-        # 3. 나머지 체크
-        if cnt > 1:
-            answer += str(cnt)
-        answer += tmp
-        result = min(result, len(answer))
-    # /1.
-    
-    
-    return result
-
+        
+    return answer
 solution("ababab")
