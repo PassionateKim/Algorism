01474b92b445186fd5c4b1907ff6a8aa127786b2
codekim97 <kimtaeu4616@gmail.com>
2022-08-02 09:57:05 +0900

    복습 횟수:2, 00:30:00, 복습필요O

--- language_PYTHON/Programmers/124_나라의_숫자.py
+++ language_PYTHON/Programmers/124_나라의_숫자.py
@@ -1,16 +1,18 @@
 # 2022-06-xx
 # 2022-07-31
+# 2022-08-02
 def solution(n):
-    answer = ''
-    a_list = ["4","1","2"]
-    
+    answer = ""
+    a_list = ['4','1','2']
     while n > 0:
-        answer = a_list[n%3] + answer
-        if(n%3 == 0):
-            n = (n//3) - 1
-        else:
-            n = (n//3)
-    
+
+        idx = n % 3
+        answer = a_list[idx] + answer
+        # if n % 3 == 0:
+        #     n = n - 1
+        
+        n = n//3
+
     return answer
 
-print(solution(9))
+print(solution(6))
