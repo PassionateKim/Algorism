e490e174664640ccc9477f9c96c5a2162f3c3803
codekim97 <kimtaeu4616@gmail.com>
2022-08-03 13:08:39 +0900

    복습 횟수:1, 00:45:00, 복습필요O

--- language_PYTHON/Programmers/카카오_메뉴_리뉴얼.py
+++ language_PYTHON/Programmers/카카오_메뉴_리뉴얼.py
@@ -1,25 +1,43 @@
+# 2022-06-08
+# 2022-08-03
 # 메뉴 리뉴얼
+from collections import defaultdict
 from itertools import combinations
-from collections import Counter
 def solution(orders, course):
     answer = []
-    for c in course:
-        order_combi = []
-        for order in orders:
-            for combi in combinations(order, c):
-                order_combi.append(''.join(sorted(combi)))
-        # ['AB', 'AC', 'AD', 'AE', 'BC', 'BD', 'BE', 'CD']
-        order_count = Counter(order_combi).most_common()
-        maxi = -1
-        for oc in order_count:
-            maxi = max(maxi, oc[1])
-        for oc in order_count:
-            if oc[1] == maxi and maxi != 1:
-                answer.append(oc[0])
+    # course
+    for cnt in course:
+        candidate_dict = defaultdict(int)
+        for od in orders:
+            # list로 바꾸기
+            food_list = list(map(str, od))
+            combi = list(combinations(food_list, cnt))
+            for c in combi: # ("A", "B")
+                
+                candi = "".join(sorted(c))
+                
+                if candi in candidate_dict.keys():
+                    candidate_dict[candi] += 1
+                else:
+                    candidate_dict[candi] = 1
+
+        
+            # /od
+        # /cnt
+        print(candidate_dict.items())
+        if candidate_dict:
+            maxi = max(candidate_dict.values())
+            for key, value in candidate_dict.items():
+                if value == maxi and maxi != 1:
+                    answer.append(key)
+
     answer.sort()
     print(answer)
     return answer
-solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4])
+
+solution(["XYZ", "XWY", "WXA"],[2,3,4])
+# solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5])
+# solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4])
 
 # set.add
 # set, dict 는 in 체크할때 해쉬를 사용해 O(1)
