8d0302d317defb68790412e31f1456eb55a3733a
codekim97 <kimtaeu4616@gmail.com>
2022-08-05 10:05:54 +0900

    복습 횟수:1, 01:00:00, 복습필요O

--- language_PYTHON/Programmers/카카오_수식_최대화.py
+++ language_PYTHON/Programmers/카카오_수식_최대화.py
@@ -1,25 +1,45 @@
 # 2022-08-03
+# 2022-08-05
 # 카카오 수식 최대화
 from itertools import permutations
-
-
+from collections import deque
+import re
 def solution(expression):
-    p = permutations(["-","*","+"], 3)
-    result = 0 
-    for pattern in p:
-        new = []
-        first = expression.split(pattern[0])
-        for section in first:
-            second = section.split(pattern[1])
-            second = [str(eval(x)) for x in second]
-            new.append(pattern[1].join(second))
-        print(new)
-        new = [str(eval(x)) for x in new]
-        print(new)
-        exit()
-        answer = abs(eval(pattern[0].join(new)))
-        result = max(result, answer)
+    answer = 0
+    # 1. 쪼개기
+    expression = re.split(r'(\+|\-|\*)', expression)
+    expression = deque(expression)
+    # 2. 우선순위
+    priority_list = list(permutations(["*","-","+"], 3))
+    
+    tmp_expression = deque()
+    tmp_expression.extend(expression)
+    
+    for priority in priority_list:
+        tmp_expression = deque()
+        tmp_expression.extend(expression)
+    
+        for op in priority: # * -> - -> + 
+            tmp = []
+            while tmp_expression:
+                s = tmp_expression.popleft()
+                if s == op:
+                    left_val = tmp.pop()
+                    right_val = tmp_expression.popleft()
+                    val = eval(left_val + op + right_val)
+                    tmp.append(str(val))
+                else:
+                    tmp.append(s)
+            #/op
+            tmp_expression.extend(tmp)
+        #/priority
+        print(abs(int(tmp_expression[0])))
+        answer = max(answer, abs(int(tmp_expression[0])))
+    # print(answer)
 
-    return result
+
+            
+
+    return answer
 
 solution("100-200*300-500+20")
