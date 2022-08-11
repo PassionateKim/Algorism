2a6f446463c787c746e462601fb09080032020bc
codekim97 <kimtaeu4616@gmail.com>
2022-08-11 13:52:29 +0900

    복습 횟수:2, 00:30:00, 복습필요O

--- language_PYTHON/Programmers/카카오_수식_최대화.py
+++ language_PYTHON/Programmers/카카오_수식_최대화.py
@@ -1,45 +1,39 @@
 # 2022-08-03
 # 2022-08-05
+# 2022-08-11
 # 카카오 수식 최대화
-from itertools import permutations
 from collections import deque
+from itertools import permutations
 import re
 def solution(expression):
     answer = 0
-    # 1. 쪼개기
+    # 1. 나누기
     expression = re.split(r'(\+|\-|\*)', expression)
     expression = deque(expression)
-    # 2. 우선순위
-    priority_list = list(permutations(["*","-","+"], 3))
-    
     tmp_expression = deque()
-    tmp_expression.extend(expression)
-    
-    for priority in priority_list:
-        tmp_expression = deque()
+    # 2.우선순위 결정해 계산하기
+    for p in permutations(["*","-","+"], 3):
         tmp_expression.extend(expression)
-    
-        for op in priority: # * -> - -> + 
-            tmp = []
+        for op in p: # *
+            print("op", op)
+            new_expression = deque()
             while tmp_expression:
-                s = tmp_expression.popleft()
-                if s == op:
-                    left_val = tmp.pop()
-                    right_val = tmp_expression.popleft()
-                    val = eval(left_val + op + right_val)
-                    tmp.append(str(val))
+                tmp = tmp_expression.popleft()
+                if tmp != op:
+                    new_expression.append(tmp)
                 else:
-                    tmp.append(s)
-            #/op
-            tmp_expression.extend(tmp)
-        #/priority
-        print(abs(int(tmp_expression[0])))
-        answer = max(answer, abs(int(tmp_expression[0])))
-    # print(answer)
+                    left = new_expression.pop()
+                    right = tmp_expression.popleft()
 
-
-            
-
+                    print("left: ",left,"right: ",right)
+                
+                    new_expression.append(str(eval(left+op+right)))
+            # 초기화
+            tmp_expression.extend(new_expression) 
+        #/ 우선순위 계산 끝
+        answer = max(answer, abs(int(tmp_expression.pop())))
+        print(answer)
+    
     return answer
 
 solution("100-200*300-500+20")
