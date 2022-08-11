9f4141dd77e9317d78b0d333aaa5116c3747734d
codekim97 <kimtaeu4616@gmail.com>
2022-08-11 13:18:55 +0900

    복습 횟수:2, 00:15:00, 복습필요X

--- language_PYTHON/Programmers/Programmers_전화번호_목록.py
+++ language_PYTHON/Programmers/Programmers_전화번호_목록.py
@@ -1,19 +1,21 @@
 # 2022-08-08
+# 2022-08-09
 # 전화번호 목록
 def solution(phone_book):
-    answer = True
     phone_book_dict = dict()
+
     for i in phone_book:
         phone_book_dict[i] = 1
 
-    for key in phone_book_dict: # <= 1,000,000
+    for string in phone_book:
         tmp = ''
-        for s in key: # <= 20
-            tmp += s
-            if tmp in phone_book_dict and tmp != key:
+        for i in string:
+            tmp += i
+            if tmp in phone_book_dict and tmp != string:
                 return False
+    return True 
 
-    return answer
+
 print(solution(["119", "97674223", "1195524421"]))
 # 생각바꾸기 119를 기준으로 1195524421 비교 -> 1195524421 -> 119 비교
 # <= 20 인 부분을 활용한 점도 기억하기
