1ae234e2d9876321b54b01fcd5cd5835523e6091
codekim97 <kimtaeu4616@gmail.com>
2022-08-12 14:57:33 +0900

    복습 횟수:5, 00:15:00, 복습필요X

--- language_PYTHON/Programmers/카카오_문자열_압축.py
+++ language_PYTHON/Programmers/카카오_문자열_압축.py
@@ -3,35 +3,32 @@
 # 2022-07-30
 # 2022-08-01
 # 2022-08-05
+# 2022-08-12
 # 문자열 압축
 def solution(s):
     answer = 1000
-
     for i in range(1, len(s) + 1):
-        i = len(s)
+        # cut의 범위
+        buffer = ""
         tmp = s[:i]
-        result = 0
         cnt = 1
-        tmp_str = ""
+        # 일단 string 자체로는 전범위를 탐색 가능하다.
         for j in range(i, len(s), i):
             string = s[j:j+i]
-
-            if string != tmp:
+            if tmp == string:
+                cnt += 1
+            else:
                 if cnt > 1:
-                    tmp_str += str(cnt)
-                tmp_str += tmp
-
+                    buffer += str(cnt)
+                buffer += tmp
                 # 초기화
                 tmp = string
-                cnt = 1
-            else:
-                cnt += 1
-        
-        # 끝
+                cnt = 1 
         if cnt > 1:
-            tmp_str += str(cnt)
-        tmp_str += tmp
-        answer = min(answer, len(tmp_str))
+            buffer += str(cnt)
+        buffer += tmp
+        answer = min(answer, len(buffer))
 
+    print(answer)    
     return answer
 solution("aabbaccc")
