0cf0e363c6421182e859fd3b8d3749a39bcf18aa
codekim97 <kimtaeu4616@gmail.com>
2022-08-14 15:13:33 +0900

    복습 횟수:1, 00:15:00, 복습필요O

--- language_PYTHON/Programmers/Programmers_가장_큰_수.py
+++ language_PYTHON/Programmers/Programmers_가장_큰_수.py
@@ -1,20 +1,13 @@
 # 2022-08-13
+# 2022-08-14
 # 가장 큰 수
-from itertools import permutations
-
 def solution(numbers):
-    answer = ''
-    num_list = []
-    # 1. num을 str로 저장
-    for num in numbers:
-        num_list.append(str(num))
-
-    num_list.sort(key=lambda x : x*3, reverse=True)
+    answer = ""
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3 , reverse = True)
     
-    answer = str(int("".join(num_list)))
-    a = ["30","3"]
-    a.sort()
-    print(a)
+    answer = "".join(numbers)
+
     return answer
 
-print(solution([221, 2]))
+print(solution([0, 0]))
