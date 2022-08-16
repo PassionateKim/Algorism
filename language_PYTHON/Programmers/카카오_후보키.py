ed65e2579a03a4ce86d00457314395447658939c
codekim97 <kimtaeu4616@gmail.com>
2022-08-16 10:11:23 +0900

    복습 횟수:1, 00:45:00, 복습필요O

--- language_PYTHON/Programmers/카카오_후보키.py
+++ language_PYTHON/Programmers/카카오_후보키.py
@@ -1,85 +1,56 @@
 # 2022-08-15
+# 2022-08-16
 # 후보키
 from itertools import combinations
 
+
 def solution(relation):
-    answer = 0
-    answer_dict = dict()
     
-    attr_length = len(relation[0])
-    tmp_list = [i for i in range(attr_length)]
+    n = len(relation)
+    m = len(relation[0])
+    combi = []
+    tmp_key_len = [i for i in range(m)]
+    
+    # 1. 가능한 모든 후보키
+    for i in range(1, m+1):
+        combi += combinations(tmp_key_len, i)
+    
+    unique_key = set()
 
-    # 완전 탐색 시작 1 ~ N
-    for i in range(1, attr_length + 1):
-        attr_cnt = i # 선택 attr 개수
-
-        index_combi = list(combinations(tmp_list, 2)) # 속성 index combination
-        
-        
-        for index in index_combi:
-            tmp = []
-            for item in relation:
-                a = list()
-                for i in index:
-                    a.append(item[i])
-                tmp.append(a)
-            print(tmp)
-            exit()
-
+    # 2. 키에 따라 값 모두 체크하기
+    for keys in combi:
+        tmp_set = []
+        # [0,1] 이라고 해보자.
+        tmp = [tuple([item[key] for key in keys]) for item in relation]
+        print(tmp)
+        exit()
         for item in relation:
-            for index in index_combi:
-                print("index:",index)
-                a = list()
-                for i in index:
-                    a.append(item[i])
-                tmp.append(a)
-            print(tmp)
-            exit(0)
+            tmp = []
+            # 키 모두 체크
+            for key in keys:
+                tmp.append(item[key])
 
+            tmp_set.append(tuple(tmp))
+        # 유일성
+        if len(set(tmp_set)) == n:
+            unique = True  
+            # 최소성
+            for x in unique_key:
+                if set(x).issubset(set(keys)):
+                    unique = False
+                    break   
 
-        # 각 인덱스들 마다 비교
-        for index in index_combi:
-            print("index:",index)
-            
-        exit()        
+            if unique:
+                unique_key.add(keys)
 
-            
-
-
-#     return answer
+    return len(unique_key)
 
 solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]])
 
 # 틀린 이유
 # tuple을 어떻게 구현할지 몰랐음 (안에서 list comprehension을 쓸 줄 몰랐음.)
 # issubset을 몰랐음
+# set 안에서 tuple은 hashable , list는 hashable X
 
 
 
-
-
-# def solution(relation):
-#     row = len(relation)
-#     col = len(relation[0])
-
-#     #가능한 속성의 모든 인덱스 조합 
-#     combi = []
-#     for i in range(1, col+1):
-#         combi.extend(combinations(range(col), i))
-        
-#     #유일성
-#     unique = []
-#     for i in combi:
-#         tmp = [tuple([item[key] for key in i]) for item in relation]
-
-#         if len(set(tmp)) == row:    # 유일성
-#             put = True
-            
-#             for x in unique:
-#                 if set(x).issubset(set(i)):  # 최소성
-#                     put = False
-#                     break
-                    
-#             if put: unique.append(i)
-   
-#     return len(unique)
