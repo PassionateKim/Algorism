d992c923e591d9eeb39c6bf9401f562843cd55c7
codekim97 <kimtaeu4616@gmail.com>
2022-08-17 11:08:44 +0900

    복습 횟수:1, 00:45:00, 복습필요O

--- language_PYTHON/Programmers/카카오_순위검색.py
+++ language_PYTHON/Programmers/카카오_순위검색.py
@@ -1,38 +1,50 @@
 # 2022-08-16
+# 2022-08-17
 # 순위검색
+from bisect import bisect_left
 from collections import defaultdict
 from itertools import combinations
-from bisect import bisect_left
 def solution(information, queries):
     answer = []
-    dic = defaultdict(list)
+    answer_dict = defaultdict(list)
+    # 1. 데이터 넣기
     for info in information:
         info = info.split()
-        condition = info[:-1]  
+
+        # 모두 dict에 저장해버리기
+        # key_list와 score 구하기
+        key_list = info[:-1]
         score = int(info[-1])
+        
+        # 그 외
         for i in range(5):
-            case = list(combinations([0,1,2,3], i))
-            for c in case:
-                tmp = condition.copy()
-                for idx in c:
-                    tmp[idx] = "-"
-                key = ''.join(tmp)
-                dic[key].append(score) 
+            combi = list(combinations([0,1,2,3], i))
 
-    for value in dic.values():
-        value.sort()   
+            # combi 단위로 -로 바꿔서 dict에 담기
+            for com in combi: #[(0,1,2), (1,2,3)]
+                tmp = key_list[:]
+                for j in com: #(0,1,2)
+                    tmp[j] = '-'
+                answer_dict["".join(tmp)].append(score) 
     
+    # 2. 데이터 정렬 for 이분탐색
+    for value in answer_dict.values():
+        value.sort()
+    
+    # 3. 이분탐색으로 체크
     for query in queries:
-        query = query.replace("and","")
+        query = query.replace("and",'')
         query = query.split()
-        target_key = ''.join(query[:-1])
-        target_num = int(query[-1])
-        count = 0
-        if target_key in dic:
-            idx = bisect_left(dic[target_key], target_num)
-            count = len(dic[target_key]) - idx
-        answer.append(count)
+        condition = query[:-1]
+        score = int(query[-1])
+        key = "".join(condition)
 
+        # 키가 있는 경우
+        if key in answer_dict.keys():
+            val = answer_dict[key] # list
+            count = len(val) - bisect_left(val, score)
+        answer.append(count)
+    print(answer)
     return answer
 
 solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],
