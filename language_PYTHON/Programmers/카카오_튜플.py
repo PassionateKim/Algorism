f88fd7671c444e7c7a746f447b6d03db68de6600
codekim97 <kimtaeu4616@gmail.com>
2022-08-19 13:33:17 +0900

    복습 횟수:3, 00:15:00, 복습필요X

--- language_PYTHON/Programmers/카카오_튜플.py
+++ language_PYTHON/Programmers/카카오_튜플.py
@@ -1,30 +1,22 @@
 # 2022-08-05
 # 2022-08-06
 # 2022-08-12
+# 2022-08-19
 # 튜플
 import re
 def solution(s):
     answer = []
-    r = re.findall('{[\d,]+}', s)
-    print(r)
-    s = re.split(r'({[\d,]+})', s[1:-1])
-    # # ", 제거"
-    # for i in s:
-    #     if i == ',':
-    #         s.remove(i)
-
-    # # 길이 순 정렬
-    # s.sort(key = lambda x: len(x))
-    # tmp = list()
-
-    # for i in s:
-    #     tmp.append(i.split(","))
-    # # # 키로 저장하기
-    # for i in tmp:
-    #     for j in i:
-    #         if int(j) not in answer:
-    #             answer.append(int(j))
     
-    # return answer
+    s = re.findall('{[\d,]+}', s)
+    print(s)
+    for i in s:
+        i = i[1:-1].split(",")
+        print("i:",i)
+        return
+        for ii in i:
+            if int(ii) not in answer:
+                answer.append(int(ii))
+    
+    return answer
 
-solution("{{20,111},{111}}")
+solution("{{4,2,3},{3},{2,3,4,1},{2,3}}")
