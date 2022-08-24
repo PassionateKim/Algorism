fbd905386a15bd79a464c1ae9bb8b8fed584e83d
codekim97 <kimtaeu4616@gmail.com>
2022-08-24 10:32:04 +0900

    복습 횟수:1, 01:00:00, 복습필요O

--- language_PYTHON/Programmers/카카오_프렌즈4블록.py
+++ language_PYTHON/Programmers/카카오_프렌즈4블록.py
@@ -1,77 +1,63 @@
 # 2022-08-17
+# 2022-08-24
 # 카카오 프렌즈4블록
-# 우 대각 하
 dir = [(0,1), (1,1), (1,0)]
 def solution(m, n, board):
     answer = 0
-    n, m = m, n # 편의를 위해 기존 graph처럼 바꿈
-    
     graph = []
-    # 문자열2차원 배열로 치환
-    for s in board:
-        graph.append(list(s))
-    
-    
-    while True:
-        tuple_set = set()
-        # 1. 체크하기
-        for x in range(n):
-            for y in range(m):
-                tmp = graph[x][y]
-                if tmp == '0': continue
 
+    # 2차원 그래프로 변경
+    for i in board:
+        graph.append(list(i))
+
+    # 전체 탐색
+    while True:
+        location_set = set()
+        # 1. 정사각형 탐색
+        for x in range(m):
+            for y in range(n):
+                if graph[x][y] == '0' :continue
+                check = graph[x][y]
                 cnt = 1
                 for idx in range(3):
                     nx, ny = x + dir[idx][0], y + dir[idx][1]
-                    # 범위 벗어나면 X
-                    if not (0<=nx<n and 0<=ny<m): continue
-                    if tmp != graph[nx][ny]: continue
-                    
-                    cnt += 1
+                    if not (0<=nx<m and 0<=ny<n): continue
 
-                # 만약 모두 같은 경우
+                    if graph[nx][ny] == check:
+                        cnt += 1
+                # 만약 cnt 4라면 정사각형을 만족하므로
                 if cnt == 4:
-                    tuple_set.add((x,y))
+                    location_set.add((x,y))
                     for idx in range(3):
                         nx, ny = x + dir[idx][0], y + dir[idx][1]
-                        tuple_set.add((nx,ny))
-        # ==탈출 조건==
-        if len(tuple_set) == 0:
+                        location_set.add((nx, ny))
+        # / 1. 정사각형 탐색
+
+        # ==탈출조건==
+        if not location_set:
             break
-
-        # ==ㅌ로직==
-        # /1. 체크하기
-        tuple_set = sorted(list(tuple_set)) # 디버깅 편의를 위해 
-
-
-        # 초기화       
-        while tuple_set:
-            x, y = tuple_set.pop()
+        # 2. 초기화
+        while location_set:
             answer += 1
-            graph[x][y] = "0"
-        
-        # 2. 그래프 아래로 초기화
-        for x in range(n-1,-1,-1):
-            for y in range(m):
+            x, y = location_set.pop()
+            graph[x][y] = '0'
+        # / 2. 초기화
+        # 3. 아래로 내리기 - 아래부터 탐색
+        for x in range(m-1,-1,-1):
+            for y in range(n):
                 if graph[x][y] == '0': continue
-                
-                tx, ty = x, y
-                # 블록인 경우
+                kx, ky = x, y
                 while True:
-                    nx, ny = tx + dir[2][0], ty + dir[2][1]
-
-                    if not (0<=nx<n and 0<=ny<m): break
+                    nx, ny = kx + 1, y
+                    
+                    if not (0<=nx<m and 0<=ny<n): break
                     if graph[nx][ny] != '0': break
-                    # 뚫려있는 경우
-                    if graph[nx][ny] == '0':
-                        graph[nx][ny], graph[tx][ty] = graph[tx][ty], graph[nx][ny]
-                        tx, ty = nx, ny  # 초기화
-         
-        #/2. 그래프 아래로 초기화
-                     
-        
 
-    print(answer)
+                    graph[kx][ky], graph[nx][ny] = graph[nx][ny], graph[kx][ky]
+                    
+                    kx, ky = nx, ny
+        # /3. 아래로 내리기
+    print(answer)       
     return answer
 
 solution(6,6,["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"])
