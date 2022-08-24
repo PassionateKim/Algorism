9023f9bb68fc2b8fd0c001a68150dfae3e8e901b
codekim97 <kimtaeu4616@gmail.com>
2022-08-24 16:44:16 +0900

    BJ1644.py, 복습 횟수:3: 00:30:00, 복습필요O

--- language_PYTHON/백준/BJ1644.py
+++ language_PYTHON/백준/BJ1644.py
@@ -1,41 +1,38 @@
 # 2022-05-15
 # 2022-07-28
 # 2022-07-29
+# 2022-08-24
 # 소수의 연속합
 import sys
 si = sys.stdin.readline
 
-N = int(si())
-# 1. 소수 배열 만들기 - 에라토스의 체
-# 0, 1, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 11, 12 
-prime_num = [False, False] + [True] * (N-1
-)
+n = int(si())
+
+# 1. 소수 구하기
 prime_list = []
-for i in range(2, len(prime_num)):
-    if prime_num[i] == True:
+prime_check = [False, False] + [True] * (n-1)
+for i in range(2, n+1):
+    if prime_check[i]:
         prime_list.append(i)
-        for j in range(i*i, len(prime_num), i):
-            prime_num[j] = False # 소수가 안되므로
+        for j in range(i*i, n+1, i):
+            prime_check[j] = False
 
-# 2. 연속된 소수의 합으로 표현가능한지 체크
-# 투포인터
-interval_sum = 0
-end = 0
+# 2. 연속합 -> 투 포인터
+left = 0
+right = 0
 answer = 0
-start = 0
+interval_sum = 0
 while True:
-    if interval_sum >= N:
-        if interval_sum == N:
+    if interval_sum >= n:
+        if interval_sum == n:
             answer += 1
-        
-        interval_sum = interval_sum - prime_list[start]
-        start += 1
-
-    elif interval_sum < N:
-        interval_sum = interval_sum + prime_list[end]
-        end += 1
+        # left 한칸 당기기
+        interval_sum -= prime_list[left]
+        left += 1
+    elif right == len(prime_list):
+         break
+    else:
+        interval_sum += prime_list[right]
+        right += 1
     
-    elif end == len(prime_list):
-        break
-
 print(answer)
