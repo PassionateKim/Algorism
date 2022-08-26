f8e2f835006fe31835ed6b534b6cc32f208b7887
codekim97 <kimtaeu4616@gmail.com>
2022-08-27 08:53:58 +0900

    복습 횟수:1, 00:15:00, 복습필요X

--- language_PYTHON/Programmers/써머윈터인턴_영어_끝말잇기.py
+++ language_PYTHON/Programmers/써머윈터인턴_영어_끝말잇기.py
@@ -1,44 +1,43 @@
 # 2022-08-26
+# 2022-08-27
 # 영어 끝말잇기
 def solution(n, words):
     answer = []
-    word_set = set()
-    # 0으로 초기화
-    who = 0
-    turn = 0
-    idx1 = len(words)
-    # 1. 같은 것을 말하는 경우
-    for i in range(len(words)):
-        if words[i] not in word_set:
-            word_set.add(words[i])
-        else:
-            idx1 = i # 인덱스 저장
-            break
-            
-    # 2. 끝말로 말하지 않는 경우
-    end = words[0][-1]
-    idx2 = len(words)
+    candidate_list = list()
+    candidate_list.append(words[0])
+    check = len(words)+1
     for i in range(1, len(words)):
-        
-        start = words[i][0]
-        if start != end:
-            idx2 = i # 인덱스 저장
+        # 탈출조건
+        if candidate_list[-1][-1] != words[i][0]:
+            check = i
             break
-        else:
-            end = words[i][-1]
+        if words[i] in candidate_list:
+            check = i
+            break
+        
+        candidate_list.append(words[i])
 
-    # 더 앞의 것으로 계산
-    i = min(idx1, idx2)
-    x = i + 1 
-    who = x % n
-    if who == 0:
-        who = n
+    # 조건이 없는 경우
+    if check == len(words)+1:
+        answer.append(0)
+        answer.append(0)
+        return answer
+    else: #조건이 있는 경우
+        x = check + 1 
+        who = x % n
+        
+        if who == 0:
+            who = n
 
-    if x % n == 0:
+        
         turn = x // n
-    else:
-        turn = (x // n) + 1  
-    
+        if (x%n) != 0:
+            turn += 1
+
+    answer.append(who)
+    answer.append(turn)
+    print(answer)
+
     return answer
 
-solution(2, ["hello", "one", "even", "neven", "neven", "world", "draw"])
+solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "tank", "tank"])
