2bc73be9d1ef39a5193160cffbccb491c573415c
codekim97 <kimtaeu4616@gmail.com>
2022-09-02 10:50:41 +0900

    복습 횟수:1, 00:15:00, 복습필요O

--- language_PYTHON/Programmers/Programmers_다리를_지나는_트럭.py
+++ language_PYTHON/Programmers/Programmers_다리를_지나는_트럭.py
@@ -1,4 +1,5 @@
 # 2022-09-01
+# 2022-09-02
 # 다리를 지나는 트럭
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
@@ -9,26 +10,27 @@ def solution(bridge_length, weight, truck_weights):
     while truck_weights:
         answer += 1
         sumi = 0
+        # 트럭의 무게 
         for truck in truck_stack:
             sumi += truck[0]
-
+        # 트럭의 무게 체크해서 올리기
         if sumi + truck_weights[0] <= weight:
-            truck_stack.append([truck_weights.popleft(), bridge_length])
-        
-        for truck in truck_stack:
-            truck[1] -= 1
-            
-        if truck_stack[0][1] == 0:
-            truck_stack.popleft()
-        
-    while truck_stack:
-        answer += 1
+            truck_stack.append([truck_weights.popleft(), bridge_length]) 
+        # answer +=1 을 한 상태이니 -1 해준다
         for truck in truck_stack:
             truck[1] -= 1
         
+        # 트럭이 다리를 건넌다면 뺀다
         if truck_stack[0][1] == 0:
             truck_stack.popleft()
     
+    while truck_stack:
+        answer += 1
+        
+        if truck_stack[0][1] == 0:
+            truck_stack.popleft()
+        for truck in truck_stack:
+            truck[1] -= 1
     print(answer)
     return answer
 
