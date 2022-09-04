bee932cb78d68f48c69f1c2799850f0257a1467b
codekim97 <kimtaeu4616@gmail.com>
2022-09-05 08:49:05 +0900

    복습 횟수:1, 00:45:00, 복습필요O

--- language_PYTHON/Programmers/Programmers_H_index.py
+++ language_PYTHON/Programmers/Programmers_H_index.py
@@ -1,12 +1,38 @@
 # 2022-09-04
+# 2022-09-05
 # H-Index
+def isHindex(h, array):
+    cnt = 0
+    for i in range(len(array)):
+        if array[i] >= h:
+            # h이상인 논문의 개수
+            cnt = len(array) - i
+            break
+    # h번 이상 인용된 논문이 h편 이상인지 체크
+    if cnt >= h:
+        return True
+
+    return False
+
 def solution(citations):
+    answer = 0
     citations.sort()
-    article_count = len(citations)
+    start, end = 0, len(citations)
 
-    for i in range(article_count):
-        if citations[i] >= article_count-i:
-            return article_count-i
-    return 0
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for i in range(len(citations)):
+            if citations[i] >= mid:
+                # h 이상인 논문의 개수
+                cnt = len(citations) - i
+                break
+        if cnt >= mid:
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
 
-print(solution([3, 0, 6, 1, 5]))
+    return answer
+
+print(solution([1, 1, 1, 1, 1]))
