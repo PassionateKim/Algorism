35982e1adc23e9355b8500c96ec109dbaf6b1126
codekim97 <kimtaeu4616@gmail.com>
2022-09-08 09:00:20 +0900

    복습 횟수:3, 00:30:00, 복습필요O

--- language_PYTHON/Programmers/Programmers_다리를_지나는_트럭.py
+++ language_PYTHON/Programmers/Programmers_다리를_지나는_트럭.py
@@ -1,40 +1,21 @@
 # 2022-09-01
 # 2022-09-02
 # 2022-09-04
+# 2022-09-08
 # 다리를 지나는 트럭
 from collections import deque
-from turtle import st
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    stack = deque()
-    truck_weights = deque(truck_weights)
+    q=[0]*bridge_length
+    q = deque(q)
+    sec=0
+    while q:
+        sec+=1
+        q.popleft()
+        if truck_weights:
+            if sum(q)+truck_weights[0]<=weight:
+                q.append(truck_weights.popleft())
+            else:
+                q.append(0)
 
-
-    while truck_weights:
-        sumi = 0
-        answer += 1
-        # 1. 현재 다리에 있는 트럭의 무게 체크
-        for truck in stack:
-            sumi += truck[0]
-        # 1.1 트럭이 건널 수 있다면
-        if sumi + truck_weights[0] <= weight:
-            stack.append([truck_weights.popleft(), bridge_length])
-        # 시간이 경과한 상태를 가정하므로
-        for truck in stack:
-            truck[1] -= 1
-        
-        # 트럭이 끝점에 도달했다면
-        if stack[0][1] == 0:
-            stack.popleft()
-
-    while stack:
-        answer += 1 
-        if stack[0][1] == 0:
-            stack.popleft()
-        
-        for truck in stack:
-            truck[1] -= 1
-
-    return answer
-
-solution(2, 10, [7,4,5,6])
+    return sec
+solution(4, 10, [7,4,5,6])
