2e94dc79be1af0b6f595282f51024e0bce86b6e7
codekim97 <kimtaeu4616@gmail.com>
2022-09-10 16:46:29 +0900

    복습 횟수:1, 00:30:00, 복습필요O

--- language_PYTHON/Programmers/Programmers_JadenCase_문자열_만들기.py
+++ language_PYTHON/Programmers/Programmers_JadenCase_문자열_만들기.py
@@ -1,42 +1,25 @@
 # 2022-09-09
+# 2022-09-10
 # JadenCase 문자열 만들기
 def solution(ss):
-    blank_list = []
-    
-    i = 0
-    while i < len(ss):
-        if ss[i] == ' ':
-            cnt = 1
-            i += 1
-            while i < len(ss) and ss[i] == ' ':
-                cnt += 1
-                i += 1
-            blank_list.append(cnt)
-        i += 1
     answer = ''
-    ss = ss.split()
-    new_ss = []
-    # 1. 2차원 배열로 넣기 -> string은 바꿀 수 없음
+    ss = ss.split(" ")
     for s in ss:
-        s = list(s)
-        new_ss.append(s)
-    # 2. 변환하기
-    for string in new_ss:
-        for i in range(len(string)):
-            if i == 0:
-                if string[i].isalpha():
-                    string[i] = string[i].upper()
-            else:
-                if string[i].isalpha():
-                    string[i] = string[i].lower()
-    
-    # 3 answer을 다시 string으로 바꾸기
-    for i in range(len(new_ss) + len(blank_list)):
-        if i % 2 == 0:
-            answer += "".join(new_ss[i//2])
+        # 일반일때
+        
+        # 공백일 때
+        if s == '':
+            answer += ' '
         else:
-            answer += (" " * blank_list[i//2])
+            tmp = ''
+            for i in range(len(s)):
+                if i == 0:
+                    tmp += s[i].upper()
+                else:
+                    tmp += s[i].lower()    
+            answer += tmp +' ' 
 
     return answer
 
-print(solution("people Aws   3   "))
+print(solution("3people unFollowed me"))
+# s=s.split(" ") 자르기 참고
