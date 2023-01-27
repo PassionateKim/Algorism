8dd5f0135a272ff123c85b73fb7bbc8acdf7a8f4
codekim97 <kimtaeu4616@gmail.com>
2023-01-27 15:33:35 +0900

    BJ2805, 복습 횟수:3, 00:30:00, 복습필요X

--- language_PYTHON/백준/BJ2805.py
+++ language_PYTHON/백준/BJ2805.py
@@ -1,20 +1,25 @@
 #나무 자르기
+# 복습 횟수:3, 00:45:00, 복습필요X
 import sys
 si = sys.stdin.readline
 N, M = map(int, si().split())
 tree_list = list(map(int, si().split()))
-def Binary(start, end, target):
-    answer = 0
+answer = 0
+def binary(start, end, target):
+    global answer
     while start <= end:
         mid = (start + end) // 2
-        sum = 0
+        sumi = 0
         for tree in tree_list:
-            if tree > mid:
-                sum += (tree - mid)
-        if sum >= target:            
+            if tree >= mid:
+                sumi += (tree - mid) # diff 
+        
+        if sumi >= target:
             answer = mid
             start = mid + 1
         else:
-            end = mid - 1 
-    print(answer)
-Binary(0, max(tree_list), M)
+            end = mid - 1
+    return
+
+binary(1, max(tree_list), M)
+print(answer)
