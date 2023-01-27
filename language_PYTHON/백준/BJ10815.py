b83b490d0e3ed8a5a03188533694dad3f2acc4ce
codekim97 <kimtaeu4616@gmail.com>
2023-01-27 14:22:00 +0900

    BJ10815, 복습 횟수:1, 00:15:00, 복습필요X

--- language_PYTHON/백준/BJ10815.py
+++ language_PYTHON/백준/BJ10815.py
@@ -1,34 +1,30 @@
 # 숫자 카드
-# 복습 횟수:0, 00:30:00, 복습필요O
+# 복습 횟수:1, 00:15:00, 복습필요X
 import sys
 si = sys.stdin.readline
 N = int(si())
 my_card = list(map(int, si().split()))
 my_card.sort()
-mini = min(my_card)
-maxi = max(my_card)
-
-answer = []
 M = int(si())
 check_list = list(map(int, si().split()))
 
+answer = []
 
 def binary(start, end, target):
     while start <= end:
-        idx = (start + end) // 2
-        mid = my_card[idx]
+        mid = (start + end) // 2
+        if my_card[mid] == target:
+            return 1
 
-        if mid <= target:
-            if mid == target:
-                return 1
-            start = idx + 1
+        if my_card[mid] > target:
+            end = mid - 1
         else:
-            end = idx - 1
+            start = mid + 1
 
     return 0
 
 for check in check_list:
-    val = binary(0, len(my_card) -1, check)
+    val = binary(0, len(my_card) - 1, check)
     answer.append(val)
 
 print(*answer)
