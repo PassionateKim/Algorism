99ed3513b3a01c559cc77463acf828701a952577
codekim97 <kimtaeu4616@gmail.com>
2023-01-29 16:01:40 +0900

    BJ1654, 복습 횟수:2, 00:15:00, 복습필요X

--- language_PYTHON/백준/BJ1654.py
+++ language_PYTHON/백준/BJ1654.py
@@ -1,27 +1,27 @@
 # 랜선 자르기
+# 복습 횟수:2
 import sys
 si = sys.stdin.readline
 K, N = map(int, si().split())
-A = []
+line_list = []
 for i in range(K):
-    A.append(int(si()))
-
-A.sort()
-
-def Binary(array, start, end, target):
-    answer = 0
-    while start<=end:
-        mid = (start+end)//2
-        cnt = 0
-        for i in array:
-            cnt += i//mid
+    line_list.append(int(si()))
+answer = 0
+def binary(start, end, target):
+    global answer
+    while start <= end:
+        mid = (start + end) // 2
+        tmp = 0
+        for line in line_list:
+            tmp += (line // mid)
         
-        if cnt >= target:
+        if tmp >= target:
             answer = mid
             start = mid + 1
         else:
             end = mid - 1
 
-    return answer
+    return
 
-print(Binary(A, 0, max(A), N))
+binary(1, max(line_list), N)
+print(answer)
