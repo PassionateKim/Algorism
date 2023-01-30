bec7c0e44b4a06d6df05c9f93832bb1923afa76f
codekim97 <kimtaeu4616@gmail.com>
2023-01-30 09:56:03 +0900

    BJ2512, 복습 횟수:1, 00:30:00, 복습필요X

--- language_PYTHON/백준/BJ2512.py
+++ language_PYTHON/백준/BJ2512.py
@@ -1,37 +1,29 @@
 # 예산
+# 복습 횟수:1, 00:30:00, 복습필요:X
 import sys
 si = sys.stdin.readline
-
 N = int(si())
-A = list(map(int, si().split()))
-maxi = int(si())
+request_list = list(map(int, si().split()))
+budget = int(si())
+answer = 0
 
-# 모든 요청이 배정될 수 있는 경우에는 요청한 금액을 그대로 배정한다.
-if sum(A) <= maxi:
-    print(max(A))
-    exit()
-
-# 모든 요청이 배정될 수 없는 경우에는 특정한 정수 상한액을 계산하여 그 이상인 예산요청에는 모두 상한액을 배정한다. 
-# 상한액 이하의 예산요청에 대해서는 요청한 금액을 그대로 배정한다. 
-
-def Binary(array, start, end, target):
-    answer = 0
-    while start<=end:
-        mid = (start+end)//2
-        
+def binary(start, end, target):
+    global answer
+    while start <= end:
         sumi = 0
-        for i in array:
-            if i >= mid:
-                sumi += mid
+        mid = (start + end) // 2
+        for request in request_list:
+            if mid >= request:
+                sumi += request
             else:
-                sumi += i
-
+                sumi += mid
+        
         if sumi <= target:
             answer = mid
             start = mid + 1
         else:
-            end = mid - 1
+            end = mid - 1 
+    return
 
-    return answer
-
-print(Binary(A, 1, max(A), maxi))
+binary(0, max(request_list), budget)
+print(answer)
