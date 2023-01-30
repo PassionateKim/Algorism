84fee4beb5b2b1ad99b33bbcc39bdb206dc7a714
codekim97 <kimtaeu4616@gmail.com>
2023-01-30 09:12:24 +0900

    BJ1789, 복습 횟수:1, 00:30:00, 복습필요O

--- language_PYTHON/백준/BJ1789.py
+++ language_PYTHON/백준/BJ1789.py
@@ -1,24 +1,22 @@
 # 수들의 합
-# 복습 횟수:0, 00:45:00, 복습필요O
+# 복습 횟수:1, 00:30:00, 복습필요O
 import sys
 si = sys.stdin.readline
 answer = 0
-sumi = 0
 N = int(si())
 
-def binary(start, end):
+def binary(start, end, target):
     global answer
     while start <= end:
         mid = (start + end) // 2
-        target = ((mid) * (mid + 1)) // 2 # n(n+1) // 2 = 총합 공식
 
-        if target <= N:
-            answer = mid
-            start = mid + 1
-        else:
+        if (mid)*(mid + 1) // 2 > target:
+            answer = mid - 1
             end = mid - 1
+        else:
+            start = mid + 1
 
     return
 
-binary(1, N)
+binary(1, 4294967295, N)
 print(answer)
