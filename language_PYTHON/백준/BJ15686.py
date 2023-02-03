982e5ba98e9d93a176bdb4f9c974654629f3e4d5
codekim97 <kimtaeu4616@gmail.com>
2023-02-03 16:00:39 +0900

    BJ15686, 복습 횟수:3, 00:15:00, 복습필요X

--- language_PYTHON/백준/BJ15686.py
+++ language_PYTHON/백준/BJ15686.py
@@ -1,48 +1,31 @@
-# 2022-05-13
-# 2022-06-05
-# 2022-07-27
 # 치킨 배달
-from re import L
+# 복습 횟수:3, 00:15:00, 복습필요X
 import sys
+from itertools import combinations
 si = sys.stdin.readline
 N, M = map(int, si().split())
-graph = [list(map(int, si().split())) for _ in range(N)]
-
+graph = []
+for i in range(N):
+    graph.append(list(map(int, si().split())))
+answer = sys.maxsize
 chicken_list = []
 house_list = []
 
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 1:
-            house_list.append([i,j])
-        elif graph[i][j] == 2:
-            chicken_list.append([i,j])
+            house_list.append([i, j])
+        if graph[i][j] == 2:
+            chicken_list.append([i, j])
 
-answer = 1e10
-def dfs(idx, arr : list):
-    global answer
-    if len(arr) == M:
-        city_d = 0
-        # 집 in 집_list:
-        for house in house_list:
-            hx, hy = house[0], house[1]
-            chicken_d = 1e10
-            for chicken in arr:
-                cx, cy = chicken[0], chicken[1]
-                tmp = abs(hx- cx) + abs(hy - cy)
-                
-                # 치킨 거리 구하기
-                chicken_d = min(chicken_d, tmp)
-            # 치킨 거리 더하기
-            city_d += chicken_d
-        # / 집 list
-        answer = min(city_d, answer)
-        return
+chicken_list_combi = list(combinations(chicken_list, M))
+for chicken_list in chicken_list_combi:
+    tmp_sum = 0
+    for house_x, house_y in house_list:
+        c_d = sys.maxsize
+        for chicken_x, chicken_y in chicken_list:
+            c_d = min(abs(house_x - chicken_x) + abs(house_y - chicken_y), c_d)
+        tmp_sum += c_d
+    answer = min(answer, tmp_sum)
 
-    for i in range(idx, len(chicken_list)):
-        arr.append(chicken_list[i])
-        dfs(i+1, arr)
-        arr.pop()
-
-dfs(0, [])
 print(answer)
