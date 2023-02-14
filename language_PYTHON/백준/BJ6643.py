ad6c22e315635c903b1fdab10712fc603ffb849a
codekim97 <kimtaeu4616@gmail.com>
2023-02-14 15:23:27 +0900

    BJ6643, 00:30:00, 복습필요O

--- language_PYTHON/백준/BJ6643.py
+++ language_PYTHON/백준/BJ6643.py
@@ -1,64 +1,32 @@
 # 애너그램
-# 복습 횟수:0, 00:30:00, 복습필요O
-# import sys
-# si = sys.stdin.readline
-# N = int(si())
-
-# def dfs(current, li: list):
-
-#     if current == len(input):
-#         word_set.add("".join(li))
-#         return
-    
-#     for i in range(len(input)):
-#         if visited[i] == 0:
-#             li.append(input[i]) # 추가
-#             visited[i] = 1 # 방문처리
-#             dfs(current + 1, li)
-#             li.pop() # 초기화
-#             visited[i] = 0 # 초기화
-
-#     return
-
-# for i in range(N):
-#     word_list = list()
-#     word_set = set() 
-
-#     input = list(map(str, si().rstrip()))
-#     input.sort()
-#     visited = [0 for _ in range(len(input))]
-
-#     dfs(0, [])
-#     word_list = list(word_set)
-#     word_list.sort()
-#     for word in word_list:
-#         print(word)
-
+# 복습 횟수:1, 00:30:00, 복습필요O
 import sys
 si = sys.stdin.readline
-def dfs(cnt):
-    if cnt == len(word):
+N = int(si())
+
+def dfs(enegram_dict, answer: list):
+
+    if len(answer) == len(c_list):
         print("".join(answer))
         return
     
-    for k in visited:
-        if visited[k]:
-            visited[k] -= 1
-            answer.append(k)
-            dfs(cnt + 1)
-            visited[k] += 1
+    for enegram in enegram_dict:
+        if enegram_dict[enegram]:
+            enegram_dict[enegram] -= 1
+            answer.append(enegram)
+            dfs(enegram_dict, answer)
+            enegram_dict[enegram] += 1
             answer.pop()
-n = int(si())
-for _ in range(n):
-    word = sorted(list(map(str, si().strip())))
-    visited = {}
-    answer = []
+    return
 
-
-    for i in word:
-        if i in visited:
-            visited[i] += 1
+for i in range(N):
+    c_list = list(map(str, si().rstrip()))
+    c_list.sort()
+    enegram_dict = dict()
+    for c in c_list:
+        if c in enegram_dict.keys():
+            enegram_dict[c] += 1
         else:
-            visited[i] = 1
-
-    dfs(0)
+            enegram_dict[c] =  1
+    
+    dfs(enegram_dict, [])
