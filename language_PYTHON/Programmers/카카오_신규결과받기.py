286bf1e880aefccff2d6fef4eb668fe4fd93e7f3
codekim97 <kimtaeu4616@gmail.com>
2023-02-21 14:29:22 +0900

    복습 횟수:1, 00:45:00, 복습필요X

--- language_PYTHON/Programmers/카카오_신규결과받기.py
+++ language_PYTHON/Programmers/카카오_신규결과받기.py
@@ -1,47 +1,35 @@
-#신고결과받기
 from collections import defaultdict
-
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)    
-    reports = {x : 0 for x in id_list}
+    answer = []
+    callie_dict = defaultdict(list)
+    report_dict = dict()
+    report_set = set(report) # 중복 제외를 위해 set 사용
 
-    for r in set(report):
-        reports[r.split()[1]] += 1
-    print(reports)
-    print(id_list)
-    for r in set(report):    
-        if reports[r.split()[1]] >= k:
-            answer[id_list.index(r.split()[0])] += 1
-            print(r.split()[0],answer)
+    for report in report_set:
+        caller, collie = map(str, report.split())
+        
+        callie_dict[caller].append(collie)
+        
+        if collie in report_dict.keys():
+            report_dict[collie] += 1
+        else:
+            report_dict[collie] = 1
+        
+    target_list = []
+    for key, value in report_dict.items():
+        if value >= k:
+            target_list.append(key)
+    
+    for id in id_list:
+        if callie_dict[id]:
+            tmp = 0
+            for callie in callie_dict[id]:
+                if callie in target_list:
+                    tmp += 1
+            answer.append(tmp)
+        else:
+            answer.append(0)
 
     return answer
 
-# def solution(id_list, report, k):
-#     answer = [0] *len(id_list)
-    
-#     report = set(report)
-
-#     user_list_i_reported = defaultdict(set)
-#     num_of_reported = defaultdict(int)
-#     prosecuted = []
-
-#     for r in report:
-#         report,be_reported = r.split()
-#         num_of_reported[be_reported] +=  1
-#         user_list_i_reported[report].add(be_reported)
-
-#         if(num_of_reported[be_reported] == k):
-#             prosecuted.append(be_reported)
-
-
-    
-    
-            
-#     print(user_list_i_reported)
-
-#     for target in prosecuted:
-#         for i in range(len(id_list)):
-#             if (target in user_list_i_reported[id_list[i]]):
-#                 answer[i] += 1
-#     return answer 
-solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2)
+solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)
