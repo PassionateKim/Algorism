bac3998e8c3b822f337575b8755ce90103331a83
codekim97 <kimtaeu4616@gmail.com>
2023-02-21 15:22:47 +0900

    복습 횟수:1, 01:00:00, 복습필요O

--- language_PYTHON/Programmers/카카오_주차_요금_계산.py
+++ language_PYTHON/Programmers/카카오_주차_요금_계산.py
@@ -1,53 +1,47 @@
-# 2022-09-14
 # 주차 요금 계산
-from collections import defaultdict
+# 복습 횟수:1, 01:00:00, 복습필요O
 import math
-def solution(fees, records):
-    check_dict = defaultdict(list)
-    answer = []
-    standard_time, standard_fee, time, fee = fees[0], fees[1], fees[2], fees[3]
-    print("기본 시간:",standard_time,"기본 요금:" , standard_fee,"단위 시간:",time,"단위 요금:", fee)
-    # 1. dict 안에 담기
-    for record in records:
-        record = record.split()
-        t, key, b = record[0], record[1], record[2]
-        # t int -> 분으로 계산하기
-        t = t.split(":")
-        t = int(t[0]) * 60 + int(t[1])
-        if b == 'IN':
-            check_dict[key].append((-t, b))
-        else:
-            check_dict[key].append((t, b))
-       
-    # 2. 3개면 무조건 출차 내역이 없는 것이므로, 23:59에 출차된 것을 추가한다.
-    for key, value in check_dict.items():
-        if len(value) % 2 != 0:
-            check_dict[key].append((1439,'OUT'))
-    
-    
-    check_dict = sorted(check_dict.items())
-    
-    # 3. 각각의 주차 시간 구하기
-    candidate = []
-    for val in check_dict:
-        sumi = 0
-        for t in val[1]:
-            sumi += t[0]
-        candidate.append(sumi)
-    
-    # 주차 요금 계산하기
-    for candi in candidate:
-        # 주차 시간이 기본 시간보다 작거나 같은 경우
-        if candi <= standard_time:
-            answer.append(standard_fee)
-        else:
-            sumi = 0 + standard_fee
-            diff_time = candi - standard_time
-            q = math.ceil(diff_time/time)
-            sumi += q * fee
+from collections import defaultdict
 
-            answer.append(sumi)
-    print(answer)
-    return answer
+def solution(fees, records):
+    answer = []
+    car_set = set()
+    car_dict = defaultdict(list)
+    # 기본 시간(분) # 기본 요금 # 단위 시간 # 단위 요금
+    for info in records:
+        time, number, in_out = map(str, info.split())
+        time = (int(time.split(":")[0]) * 60) + int(time.split(":")[1])
+
+        # car 넣기 - for sort
+        car_set.add(number)
+        car_list = list(car_set)
+        car_list.sort()
+
+        if car_dict[number]:
+            tmp = time - car_dict[number][0]
+            answer.append([number, tmp])
+            car_dict[number].pop()
+        else:
+            car_dict[number].append(time)    
+    
+    for key, value in car_dict.items():
+        if len(value) == 1:
+            answer.append([key, 23*60 + 59 - value[0]]) 
+    
+    # 계산하기
+    time_list = [0] * len(car_list)
+    for val in answer:
+        index = car_list.index(val[0])
+        time_list[index] += val[1]
+    result = []
+
+    for time in time_list:
+        if time <= fees[0]: #기본시간보다 작거나 같은 경우
+            result.append(fees[1])
+        else:
+            diff_t = time - fees[0]
+            diff_fee = math.ceil((diff_t / fees[2])) * fees[3]
+            result.append(fees[1] + diff_fee) 
+    return result
 
 solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"])
