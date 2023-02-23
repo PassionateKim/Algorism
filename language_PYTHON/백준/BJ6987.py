e5c5e29786fc1cfe6f88e54c708e8eed2ed4d451
codekim97 <kimtaeu4616@gmail.com>
2023-02-23 10:20:46 +0900

    BJ987, 복습 횟수:1, 00:30:00, 복습필요O

--- language_PYTHON/백준/BJ6987.py
+++ language_PYTHON/백준/BJ6987.py
@@ -1,51 +1,58 @@
 # 월드컵
-# 복습 횟수:0, 01:00:00, 복습필요O
-import sys
+# 복습 횟수:1, 00:30:00, 복습필요O
 from itertools import combinations
-
+import sys
 si = sys.stdin.readline
-battle_case = list(combinations(range(6), 2))
-
-def dfs(home, away):
-    global L, tmp
-
-    if away == 6:
-        home += 1
-        away = home + 1
-    
-    if home == 5:
-        if L == [[0,0,0] for _ in range(6)]:
-            tmp = 1
-            return
-
-    # home 팀이 이기는 경우
-    if L[home][0] > 0 and L[away][2] > 0:
-        L[home][0], L[away][2]  = L[home][0] - 1, L[away][2] - 1
-        dfs(home, away + 1)
-        L[home][0], L[away][2]  = L[home][0] + 1, L[away][2] + 1
-    
-    # home 팀이 지는 경우
-    if L[home][2] > 0 and L[away][0] > 0:
-        L[home][2], L[away][0] = L[home][2] - 1, L[away][0] - 1
-        dfs(home, away + 1)
-        L[home][2], L[away][0] = L[home][2] + 1, L[away][0] + 1
-    
-    # 비기는 경우
-    if L[home][1] > 0 and L[away][1] > 0:
-        L[home][1], L[away][1] = L[home][1] - 1, L[away][1] - 1
-        dfs(home, away + 1)
-        L[home][1], L[away][1] = L[home][1] + 1, L[away][1] + 1
-
-
 answer = []
-for round in range(4):
-    question = list(map(int, si().split()))
-    L = []
-    for i in range(6):
-        L.append([question[i*3], question[(i*3) +1], question[i*3 + 2]])
+def dfs(index):
+    global flag
+    if index == 15:
+        check = 0
+        for val in graph:
+            check += sum(val)
+        if check == 0:
+            flag = 1
+
+        return
+
+    me, you = match_list[index]
     
-    tmp = 0
-    dfs(0, 1)
-    answer.append(tmp)
+    # me 승
+    if graph[me][0] > 0 and graph[you][2] > 0:
+        graph[me][0] -= 1
+        graph[you][2] -= 1
+        dfs(index + 1)
+        graph[me][0] += 1
+        graph[you][2] += 1
+
+    # me 무
+    if graph[me][1] > 0 and graph[you][1] > 0:
+        graph[me][1] -= 1
+        graph[you][1] -= 1
+        dfs(index + 1)    
+        graph[me][1] += 1
+        graph[you][1] += 1
+        
+    # me 패
+    if graph[me][2] > 0 and graph[you][0] > 0:
+        graph[me][2] -= 1
+        graph[you][0] -= 1
+        dfs(index + 1)
+        graph[me][2] += 1
+        graph[you][0] += 1
+
+    return
+
+for i in range(4):
+    tmp = list(map(int, si().split()))
+    graph = [[tmp[3*j], tmp[3*j + 1], tmp[3*j + 2]] for j in range(6)]
+    
+    match_list = list(combinations(range(6), 2))
+    flag = 0
+    dfs(0)
+    if flag:
+        answer.append(1)
+    else:
+        answer.append(0)
 
 print(*answer)
