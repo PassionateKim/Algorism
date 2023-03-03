177628f9ca062b5d74bf88dde81885936dfea274
codekim97 <kimtaeu4616@gmail.com>
2023-03-03 15:27:04 +0900

    BJ1018, 복습 횟수:2, 00:15:00, 복습필요X

--- language_PYTHON/백준/BJ1018.py
+++ language_PYTHON/백준/BJ1018.py
@@ -1,44 +1,44 @@
 #체스판 다시 칠하기
+# 복습 횟수:2, 00:15:00, 복습필요X
 import sys
 si = sys.stdin.readline
+answer = sys.maxsize
+case1 = []
+case2 = []
+for i in range(8):
+    if i % 2 == 0:
+        case1.append(list('WBWBWBWB'))
+    else:
+        case1.append(list('BWBWBWBW'))
+
+for i in range(8):
+    if i % 2 == 0:
+        case2.append(list('BWBWBWBW'))
+    else:
+        case2.append(list('WBWBWBWB'))
 
 N, M = map(int, si().split())
-graph = []
+graph = [list(map(str, si().strip())) for _ in range(N)]
+def checkDiff(i, j):
+    cnt1 = 0
+    # case1 체크
+    for x in range(8):
+        for y in range(8):
+            if case1[x][y] != graph[i + x][j + y]:
+                cnt1 += 1
+    
+    cnt2 = 0
+     # case2 체크
+    for x in range(8):
+        for y in range(8):
+            if case2[x][y] != graph[i + x][j + y]:
+                cnt2 += 1
 
-for i in range(N):
-    graph.append(list(si()))
+    return min(cnt1, cnt2)
 
-answer = 1e5 # 최대 50 * 50
-
-for i in range((N-8) + 1):
-    for j in range((M-8) + 1):
-        white_start, black_start = 0, 0
-        
-        for x in range(8): # 8*8 돌기
-            for y in range(8):
-                nx, ny = x + i, y + j
-                #시작
-                if (nx + ny) % 2 == 0: 
-                    if graph[nx][ny] == 'B': # white 여야 하는 부분
-                        white_start += 1
-                    elif graph[nx][ny] == 'W': # black 이어야 하는 부분
-                        black_start += 1
-                else: 
-                    if graph[nx][ny] == 'W':
-                        white_start += 1 # black이어야하는부분
-                    elif graph[nx][ny] == 'B':
-                        black_start += 1 # white이어야하는부분
-                
-                #// y
-        if answer > min(white_start, black_start):
-            answer = min(white_start, black_start)
+for i in range(N - 8 + 1):
+    for j in range(M - 8 + 1):
+        cnt = checkDiff(i, j)
+        answer = min(answer, cnt)
 
 print(answer)
-
-
-
-
-
-        
-                
-            
