af65a651b5d4b4e53e9450767434f96ebee26017
codekim97 <kimtaeu4616@gmail.com>
2023-03-07 14:11:37 +0900

    복습 횟수:1, 00:30:00, 복습필요X

--- language_PYTHON/Programmers/Programmers_야근_지수.py
+++ language_PYTHON/Programmers/Programmers_야근_지수.py
@@ -1,23 +1,24 @@
-# 복습 횟수:0, 00:30:00, 복습필요O
+# 복습 횟수:1, 00:30:00, 복습필요X
 import heapq
-def solution(works: list, n):
+def solution(n, works: list):
     answer = 0
     works = [-w for w in works]
-    heapq.heapify(works)
-
+    heapq.heapify(works)    
+    
     for i in range(n):
         val = heapq.heappop(works)
-
         if val != 0:
             val = val + 1
         heapq.heappush(works, val)
-
     
-    for val in works:
-        answer += val ** 2
+    
+    for w in works:
+        answer += w ** 2
 
-    print(answer)
     return answer
 
+solution([4, 3, 3], 4)
+
+solution([2, 1, 2], 1)
 
 solution([1, 1], 3)
