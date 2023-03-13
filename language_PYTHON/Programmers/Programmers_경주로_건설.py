524f98cf0ce57d07d6e78a235b55e0971603f532
codekim97 <kimtaeu4616@gmail.com>
2023-03-13 11:55:02 +0900

    복습 횟수:1, 00:30:00, 복습필요O

--- language_PYTHON/Programmers/Programmers_경주로_건설.py
+++ language_PYTHON/Programmers/Programmers_경주로_건설.py
@@ -1,31 +1,38 @@
-# 복습 횟수:0, 02:30:00, 복습필요O
+# 복습 횟수:1, 00:30:00, 복습필요O
 from collections import deque
 import math
-dx,dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def solution(board):
-    def bfs(x, y, cost, direction):
-        n = len(board)
-        result = [[math.inf for i in range(n)] for i in range(n)]
 
+    def bfs(x, y, direction, cost):
         q = deque()
-        q.append([x, y, cost, direction])
-        result[x][y] = 0
+        q.append([x, y, direction, cost])
+        n = len(board)
+        result = [[math.inf for _ in range(n)] for _ in range(n)]
+        result[x][y] = 0 
+
         while q:
-            x, y, cost, direction = q.popleft()
+            x, y, direction, cost = q.popleft()
             for idx in range(4):
                 nx, ny = x + dx[idx], y + dy[idx]
+
                 if not (0 <= nx < n and 0 <= ny < n): continue
+                if board[nx][ny] == 1: continue
 
-                if direction == idx:
-                    new_cost = cost + 100
-                else: # 방향이 다르다면
+                if direction != idx:
                     new_cost = cost + 600
-
-                if new_cost < result[nx][ny] and board[nx][ny] == 0: # 이때만 가능함.
-                    q.append([nx, ny, new_cost, idx])
+                else:
+                    new_cost = cost + 100
+                
+                if new_cost < result[nx][ny]:
+                    q.append([nx, ny, idx, new_cost])
                     result[nx][ny] = new_cost
-        
+
         return result[-1][-1]
     
-    return min(bfs(0, 0, 0, 1), bfs(0, 0, 0, 3)) 
+    return min(bfs(0, 0, 1, 0), bfs(0, 0, 3, 0))
+    
+print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]))
