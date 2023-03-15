0db8dfc32206a9eccfbf1b76b8b0b3372aa63f96
codekim97 <kimtaeu4616@gmail.com>
2023-03-15 16:44:00 +0900

    BJ17780, 복습 횟수:1, 00:45:00, 복습필요O

--- language_PYTHON/백준/BJ17780.py
+++ language_PYTHON/백준/BJ17780.py
@@ -1,11 +1,11 @@
 # 새로운 게임
-# 복습 횟수:0, 01:30:00, 복습필요O
+# 복습 횟수:1, 00:45:00, 복습필요O
 import sys
 from collections import deque
 si = sys.stdin.readline
 N, K = map(int, si().split())
 
-dx = [0, 0 ,-1, 1]
+dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 graph = [list(map(int, si().split())) for _ in range(N)]
 
@@ -23,70 +23,74 @@ def changeDir(dir):
         return 0
     if dir == 2:
         return 3
-    else: 
-        return 2
     
+    return 2
 answer = 0
-
 def check():
     global answer
     while True:
         if answer > 1000:
             answer = -1
-            break
+            return
         
-        # 탈출 조건
-        for x in range(N):
-            for y in range(N):
-                if len(horse_graph[x][y]) >= 4:
+        for i in range(N):
+            for j in range(N):
+                if len(horse_graph[i][j]) >= 4:
                     return
+
+        answer += 1
         
-        # 이동 시작
-        for i in range(K):
+
+        # 체스 번호    
+        for chess in range(K):
             flag = 0
             for x in range(N):
                 for y in range(N):
-                    if len(horse_graph[x][y]) != 0 and i == horse_graph[x][y][0][0]: # 맨 아래에 있다면
-                        dir = horse_graph[x][y][0][1]
+                    horse_list = horse_graph[x][y]
+                    # 맨 아래에 있는 경우에만 움직일 수 있다.
+                    if len(horse_list) != 0 and horse_list[0][0] == chess:
+                        dir = horse_list[0][1]
                         nx, ny = x + dx[dir], y + dy[dir]
-
-                        # 넘어가는 경우 or 파란색인 경우
+                        # 파란색 or 밖
                         if not (0 <= nx < N and 0 <= ny < N) or graph[nx][ny] == 2:
-                            dir = changeDir(dir)
-                            horse_graph[x][y][0][1] = dir
+                            horse_list[0][1] = changeDir(horse_list[0][1])
+                            dir = horse_list[0][1]
                             nx, ny = x + dx[dir], y + dy[dir]
-                            
-                            # 결국 파란색인 경우
+                            # 파란색인 경우
                             if not (0 <= nx < N and 0 <= ny < N) or graph[nx][ny] == 2:
-                                continue
+                                flag = 1
+                                break 
                             # 빨간색인 경우
-                            elif graph[nx][ny] == 1:
+                            if graph[nx][ny] == 1:
                                 while horse_graph[x][y]:
                                     horse = horse_graph[x][y].pop()
                                     horse_graph[nx][ny].append(horse)
+                                flag = 1
+                                break
                             # 흰색인 경우
-                            elif graph[nx][ny] == 0:
+                            if graph[nx][ny] == 0:
                                 while horse_graph[x][y]:
                                     horse = horse_graph[x][y].popleft()
                                     horse_graph[nx][ny].append(horse)
-                                
+                                flag = 1
+                                break
+
                         # 빨간색인 경우
-                        elif graph[nx][ny] == 1:
+                        if graph[nx][ny] == 1:
                             while horse_graph[x][y]:
                                 horse = horse_graph[x][y].pop()
                                 horse_graph[nx][ny].append(horse)
+                            flag = 1
+                            break
                         # 흰색인 경우
-                        elif graph[nx][ny] == 0:
+                        if graph[nx][ny] == 0:
                             while horse_graph[x][y]:
                                 horse = horse_graph[x][y].popleft()
                                 horse_graph[nx][ny].append(horse)
-                        flag = 1
-                    if flag:
-                        break
+                            flag = 1
+                            break
                 if flag:
                     break
-                        
-        answer += 1
 
 check()
 print(answer)
