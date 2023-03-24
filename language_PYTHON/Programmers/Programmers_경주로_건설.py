fdd81d05788318b76af2cbe67d9f5e302ca24062
codekim97 <kimtaeu4616@gmail.com>
2023-03-24 15:46:08 +0900

    복습 횟수:2, 00:30:00, 복습필요X

--- language_PYTHON/Programmers/Programmers_경주로_건설.py
+++ language_PYTHON/Programmers/Programmers_경주로_건설.py
@@ -1,38 +1,44 @@
-# 복습 횟수:1, 00:30:00, 복습필요O
+# 복습 횟수:2, 00:30:00, 복습필요O
+
 from collections import deque
 import math
-
+# 상하좌우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def solution(board):
+    
+    N = len(board)
+    M = len(board[0])
 
-    def bfs(x, y, direction, cost):
+    def bfs(x, y, dir, price):
+        visited = [[math.inf for _ in range(M)] for __ in range(N)]
+        visited[x][y] = 0 # 처음부분은 0으로 초기화
+
         q = deque()
-        q.append([x, y, direction, cost])
-        n = len(board)
-        result = [[math.inf for _ in range(n)] for _ in range(n)]
-        result[x][y] = 0 
+        q.append([x, y, dir, price])
 
         while q:
-            x, y, direction, cost = q.popleft()
+            x, y, dir, price = q.popleft()
+        
+            # new_price가 더 작을 때만 갈 수 있도록 하기
             for idx in range(4):
                 nx, ny = x + dx[idx], y + dy[idx]
 
-                if not (0 <= nx < n and 0 <= ny < n): continue
-                if board[nx][ny] == 1: continue
-
-                if direction != idx:
-                    new_cost = cost + 600
-                else:
-                    new_cost = cost + 100
+                if not (0 <= nx < N and 0 <= ny < M): continue # 범위밖 X
+                if board[nx][ny] == 1: continue # 벽이기 때문에 갈 수 없으므로
+                # 방향이 같은 경우
+                if idx == dir:
+                    new_price = price + 100
+                else: # 방향이 달라서 꺾어야 하는 경우
+                    new_price = price + 600
                 
-                if new_cost < result[nx][ny]:
-                    q.append([nx, ny, idx, new_cost])
-                    result[nx][ny] = new_cost
+                if new_price < visited[nx][ny]: # 가격이 더 작을 때에만
+                    q.append([nx, ny, idx, new_price])
+                    visited[nx][ny] = new_price
 
-        return result[-1][-1]
-    
+        return visited[-1][-1]
+
     return min(bfs(0, 0, 1, 0), bfs(0, 0, 3, 0))
     
-print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]))
+print(solution([[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]]))
