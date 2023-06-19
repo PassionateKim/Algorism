a9de9ed0d07e0b13b03babb43c60599dfede951e
codekim97 <kimtaeu4616@gmail.com>
2023-06-19 10:54:42 +0900

    BJ11725, 복습 횟수:1, 00:30:00, 복습필요O

--- language_PYTHON/백준/BJ11725.py
+++ language_PYTHON/백준/BJ11725.py
@@ -1,10 +1,12 @@
 # 트리의 부모 찾기
-# 복습 횟수:0, 01:00:00, 복습필요O
+# 복습 횟수:1, 00:30:00, 복습필요O
 import sys
 from collections import deque
 si = sys.stdin.readline
 N = int(si())
-graph = [[] for _ in range(N+1)]
+
+
+graph = [[] for i in range(N+1)]
 answer = [0] * (N+1)
 visited = [0] * (N+1)
 
@@ -13,6 +15,8 @@ for i in range(N-1):
     graph[x].append(y)
     graph[y].append(x)
 
+
+                 
 def bfs(start):
     q = deque()
     visited[start] = 1 # 방문처리
@@ -20,15 +24,18 @@ def bfs(start):
 
     while q:
         idx = q.popleft()
-        
+
         for val in graph[idx]:
             if visited[val]: continue
 
             q.append(val)
             answer[val] = idx
-            visited[val] = 1 
+            visited[val] = 1
+
     return
 
 bfs(1)
+
 for i in range(2, len(answer)):
     print(answer[i])
+
