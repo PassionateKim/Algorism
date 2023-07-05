bddbc4cc73e4d23a01b5fca788b914f28a74e101
codekim97 <kimtaeu4616@gmail.com>
2023-07-05 09:38:28 +0900

    복습 횟수:1, 00:30:00, 복습필요X

--- language_PYTHON/codetree/lv5_hashmap_두_수의_합.py
+++ language_PYTHON/codetree/lv5_hashmap_두_수의_합.py
@@ -1,18 +1,22 @@
+# 복습 횟수:1, 00:30:00, 복습필요X
 import sys
 si = sys.stdin.readline
 N, M = map(int, si().split())
-num_list = list(map(int, si().split()))
+num_arr = list(map(int, si().split()))
 
-myDict = dict()
 answer = 0
-
-for num in num_list:
-    if M-num in myDict:
-        answer += myDict[M-num]
-    
-    if num in myDict:
-        myDict[num] += 1
+num_dict = dict()
+for num in num_arr:
+    if(num not in num_dict.keys()):
+        num_dict[num] = 1
     else:
-        myDict[num] = 1
+        num_dict[num] += 1
+
+for num in num_arr:
+    num_dict[num] -= 1
+
+    diff = M - num
+    if diff in num_dict.keys():
+        answer += num_dict[diff]
 
 print(answer)
