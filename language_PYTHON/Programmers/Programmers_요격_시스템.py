2a87df1403496f61345979d24a4ddead769fa8fc
codekim97 <kimtaeu4616@gmail.com>
2023-07-12 09:28:49 +0900

    복습 횟수:1, 00:30:00, 복습필요:*(세 달뒤 정도)

--- language_PYTHON/Programmers/Programmers_요격_시스템.py
+++ language_PYTHON/Programmers/Programmers_요격_시스템.py
@@ -1,17 +1,17 @@
 # 2023-06-30
-# 복습 횟수:0, 01:00:00, 복습필요O
-
+# 2023-07-12
+# 복습 횟수:1, 00:30:00, 복습필요:*(세 달 뒤 쯤 복습하면 될듯 - 그때도 맞으면 끝)
 def solution(targets: list):
     answer = 0
-    
-    targets.sort(key = lambda x: x[1])
+    targets.sort(key=lambda x: x[1])
+    end = 0
 
-    s = e = 0
     for target in targets:
-        if target[0] >= e:
+        if target[0] >= end:
             answer += 1
-            e = target[1]
-        
+            end = target[1]
+
+
     return answer
 
-solution([[1,4], [4,5], [4,8], [10,14], [11,13] ,[5, 12], [3, 7]])
+solution([[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]])
