79ca4069c682a96a867659e13956a251e0520e72
codekim97 <kimtaeu4616@gmail.com>
2023-07-13 10:09:41 +0900

    복습 횟수:2, 00:30:00, 복습필요:***

--- language_PYTHON/codetree/lv5_hashmap_세_수의_합.py
+++ language_PYTHON/codetree/lv5_hashmap_세_수의_합.py
@@ -1,28 +1,27 @@
-# 복습 횟수:0, 01:30:00, 복습필요O
-
+# 2023-07-15
+# 2023-07-23
+# 복습 횟수:2, 00:30:00, 복습필요:***
 import sys
 si = sys.stdin.readline
 
 N, K = map(int, si().split())
-arr = list(map(int, si().split()))
+elem_list = list(map(int, si().split()))
 
 mydict = dict()
-
-for i in range(len(arr)):
-    if( arr[i] not in mydict.keys()):
-        mydict[arr[i]] = 1
-    else:
-        mydict[arr[i]] += 1
 answer = 0
+for elem in elem_list:
+    if elem not in mydict:
+        mydict[elem] = 1
+    else:
+        mydict[elem] += 1
 
-for i in range(len(arr)):
-
-    mydict[arr[i]] -= 1
+for i in range(len(elem_list)):
+    mydict[elem_list[i]] -= 1
 
     for j in range(i):
-        diff = K - arr[i] - arr[j]
-
-        if(diff in mydict.keys()):
+        diff = K - elem_list[i] - elem_list[j]
+        
+        if diff in mydict.keys():
             answer += mydict[diff]
 
 print(answer)
