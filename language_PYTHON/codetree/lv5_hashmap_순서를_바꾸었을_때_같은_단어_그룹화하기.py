8c38a458dd362a1ebdc14e8e8646c64aa43cd3c1
codekim97 <kimtaeu4616@gmail.com>
2023-07-13 10:29:34 +0900

    복습 횟수:1, 00:20:00, 복습필요X

--- language_PYTHON/codetree/lv5_hashmap_순서를_바꾸었을_때_같은_단어_그룹화하기.py
+++ language_PYTHON/codetree/lv5_hashmap_순서를_바꾸었을_때_같은_단어_그룹화하기.py
@@ -1,15 +1,17 @@
-# 복습 횟수:0, 01:00:00, 복습필요O
+# 2023-07-13
+# 복습 횟수:1, 00:20:00, 복습필요X
 import sys
 si = sys.stdin.readline
-
 N = int(si())
+
 mydict = dict()
 for i in range(N):
-    string_ = list(map(str, si().rstrip()))
-    sortedString = "".join(sorted(string_))
-    if sortedString not in mydict.keys():
-        mydict[sortedString] = 1
+    input = list(map(str, si().rstrip()))
+    key = "".join(sorted(input))
+    
+    if key not in mydict.keys():
+        mydict[key] = 1
     else:
-        mydict[sortedString] += 1
+        mydict[key] += 1
 
 print(max(mydict.values()))
