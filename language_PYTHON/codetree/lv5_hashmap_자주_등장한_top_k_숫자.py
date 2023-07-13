3453190b94925a95c5e48d37413ef92bbb1b0c0d
codekim97 <kimtaeu4616@gmail.com>
2023-07-13 11:05:22 +0900

    복습 횟수:1, 00:30:00, 복습필요X

--- language_PYTHON/codetree/lv5_hashmap_자주_등장한_top_k_숫자.py
+++ language_PYTHON/codetree/lv5_hashmap_자주_등장한_top_k_숫자.py
@@ -1,19 +1,18 @@
-# 복습 횟수:0, 01:00:00, 복습필요O
+# 2023-07-13
+# 복습 횟수:1, 00:30:00, 복습필요X
 import sys
-from collections import defaultdict
 si = sys.stdin.readline
+N, K = map(int, si().split())
+elem_list = list(map(int, si().split()))
 
-N, M = map(int, si().split())
-num_list = list(map(int, si().split()))
-mydict = defaultdict(int)
-
-for num in num_list:
-    if(num not in mydict.keys()):
-        mydict[num] = 1 
+mydict = dict()
+for elem in elem_list:
+    if elem not in mydict.keys():
+        mydict[elem] = 1
     else:
-        mydict[num] += 1
+        mydict[elem] += 1
 
-sorted_dict = sorted(mydict.items(), key = lambda x: [x[1], x[0]], reverse=True)
-candidate_dict = sorted_dict[:M]
-for i in range(M):
-    print(candidate_dict[i][0], end=' ')
+sortedDict = sorted(mydict.items(), key=lambda x: [-x[1], -x[0]])
+
+for i in range(K):
+    print(sortedDict[i][0], end = ' ')
