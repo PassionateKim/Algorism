ebb6e833bb4621a3e515a44d6266d15557d24661
codekim97 <kimtaeu4616@gmail.com>
2023-07-13 10:46:19 +0900

    복습 횟수:1, 00:30:00, 복습필요X

--- language_PYTHON/codetree/lv5_hashmap_원소의_합이_0.py
+++ language_PYTHON/codetree/lv5_hashmap_원소의_합이_0.py
@@ -1,29 +1,40 @@
-# 복습 횟수:0, 02:00:00, 복습필요O
+# 2023-07-13
+# 복습 횟수:1, 00:30:00, 복습필요X
 import sys
 si = sys.stdin.readline
-
 N = int(si())
-arr_list = []
-for i in range(4):
-    tmp = list(map(int, si().split()))
-    arr_list.append(tmp)
+A_list = list(map(int, si().split()))
+B_list = list(map(int, si().split()))
+C_list = list(map(int, si().split()))
+D_list = list(map(int, si().split()))
 
-mydict = dict()
-answer = 0
-for i in range(len(arr_list[0])):
-    for j in range(len(arr_list[1])):
-        sumi = arr_list[0][i] + arr_list[1][j]
-        if(sumi not in mydict.keys()):
-            mydict[sumi] = 1
+A_B_dict = dict()
+
+for i in range(len(A_list)):
+    for j in range(len(B_list)):
+        sumi = A_list[i] + B_list[j]
+        if sumi not in A_B_dict.keys():
+            A_B_dict[sumi] = 1
         else:
-            mydict[sumi] += 1
+            A_B_dict[sumi] += 1
+
+C_D_dict = dict()
 
 
-for i in range(len(arr_list[2])):
-    for j in range(len(arr_list[3])):
-        sumi = arr_list[2][i] + arr_list[3][j]
-        diff = 0 - sumi
-        if diff in mydict.keys():
-            answer += mydict[diff]
+for i in range(len(C_list)):
+    for j in range(len(D_list)):
+        sumi = C_list[i] + D_list[j]
+        if sumi not in C_D_dict.keys():
+            C_D_dict[sumi] = 1
+        else:
+            C_D_dict[sumi] += 1
+
+answer = 0
+
+for key, value in A_B_dict.items():
+    diff = 0 - key
+
+    if diff in C_D_dict.keys():
+        answer += (value * C_D_dict[diff])
 
 print(answer)
