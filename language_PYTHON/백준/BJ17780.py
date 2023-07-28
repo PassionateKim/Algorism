44d7c1971b9ae5e340fee728207158be7c7cd9ae
codekim97 <kimtaeu4616@gmail.com>
2023-07-28 09:48:54 +0900

    BJ17780, 복습횟수:2, 01:00:00, 복습필요X

--- language_PYTHON/백준/BJ17780.py
+++ language_PYTHON/백준/BJ17780.py
@@ -1,96 +1,101 @@
 # 새로운 게임
-# 복습 횟수:1, 00:45:00, 복습필요O
+# 복습 횟수:2, 01:00:00, 복습필요X
 import sys
-from collections import deque
-si = sys.stdin.readline
-N, K = map(int, si().split())
+si = sys.stdin.readline 
 
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-graph = [list(map(int, si().split())) for _ in range(N)]
+N, K = map(int, si().split()) # chess판 크기, 말의 개수
+graph_info = [list(map(int, si().split())) for i in range(N)]
 
-horse_graph = [[deque() for _ in range(N)] for _ in range(N)]
+horse_list = [[[] for i in range(N)] for i in range(N)]
 
 for i in range(K):
-    x, y, d = list(map(int, si().split()))
-    x, y, d = x - 1, y - 1, d - 1
-    horse_graph[x][y].append([i, d]) # 체스 번호, 방향
+    x, y, dir = map(int, si().split())
 
-def changeDir(dir):
-    if dir == 0:
-        return 1
+    horse_list[x-1][y-1].append([i+1, dir]) # 말번호, dir
+
+
+answer = 0
+
+
+def convertLocation(x, y, dir):
     if dir == 1:
-        return 0
+        return x, y+1
     if dir == 2:
+        return x, y-1
+    if dir == 3:
+        return x-1, y
+    else:
+        return x+1, y
+
+def convertDir(dir):
+    if dir == 1:
+        return 2
+    if dir == 2:
+        return 1
+    if dir == 3:
+        return 4
+    else:
         return 3
     
-    return 2
-answer = 0
-def check():
-    global answer
-    while True:
-        if answer > 1000:
-            answer = -1
-            return
-        
-        for i in range(N):
-            for j in range(N):
-                if len(horse_graph[i][j]) >= 4:
+def check_horse(horse_number):
+    for x in range(N):
+        for y in range(N):
+            if not horse_list[x][y]: continue
+
+            bottom_horse = horse_list[x][y][0]
+            if bottom_horse[0] == horse_number: # 아래에 있다면
+                dir = bottom_horse[1]
+                nx, ny = convertLocation(x, y, dir)
+                # 흰, 빨, 파, 밖 구분
+                if (not (0<= nx< N and 0 <= ny < N)) or graph_info[nx][ny] == 2: # 밖이거나 파란색인 경우
+                    converted_dir = convertDir(dir)
+                    bottom_horse[1] = converted_dir
+                    # logic 다시 수행
+                    nx, ny = convertLocation(x, y, converted_dir)
+                    if (not (0<= nx< N and 0 <= ny < N)) or graph_info[nx][ny] == 2:
+                        return
+
+                    elif graph_info[nx][ny] == 0: # 흰색인 경우
+                        horse_list[nx][ny].extend(horse_list[x][y])
+                        horse_list[x][y] = []
+                        return
+                    
+                    elif graph_info[nx][ny] == 1: # 빨간색인 경우
+                        horse_list[x][y].reverse()
+                        horse_list[nx][ny].extend(horse_list[x][y])
+                        horse_list[x][y] = []
+                        return
+                    
+                elif graph_info[nx][ny] == 0: # 흰색인 경우
+                        horse_list[nx][ny].extend(horse_list[x][y])
+                        horse_list[x][y] = []
+                        return
+                
+                elif graph_info[nx][ny] == 1: # 빨간색인 경우
+                    horse_list[x][y].reverse()
+                    horse_list[nx][ny].extend(horse_list[x][y])
+                    horse_list[x][y] = []
                     return
 
-        answer += 1
-        
+while True:
+    # 탈출 조건
+    if answer > 1000:
+        print(-1)
+        break
 
-        # 체스 번호    
-        for chess in range(K):
-            flag = 0
-            for x in range(N):
-                for y in range(N):
-                    horse_list = horse_graph[x][y]
-                    # 맨 아래에 있는 경우에만 움직일 수 있다.
-                    if len(horse_list) != 0 and horse_list[0][0] == chess:
-                        dir = horse_list[0][1]
-                        nx, ny = x + dx[dir], y + dy[dir]
-                        # 파란색 or 밖
-                        if not (0 <= nx < N and 0 <= ny < N) or graph[nx][ny] == 2:
-                            horse_list[0][1] = changeDir(horse_list[0][1])
-                            dir = horse_list[0][1]
-                            nx, ny = x + dx[dir], y + dy[dir]
-                            # 파란색인 경우
-                            if not (0 <= nx < N and 0 <= ny < N) or graph[nx][ny] == 2:
-                                flag = 1
-                                break 
-                            # 빨간색인 경우
-                            if graph[nx][ny] == 1:
-                                while horse_graph[x][y]:
-                                    horse = horse_graph[x][y].pop()
-                                    horse_graph[nx][ny].append(horse)
-                                flag = 1
-                                break
-                            # 흰색인 경우
-                            if graph[nx][ny] == 0:
-                                while horse_graph[x][y]:
-                                    horse = horse_graph[x][y].popleft()
-                                    horse_graph[nx][ny].append(horse)
-                                flag = 1
-                                break
+    flag = False
+    for i in range(N):
+        for j in range(N):
+            if len(horse_list[i][j]) >= 4:
+                print(answer)
+                flag = True
 
-                        # 빨간색인 경우
-                        if graph[nx][ny] == 1:
-                            while horse_graph[x][y]:
-                                horse = horse_graph[x][y].pop()
-                                horse_graph[nx][ny].append(horse)
-                            flag = 1
-                            break
-                        # 흰색인 경우
-                        if graph[nx][ny] == 0:
-                            while horse_graph[x][y]:
-                                horse = horse_graph[x][y].popleft()
-                                horse_graph[nx][ny].append(horse)
-                            flag = 1
-                            break
-                if flag:
-                    break
+    if flag:
+        break
+    
+    for horse_number in range(1, K+1):
+        # 있는지 체크
+        flag = False
+        check_horse(horse_number)
 
-check()
-print(answer)
+    answer += 1
