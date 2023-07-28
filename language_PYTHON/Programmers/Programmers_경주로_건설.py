cf560e520696a931c860868c1d4f23b6ac65c1e7
codekim97 <kimtaeu4616@gmail.com>
2023-07-28 13:46:44 +0900

    복습횟수:3, 00:30:00, 복습필요X

--- language_PYTHON/Programmers/Programmers_경주로_건설.py
+++ language_PYTHON/Programmers/Programmers_경주로_건설.py
@@ -1,44 +1,81 @@
-# 복습 횟수:2, 00:30:00, 복습필요O
-
-from collections import deque
-import math
-# 상하좌우
-dx = [-1, 1, 0, 0]
+# 복습 횟수:3, 00:30:00, 복습필요X
+dx = [-1, 1, 0, 0] # 상 하 좌 우 
 dy = [0, 0, -1, 1]
-
+import sys
+from collections import deque
 def solution(board):
-    
-    N = len(board)
-    M = len(board[0])
+    inf = sys.maxsize
+    candidate = []
+    # 가로 시작
+    visited = [[inf for i in range(len(board[0]))] for i in range(len(board))]
+    q = deque()
+    q.append([0, 0, 0, 1]) # x y price dir
+    while q:
+        x, y, price, dir = q.popleft()
 
-    def bfs(x, y, dir, price):
-        visited = [[math.inf for _ in range(M)] for __ in range(N)]
-        visited[x][y] = 0 # 처음부분은 0으로 초기화
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if not (0 <= nx < len(board) and 0 <= ny < len(board[0])): continue
+            if board[nx][ny] == 1: continue
 
-        q = deque()
-        q.append([x, y, dir, price])
-
-        while q:
-            x, y, dir, price = q.popleft()
-        
-            # new_price가 더 작을 때만 갈 수 있도록 하기
-            for idx in range(4):
-                nx, ny = x + dx[idx], y + dy[idx]
-
-                if not (0 <= nx < N and 0 <= ny < M): continue # 범위밖 X
-                if board[nx][ny] == 1: continue # 벽이기 때문에 갈 수 없으므로
-                # 방향이 같은 경우
-                if idx == dir:
+            if dir == 1: # 가로인 경우
+                if i in [0, 1]:
+                    new_price = price + 500 + 100
+                    new_dir = -1
+                else:
                     new_price = price + 100
-                else: # 방향이 달라서 꺾어야 하는 경우
-                    new_price = price + 600
-                
-                if new_price < visited[nx][ny]: # 가격이 더 작을 때에만
-                    q.append([nx, ny, idx, new_price])
-                    visited[nx][ny] = new_price
+                    new_dir = 1 
+            else: # 세로인 경우
+                if i in [0, 1]:
+                    new_price = price + 100
+                    new_dir = -1
+                else:
+                    new_price = price + 500 + 100
+                    new_dir = 1
+            
 
-        return visited[-1][-1]
-
-    return min(bfs(0, 0, 1, 0), bfs(0, 0, 3, 0))
+            if new_price <= visited[nx][ny]:
+                q.append([nx, ny, new_price, new_dir])
+                visited[nx][ny] = new_price
     
-print(solution([[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]]))
+    candidate.append(visited[len(board)-1][len(board[0])-1])
+
+
+    # 세로 시작
+    visited = [[inf for i in range(len(board[0]))] for i in range(len(board))]
+    q = deque()
+    q.append([0, 0, 0, -1])
+    while q:
+        x, y, price, dir = q.popleft()
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if not (0 <= nx < len(board) and 0 <= ny < len(board[0])): continue
+            if board[nx][ny] == 1: continue
+
+            if dir == 1: # 가로인 경우
+                if i in [0, 1]:
+                    new_price = price + 500 + 100
+                    new_dir = -1
+                else:
+                    new_price = price + 100
+                    new_dir = 1 
+            else: # 세로인 경우
+                if i in [0, 1]:
+                    new_price = price + 100
+                    new_dir = -1
+                else:
+                    new_price = price + 500 + 100
+                    new_dir = 1
+            
+
+            if new_price <= visited[nx][ny]:
+                q.append([nx, ny, new_price, new_dir])
+                visited[nx][ny] = new_price
+
+    candidate.append(visited[len(board)-1][len(board[0])-1])
+
+    return min(candidate)
+
+
+print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]))
