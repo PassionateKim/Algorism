99412b0e6cc7addb81a46ee241d0d36c5701f1fa
codekim97 <kimtaeu4616@gmail.com>
2023-07-28 13:06:48 +0900

    BJ15591, 복습횟수:2, 01:00:00, 복습필요3

--- language_PYTHON/백준/BJ15591.py
+++ language_PYTHON/백준/BJ15591.py
@@ -1,39 +1,37 @@
 # MooTube (Silver)
-# 복습 횟수:1, 01:00:00, 복습필요O
+# 복습 횟수:2, 01:00:00, 복습필요3
 import sys
 from collections import deque
-N, Q = map(int, sys.stdin.readline().split())
-si = sys.stdin.readline
+si = sys.stdin.readline 
 
-def bfs():
-    q = deque()
-    visited[v] = 1 # 방문처리
-    for nv, nk in graph[v]:
-        q.append([nv, nk])
-        visited[nv] = 1 # 방문처리
-    
-    result = 0
-    while q:
-        ov, ok = q.popleft()
-        if ok >= k:
-            result += 1
-
-        for nv, nk in graph[ov]:
-            if visited[nv] == 1: continue
-
-            q.append([nv, min(nk, ok)])
-            visited[nv] = 1 # 방문처리
-
-    return result
+N, Q = map(int, si().split())
 
 graph = [[] for i in range(N+1)]
 
 for i in range(N-1):
-    p, q, r = map(int, si().split())
-    graph[p].append([q, r])
-    graph[q].append([p, r])
+    x, y, usado = map(int, si().split())
+    graph[x].append([y, usado])
+    graph[y].append([x, usado])
+
+
+def bfs(k, start):
+    visited = [0 for i in range(N+1)]
+    visited[start] = 1 # 방문 처리
+    q = deque()
+    q.append([start, 1e11])
+    answer = 0
+    while q:
+        start, current_usado = q.popleft()
+        if current_usado >= k:
+            answer += 1
+
+        for target, target_usado in graph[start]:
+            if visited[target] == 0:
+                q.append([target, min(target_usado, current_usado)])
+                visited[target] = 1 # 방문처리
+
+    return answer - 1
 
 for i in range(Q):
-    visited = [0 for i in range(N+1)]
     k, v = map(int, si().split())
-    print(bfs())
+    print(bfs(k, v))
