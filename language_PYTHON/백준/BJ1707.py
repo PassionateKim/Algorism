1b153f9c0f5e41f5a2ecf8b8f3b596488f6879ab
codekim97 <kimtaeu4616@gmail.com>
2023-08-04 09:57:01 +0900

    BJ1707, 복습횟수:2, 00:30:00, 복습필요X

--- language_PYTHON/백준/BJ1707.py
+++ language_PYTHON/백준/BJ1707.py
@@ -1,35 +1,44 @@
 # 이분 그래프
-# 복습 횟수:1, 00:30:00, 복습필요O
+# 복습 횟수:2, 00:30:00, 복습필요X
+
 import sys
 from collections import deque
-si = sys.stdin.readline
-
+si = sys.stdin.readline 
 K = int(si())
-
-for i in range(K):
-    V,E = map(int, si().split())
-    graph = [[] for _ in range(V+1)] #빈 그래프 생성
-    visited = [0] * (V+1)   #방문한 정점 체크
-    
-    #그래프 값 넣기
-    for i in range(E):
-        x,y = map(int,si().split())
-        graph[x].append(y) #무방향 그래프
-        graph[y].append(x) #무방향 그래프
+def bfs(k, graph):
+    global flag
 
     q = deque()
-    group = 1
-    bipatite = True
-    for i in range(1,V+1):
-        if visited[i] == 0: #방문하지 않은 정점이면 BFS 수행
-            q.append(i)
-            visited[i] = group
-            while q:
-                v = q.popleft()
-                for w in graph[v]:
-                    if visited[w] == 0: #방문하지 않은 정점이면 큐에 삽입 
-                        q.append(w)
-                        visited[w] = -1 * visited[v]
-                    elif visited[w] == visited[v]:
-                        bipatite = False
-    print("YES" if bipatite else "NO")   
+    q.append(k)
+    visited[k] = 1
+    while q:
+        start = q.popleft()
+        for end in graph[start]:
+            if visited[end] == 0:
+                q.append(end)
+                visited[end] = -visited[start]
+            if visited[end] != 0:
+                # check
+                if visited[start] == visited[end]:
+                    flag = False
+                    return 
+
+for i in range(K):
+    V, E = map(int, si().split())
+    graph = [[] for i in range(V+1)]
+    # graph 초기화
+    for j in range(E):
+        x, y = map(int, si().split())
+        graph[x].append(y)
+        graph[y].append(x)
+        
+    visited = [0 for i in range(V+1)]
+    flag = True
+    for k in range(1, V+1):
+        if visited[k] == 0:
+            bfs(k, graph)
+
+    if flag:
+        print("YES")
+    else:
+        print("NO")
