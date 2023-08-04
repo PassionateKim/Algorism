e1117cf490cd40ddc0418e690197df974fbceef9
codekim97 <kimtaeu4616@gmail.com>
2023-08-04 09:31:05 +0900

    BJ13164,복습횟수:1, 01:00:00, 복습필요3

--- language_PYTHON/백준/BJ13164.py
+++ language_PYTHON/백준/BJ13164.py
@@ -1,14 +1,14 @@
 # 행복 유치원
-# 복습 횟수:0, 01:00:00
+# 복습 횟수:1, 01:00:00, 복습필요3
 import sys
 si = sys.stdin.readline
 N, K = map(int, si().split())
-people_list = list(map(int, si().split()))
+arr = list(map(int, si().split()))
 
 diff_list = []
 
 for i in range(1, N):
-    diff = people_list[i] - people_list[i-1]
+    diff = arr[i] - arr[i-1]
     diff_list.append(diff)
 
 diff_list.sort()
