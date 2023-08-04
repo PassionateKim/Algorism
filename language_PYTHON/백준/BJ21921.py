8d93aab955fd1755b2caf5bf477c569fb35441f3
codekim97 <kimtaeu4616@gmail.com>
2023-08-04 12:59:06 +0900

    BJ21921, 복습횟수:1, 00:30:00, 복습필요3

--- language_PYTHON/백준/BJ21921.py
+++ language_PYTHON/백준/BJ21921.py
@@ -1,29 +1,33 @@
 # 블로그
-# 복습 횟수:0, 00:30:00, 복습필요O
-n, x = map(int, input().split())  # 블로그 일수, x일 동안 가장 많이 들어온 방문자
-arr = list(map(int, input().split()))
+# 복습 횟수:1, 00:30:00, 복습필요O
+import sys
+si = sys.stdin.readline 
 
+N, X = map(int, si().split())
+answer = 0
+arr = list(map(int, si().split()))
+sum_list = list()
 if max(arr) == 0:
     print("SAD")
 else:
-    # prefix 만들기
-    prefix = [0]
-    for i, a in enumerate(arr):
-        prefix.append(prefix[i] + a)
+    candidate_sum = sum(arr[0:X])
 
-    left = 0
-    right = x
-    result = 0
-    count = 0
+    sum_list.append(candidate_sum)
 
-    while right <= n:
-        if result < prefix[right] - prefix[left]:
-            result = prefix[right] - prefix[left]
-            count = 1
-        elif result == prefix[right] - prefix[left]:
-            count += 1
-        left += 1
-        right += 1
+    answer = max(answer, candidate_sum)
 
-    print(result)
-    print(count)
+    minus_index = 0
+    plus_index = X
+    while plus_index != len(arr):
+        candidate_sum = candidate_sum - arr[minus_index]
+        candidate_sum = candidate_sum + arr[plus_index]
+
+        answer = max(answer, candidate_sum)
+        sum_list.append(candidate_sum)
+
+        minus_index += 1
+        plus_index += 1
+
+
+    print(answer)
+    print(sum_list.count(answer))
