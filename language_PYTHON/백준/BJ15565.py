6cec36d5b1f32a27ec231791464a30b485c294b3
codekim97 <kimtaeu4616@gmail.com>
2023-08-07 10:51:47 +0900

    BJ15565, 복습횟수:2, 00:30:00, 복습필요X

--- language_PYTHON/백준/BJ15565.py
+++ language_PYTHON/백준/BJ15565.py
@@ -1,28 +1,40 @@
 # 귀여운 라이언
-# 복습 횟수:1, 01:00:00, 복습필요O
+# 복습 횟수:2, 00:30:00, 복습필요X
 import sys
-si = sys.stdin.readline
-N, K = map(int, si().split())
-li = list(map(int, si().split())) # 라이언 1, 어피치 2
+si = sys.stdin.readline 
 
-answer = sys.maxsize
-check = 0
+N, K = map(int, si().split())
+arr = list(map(int, si().split()))
+
+result = int(1e9)
+count = 0
+
 left = 0
 right = 0
 
-while left <= len(li) - 1 and right <= len(li) - 1:
-    if li[right] == 1:
-        check += 1
+one = 0
+answer = sys.maxsize
+
+if arr[left] == 1:
+    one += 1
+
+while left < len(arr) and right < len(arr):
+    # 라이언 인형의 개수가 부족하면 오른쪽으로 포인터
+    if one < K:
+        right += 1
+        if right < len(arr) and arr[right] == 1:
+            one += 1
     
-    if check == K:
-        while left <= len(li)-1 and check == K:
-            answer = min(answer, (right - left) + 1) 
-            if li[left] == 1:
-                check -= 1
-            left += 1
-    
-    right += 1
+    else:
+        if one == K:
+            answer = min(answer, right - left + 1)
+        
+        if left < len(arr) and arr[left] == 1:
+            one -= 1
+        
+        left += 1
 
 if answer == sys.maxsize:
     print(-1)
-else: print(answer)
+else:
+    print(answer)
