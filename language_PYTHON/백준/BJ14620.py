d825fed12447d1c26e5f51fec9f04283e2f45638
codekim97 <kimtaeu4616@gmail.com>
2023-08-08 19:34:36 +0900

    BJ14620, 복습 횟수1, 01:00:00, 복습필요:3

--- language_PYTHON/백준/BJ14620.py
+++ language_PYTHON/백준/BJ14620.py
@@ -1,75 +1,81 @@
 # 꽃길
-# 복습 횟수:0, 01:00:00, 복습필요O
+# 복습 횟수:1, 01:00:00, 복습필요O
 import sys
-from collections import deque
-si = sys.stdin.readline
+si = sys.stdin.readline 
 N = int(si())
 graph = []
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 for i in range(N):
     tmp = list(map(int, si().split()))
     graph.append(tmp)
-answer = sys.maxsize
 
-# 완전 탐색
-for i in range(1, N-1):
-    for j in range(1, N-1):
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+answer = sys.maxsize
+# 가능한 순서쌍
+case_list = list()
+
+
+for x1 in range(1, N - 1):
+    for y1 in range(1, N - 1):
         visited = [[0 for i in range(N)] for i in range(N)]
-        # 첫번째 꽃
-        visited[i][j] = 1 # 방문처리
+         
+        visited[x1][y1] = 1 # 방문 처리
         for idx in range(4):
-            ni, nj = i + dx[idx], j + dy[idx]
-            visited[ni][nj] = 1 # 방문처리
-        
-        # 두번째 꽃
-        for k in range(1, N-1):
-            for l in range(1, N-1):
-                q = deque()
-                q.append([k, l])
+            nx, ny = x1 + dx[idx], y1 + dy[idx]
+            visited[nx][ny] = 1 # 방문처리
+                    
+        for x2 in range(1, N - 1):
+            for y2 in range(1, N - 1):
+                q = list()
+                q.append([x2, y2])
+
                 for idx in range(4):
-                    nk, nl = k + dx[idx], l + dy[idx]
-                    q.append([nk, nl])
-                
-                tmp = 0
+                    nx, ny = x2 + dx[idx], y2 + dy[idx]
+                    q.append([nx, ny])
+
+                flag = False
                 for x, y in q:
                     if visited[x][y] == 1:
-                        tmp += 1
-                if tmp > 0: continue
+                        flag = True
                 
-                for x, y in q:
-                    visited[x][y] = 1 
+                if flag: continue
 
-                # 세번째 꽃
-                for x in range(1, N-1):
-                    for y in range(1, N-1):
-                        q2 = deque()
-                        q2.append([x, y])
+
+                for x, y in q:
+                    visited[x][y] = 1 # 방문처리
+
+                for x3 in range(1, N - 1):
+                    for y3 in range(1, N - 1):
+                        q2 = list()
+
+                        q2.append([x3, y3])
+
                         for idx in range(4):
-                            nx, ny = x + dx[idx], y + dy[idx]
+                            nx, ny = x3 + dx[idx], y3 + dy[idx]
                             q2.append([nx, ny])
                         
-                        tmp = 0
+                        flag = False
                         for x, y in q2:
                             if visited[x][y] == 1:
-                                tmp += 1
-                        if tmp > 0: continue
+                                flag = True
                         
+                        if flag: continue
+
                         for x, y in q2:
                             visited[x][y] = 1 # 방문처리
 
-                        # 여기까지 코드가 도착하는 경우 겹치지 않는 것임
-                        sumi = 0
-                        for xx in range(N):
-                            for yy in range(N):
-                                if visited[xx][yy] == 1:
-                                    sumi += graph[xx][yy]
+                        candidate_sum = 0
+                        for x in range(N):
+                            for y in range(N):
+                                if visited[x][y] == 1:
+                                    candidate_sum += graph[x][y]
                         
-                        answer = min(answer, sumi)
+                        answer = min(answer, candidate_sum)
 
                         for x, y in q2:
                             visited[x][y] = 0 # 원상복구
-                # 원상복구
                 for x, y in q:
-                    visited[x][y] = 0 
+                    visited[x][y] = 0        
+
 print(answer)
