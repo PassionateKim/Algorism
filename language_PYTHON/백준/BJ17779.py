ec9819d53f315d0a3ff8be4fee231ca0ea324463
codekim97 <kimtaeu4616@gmail.com>
2023-08-10 16:16:44 +0900

    BJ17779, 복습 횟수:1, 01:30:00, 복습필요X

--- language_PYTHON/백준/BJ17779.py
+++ language_PYTHON/백준/BJ17779.py
@@ -1,91 +1,175 @@
 # 게리맨더링 2
-# 복습 횟수:0, 02:30:00, 복습필요O
+# 복습 횟수:1, 01:30:00, 복습필요X
 import sys
-si = sys.stdin.readline
+si = sys.stdin.readline 
 N = int(si())
 graph = []
+
 for i in range(N):
-    graph.append(list(map(int, si().split())))
+    tmp = list(map(int, si().split()))
+    graph.append(tmp)
 
+# x, y 에따라 가능한 d1, d2의 조합 모두 체크
 case_list = []
-answer = sys.maxsize
-# case 구하기
-for d1 in range(1, N-1):
-    for d2 in range(1, N-1):
-        if d1+d2 >= N: continue
-        for x in range(1, N - (d1+d2) + 1):
-            x = x -1 # index 0부터 시작이므로
-            for y in range(1+d1, N - d2 + 1):
-                y = y - 1 # index 0부터 시작이므로
-                case_list.append([d1, d2, x, y])
+for d1 in range(1, N):
+    for d2 in range(1, N):
+        for x in range(1,  N - (d1 + d2)+ 1):
+            for y in range(1 + d1, N - d2 + 1):
+                case_list.append([x-1, y-1, d1, d2])
 
-for d1, d2, x, y in case_list:
-    visited = [[0 for i in range(N)] for i in range(N)]
 
-    # 경계선 먼저 지정하기
-    # 1.
-    j = y
-    for i in range(x, x+d1+1):
-        visited[i][j] = 5
-        j = j - 1
-    # 2.
-    j = y
-    for i in range(x, x+d2+1):
-        visited[i][j] = 5
-        j = j + 1
-    # 3.
-    j = y-d1
-    for i in range(x+d1, x+d1+d2+1):
-        visited[i][j] = 5
-        j = j + 1 
-    # 4.
-    j = y+d2
-    for i in range(x+d2, x+d2+d1+1):
-        visited[i][j] = 5
-        j = j - 1
-    
-    # 구역 나누기
-    # 1번 선거구
-    for r in range(0, x+d1):
-        for c in range(0, y+1):
-            if visited[r][c] == 5: break
+def init_one():
+    for i in range(x+d1):
+        for j in range(N):
 
-            visited[r][c] = 1
-    # 2번 선거구
-    for r in range(0, x+d2+1):
-        for c in range(N-1, y, -1):
-            if visited[r][c] == 5: break
+            if visited[i][j] == 1 or visited[i][j] == 5:
+                break
 
-            visited[r][c] = 2
-    # 3번 선거구
-    for r in range(x+d1, N):
-        for c in range(y-d1+d2):
-            if visited[r][c] == 5: break
+            visited[i][j] = 1
 
-            visited[r][c] = 3 
-    # 4번 선거구
-    for r in range(x+d2+1, N):
-        for c in range(N-1, y-d1+d2-1, -1):
-            if visited[r][c] == 5: break
+    return
 
-            visited[r][c] = 4
+def init_two():
+    for i in range(x+d2):
+        for j in range(N-1, -1, -1):
 
-    sumi1, sumi2, sumi3, sumi4, sumi5 = 0, 0, 0, 0, 0
+            if visited[i][j] == 1 or visited[i][j] == 5: break
+
+            visited[i][j] = 2
+    return
+
+def init_three():
+    for i in range(N-1, x + d1, -1):
+        for j in range(N):
+
+            if visited[i][j] == 4 or visited[i][j] == 5: break
+
+            visited[i][j] = 3
+
+    return
+
+def init_four():
+    for i in range(N-1, x + d2, -1):
+        for j in range(N-1, -1, -1):
+
+            if visited[i][j] == 4 or visited[i][j] == 5: break
+
+            visited[i][j] = 4
+    return
+
+def init_five():
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] == 0:
+                visited[i][j] = 5
+    return
+
+def check_sum():
+    sum_list = [0, 0, 0, 0, 0]
+
     for i in range(N):
         for j in range(N):
             if visited[i][j] == 1:
-                sumi1 += graph[i][j]
-            elif visited[i][j] == 2:
-                sumi2 += graph[i][j]
-            elif visited[i][j] == 3:
-                sumi3 += graph[i][j]
-            elif visited[i][j] == 4:
-                sumi4 += graph[i][j]
-            else: 
-                sumi5 += graph[i][j]
-    maxi = max(sumi1, sumi2, sumi3, sumi4, sumi5)
-    mini = min(sumi1, sumi2, sumi3, sumi4, sumi5)
+                sum_list[0] += graph[i][j]
+            
+            if visited[i][j] == 2:
+                sum_list[1] += graph[i][j]
+            
+            if visited[i][j] == 3:
+                sum_list[2] += graph[i][j]
 
-    answer = min(answer, maxi - mini)
+            if visited[i][j] == 4:
+                sum_list[3] += graph[i][j]
+            
+            if visited[i][j] == 5:
+                sum_list[4] += graph[i][j]
+
+    return sum_list
+
+answer = sys.maxsize
+# 경계선 기준 상하 좌우로 쭉 1, 2, 3, 4 해놓고
+for x, y, d1, d2 in case_list:
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    
+    for i in range(d1 + 1):
+        nx, ny = x + i, y - i
+        if not (0 <= nx < N and 0 <= ny < N): continue
+
+        visited[nx][ny] = 5
+
+    for i in range(d2 + 1):
+        nx, ny = x + i, y + i
+        if not (0 <= nx < N and 0 <= ny < N): continue
+
+        visited[nx][ny] = 5
+
+
+    for i in range(d2 + 1):
+        nx, ny = x + d1+ i, y - d1 + i
+        if not (0 <= nx < N and 0 <= ny < N): continue
+
+        visited[nx][ny] = 5
+    
+    for i in range(d1 + 1):
+        nx, ny = x + d2 +i, y + d2 - i
+        if not ( 0 <= nx < N and 0 <= ny < N): continue
+
+        visited[nx][ny] = 5
+
+    cnt = 1
+    while True:
+        nx, ny = x - cnt, y
+        if not (0 <= nx < N and 0 <= ny < N): break
+
+        visited[nx][ny] = 1
+
+        cnt += 1
+
+    cnt = 1
+    while True:
+        nx, ny = x  + d2, y + d2 + cnt
+        if not (0 <= nx < N and 0 <= ny < N): break
+
+        visited[nx][ny] = 2       
+        cnt += 1
+
+
+
+    cnt = -1
+    while True:
+        nx, ny = x  + d1, y - d1 + cnt
+        if not (0 <= nx < N and 0 <= ny < N): break
+
+        visited[nx][ny] = 3
+        cnt -= 1
+
+
+    cnt = 1
+    while True:
+        nx, ny = x + d1 + d2 + cnt, y - d1 + d2
+        if not (0 <= nx < N and 0 <= ny < N): break
+
+        visited[nx][ny] = 4       
+        cnt += 1
+
+
+    # 1은 왼쪽부터 쭉 오른쪽으로는 1, 5가 나올 때, 아래로는 3이 나올 때
+    init_one()
+    # 2는 오른쪽부터 쭉 왼쪽으로는 1, 5가 나올 때, 아래로는 2가 나올 때
+    init_two()
+    # 3은 왼쪽부터 쭉 오른쪽으로는 4, 5가 나올 때, 위로는 3이 나올 때
+    init_three()
+    # 4는 오른쪽부터 쭉 왼쪽으로는 4, 5가 나올 때 위로는 4가 나올 때
+    init_four()
+    # 0인 곳은 5로 채운다
+    init_five()
+
+    # 1, 2, 3, 4, 5의 sum을 체운다
+    sum_list = check_sum()
+    
+    # 가장 많은 곳과 가장 적은 곳의 인구 차이의 최솟값을 구한다.
+    diff = max(sum_list) - min(sum_list)
+
+    answer = min(answer, diff)
 
 print(answer)
