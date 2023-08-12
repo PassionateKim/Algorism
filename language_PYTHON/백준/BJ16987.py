d1fd76da83791bd203576f52cb60f17477fb8e30
codekim97 <kimtaeu4616@gmail.com>
2023-08-12 17:15:21 +0900

    BJ16987, 복습횟수2, 00:45:00, 복습필요3

--- language_PYTHON/백준/BJ16987.py
+++ language_PYTHON/백준/BJ16987.py
@@ -1,41 +1,53 @@
 # 계란으로 계란치기
-# 복습 횟수:1, 00:45:00, 복습필요O
+# 복습 횟수:2, 00:45:00, 복습필요2
 import sys
 si = sys.stdin.readline
 N = int(si())
-eggs = [list(map(int, si().split())) for i in range(N)]
-answer = 0
+egg_list = []
 
-def dfs(index, eggs):
+for i in range(N):
+    shield, weight = map(int, si().split())
+    egg_list.append([shield, weight])
+
+answer = 0
+def dfs(egg_index):
     global answer
-    if index == len(eggs):
-        cnt = 0
-    
-        for egg in eggs:
-            if egg[0] <= 0:
-                cnt += 1
+    if egg_index == len(egg_list):
+        candidate = 0
         
-        answer = max(answer, cnt)
+        for shield, weight in egg_list:
+            if shield <= 0:
+                candidate += 1
+        
+        answer = max(answer, candidate)
         return
 
-    if eggs[index][0] <= 0:
-        dfs(index+1, eggs)
+    current_egg_shield, current_egg_weight = egg_list[egg_index]
+    if current_egg_shield <= 0: # 손에 든 계란이 꺠진 경우
+        dfs(egg_index + 1) # 그냥 넘어간다
     else:
         is_all_broken = True
-        for i in range(len(eggs)):
-            if i != index and eggs[i][0] > 0:
-                is_all_broken = False
-                eggs[index][0] -= eggs[i][1]
-                eggs[i][0] -= eggs[index][1]
+        for i in range(len(egg_list)):
+            if i == egg_index or egg_list[i][0] <= 0: continue # 같은 것끼리 깰 순 없으므로
+            
+            is_all_broken = False
 
-                dfs(index+1, eggs)
-                
-                eggs[index][0] += eggs[i][1]
-                eggs[i][0] += eggs[index][1]
-                
+            target_egg_shield, target_egg_weight = egg_list[i]
+            # if target_egg_shield <= 0: # target이 깨진 경우
+                # dfs(egg_index + 1) # 그냥 넘어간다.
+                # 서로 꺤다
+            egg_list[egg_index][0] -= target_egg_weight
+            egg_list[i][0] -= current_egg_weight
+            
+            dfs(egg_index + 1)
+
+            # reset
+            egg_list[egg_index][0] += target_egg_weight
+            egg_list[i][0] += current_egg_weight
+
         if is_all_broken:
-            dfs(len(eggs), eggs)
+            dfs(len(egg_list))    
+    return
 
-
-dfs(0, eggs)
+dfs(0)
 print(answer)
