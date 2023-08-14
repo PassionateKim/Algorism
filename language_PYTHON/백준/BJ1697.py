7bd9bd61942ff30022a71fe38745bfae226a5b5f
codekim97 <kimtaeu4616@gmail.com>
2023-08-14 18:37:17 +0900

    BJ1697, 복습횟수:1, 00:30:00, 복습필요:2

--- language_PYTHON/백준/BJ1697.py
+++ language_PYTHON/백준/BJ1697.py
@@ -1,22 +1,37 @@
 # 숨바꼭질
-# 복습 횟수:0, 00:30:00, 복습필요O
+# 복습 횟수:1, 00:30:00, 복습필요2
 import sys
 from collections import deque
-si = sys.stdin.readline
-N, K = map(int, si().split())
-check = set()
-def bfs():
-    q = deque()
-    q.append([N, 0])
-    check.add(N)
-    while q:
-        location, cnt = q.popleft()
-        if location == K:
-            return cnt
-        # 2배
-        for i in (location -1 ,location +1, location*2):
-            if 0 <= i <= 100000 and not i in check:
-                q.append([i, cnt + 1])
-                check.add(i)
 
-print(bfs())
+si = sys.stdin.readline 
+N, K = map(int, si().split())
+def bfs(start):
+    visited = [0 for i in range(100001)]
+    visited[start] = 1 # 방문처리
+    q = deque()
+    q.append([start, 0])
+
+    while q:
+        start, time = q.popleft()
+        if start == K:
+            print(time)
+            break
+        
+        if 0 <= start - 1 <= 100000:
+            if visited[start - 1] == 0:
+                q.append([start -1, time + 1])
+                visited[start - 1] = 1 # 방문 처리
+        
+        if 0 <= start + 1 <= 100000:
+            if visited[start + 1] == 0:
+                q.append([start + 1, time + 1]) 
+                visited[start + 1] = 1 # 방문처리
+
+        if 0 <= start * 2 <= 100000:
+            if visited[start * 2] == 0:
+                q.append([start*2, time + 1])
+                visited[start * 2] = 1 # 방문처리
+
+    return
+
+bfs(N)
