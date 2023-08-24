2ce63bebfb676ffe614e82a9e6763559e86d31f1
codekim97 <kimtaeu4616@gmail.com>
2023-08-24 13:33:19 +0900

    BJ10971, 복습횟수:1, 00:45:00, 복습필요X

--- language_PYTHON/백준/BJ10971.py
+++ language_PYTHON/백준/BJ10971.py
@@ -1,33 +1,39 @@
 # 외판원 순회 2
-# 복습 횟수:0, 00:45:00, 복습필요O
+# 복습 횟수:1, 00:45:00, 복습필요X
 import sys
-si = sys.stdin.readline
+si = sys.stdin.readline 
 N = int(si())
-graph = [list(map(int, si().split())) for i in range(N)]
+
+graph = []
+for i in range(N):
+    tmp = list(map(int, si().split()))
+    graph.append(tmp)
+
 visited = [0 for i in range(N)]
 answer = sys.maxsize
-def dfs(first, start, count, sumi):
+
+def dfs(sumi, current):
     global answer
-    if count == N-1: 
-        if graph[start][first] == 0: return
 
-        answer = min(answer, sumi + graph[start][first])
-        
+    if visited.count(0) == 0: # 모두 돌았을 때
+        if graph[current][start]:
+            sumi = sumi + graph[current][start]
+            answer = min(answer, sumi)
         return
-    
-    for i in range(N):
-        if visited[i]: continue
-        if graph[start][i] == 0: continue
 
-        visited[i] = 1
-        tmp = graph[start][i]
-        dfs(first, i, count + 1, sumi + tmp)
-        visited[i] = 0
+    for index, next in enumerate(graph[current]):
+        if visited[index] == 1: continue
+        if next == 0: continue
+
+        visited[index] = 1 
+        dfs(sumi + graph[current][index], index)
+        visited[index] = 0
+
     return
 
-for i in range(N):
-    visited[i] = 1
-    dfs(i, i, 0, 0)
-    visited[i] = 0
+for start in range(N):
+    visited[start] = 1 # 방문처리
+    dfs(0, start)
+    visited[start] = 0 # 초기화
 
 print(answer)
