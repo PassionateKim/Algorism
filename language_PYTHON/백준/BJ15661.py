a7e2d0053a4638ebb91e2aee791c1d3c85a906e2
codekim97 <kimtaeu4616@gmail.com>
2023-08-25 13:44:51 +0900

    BJ15661, 복습횟수:1, 01:00:00, 복습필요X

--- language_PYTHON/백준/BJ15661.py
+++ language_PYTHON/백준/BJ15661.py
@@ -1,39 +1,62 @@
 # 링크와 스타트
-# 복습 횟수:0, 01:45:00, 복습필요O
+# 복습 횟수:1, 01:00:00, 복습필요X
 import sys
-from itertools import combinations
-si = sys.stdin.readline
+from itertools import combinations 
+si = sys.stdin.readline 
 N = int(si())
-answer = sys.maxsize
-graph = []
+score_graph = []
 for i in range(N):
     tmp = list(map(int, si().split()))
-    graph.append(tmp)
+    score_graph.append(tmp)
 
-team_list = [i for i in range(N)]
+# 1. home, away 팀원 수 
+number_of_teammate_case = []
+home, away = 2, N - 2
+while home <= away:
+    number_of_teammate_case.append([home, away])
+    home += 1
+    away -= 1
 
-for i in range(2, len(team_list)//2 + 1):
-    combi_list = list(combinations(team_list, i))
-    for combi in combi_list:
-        other_list = [x for x in team_list if x not in combi]
-        start = combi
-        link = other_list
-        # 점수 구하기
-        start_sum = 0
-        link_sum = 0
-        start_pair = list(combinations(start, 2))
-        link_pair = list(combinations(link, 2))
-        # start 점수 
-        # print("start_pair",start_pair)
-        for x, y in start_pair:
-            start_sum += graph[x][y]
-            start_sum += graph[y][x]
-        # link 점수
-        for x, y in link_pair:
-            link_sum += graph[x][y]
-            link_sum += graph[y][x]
-        # 차이
-        diff = abs(start_sum - link_sum)
-        # print(start, link, "start:", start_sum, "link", link_sum, "diff", diff)
-        answer = min(answer, diff)
+def dfs(team_list: list, case_list: list, depth, index, tmp: list):
+    if len(tmp) == 2:
+        case_list.append(tmp[:])
+        return
+    
+    for i in range(index, len(team_list)):
+        if team_list[i] not in tmp:
+            tmp.append(team_list[i])
+            dfs(team_list, case_list, depth + 1, i + 1, tmp)
+            tmp.pop()
+
+    return
+
+answer = sys.maxsize
+
+# home, away 가능한 case 구하기
+for home_number, away_number in number_of_teammate_case:
+    home_team_list = list(combinations(range(N), home_number))
+    
+    for home_team in home_team_list:
+        home_team = list(home_team)
+        away_team = [i for i in range(N) if not i in home_team]
+        
+        home_team_total_score = 0
+        away_team_total_score = 0
+
+        home_case = []
+        dfs(home_team, home_case, 0, 0, [])
+        away_case = []
+        dfs(away_team, away_case, 0, 0, [])
+
+        for x, y in home_case:
+            home_team_total_score += score_graph[x][y]
+            home_team_total_score += score_graph[y][x]
+        
+        for x, y in away_case:
+            away_team_total_score += score_graph[x][y]
+            away_team_total_score += score_graph[y][x]
+
+        diff = abs(away_team_total_score - home_team_total_score)
+        answer = min(diff, answer)
+    
 print(answer)
