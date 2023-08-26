7313b72fea57c1312933456e21cb12a7e780ca5f
codekim97 <kimtaeu4616@gmail.com>
2023-08-26 11:25:11 +0900

    BJ6987, 복습 횟수:2, 01:30:00, 복습필요:2

--- language_PYTHON/백준/BJ6987.py
+++ language_PYTHON/백준/BJ6987.py
@@ -1,58 +1,74 @@
 # 월드컵
-# 복습 횟수:1, 00:30:00, 복습필요O
-from itertools import combinations
+# 복습 횟수:2, 01:30:00, 복습필요2
 import sys
-si = sys.stdin.readline
-answer = []
-def dfs(index):
+from itertools import combinations
+
+si = sys.stdin.readline 
+
+# A B C D E F
+country_list = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+def dfs(depth):
     global flag
-    if index == 15:
-        check = 0
-        for val in graph:
-            check += sum(val)
-        if check == 0:
+    if depth == 15:
+        if result_list == country_list:
             flag = 1
-
         return
-
-    me, you = match_list[index]
     
-    # me 승
-    if graph[me][0] > 0 and graph[you][2] > 0:
-        graph[me][0] -= 1
-        graph[you][2] -= 1
-        dfs(index + 1)
-        graph[me][0] += 1
-        graph[you][2] += 1
+    home_index, away_index = match_list[depth]
+    
+    home = result_list[home_index]
+    away = result_list[away_index]
 
-    # me 무
-    if graph[me][1] > 0 and graph[you][1] > 0:
-        graph[me][1] -= 1
-        graph[you][1] -= 1
-        dfs(index + 1)    
-        graph[me][1] += 1
-        graph[you][1] += 1
+    # 내가 이기는 경우
+    if home[0] >= 1 and away[2] >= 1:
+        home[0] -= 1
+        away[2] -= 1
+        dfs(depth + 1)
+        home[0] += 1
+        away[2] += 1
+
+    # 무승부
+    if home[1] >= 1 and away[1] >= 1: 
+        home[1] -= 1
+        away[1] -= 1
+        dfs(depth + 1)
+        home[1] += 1
+        away[1] += 1
         
-    # me 패
-    if graph[me][2] > 0 and graph[you][0] > 0:
-        graph[me][2] -= 1
-        graph[you][0] -= 1
-        dfs(index + 1)
-        graph[me][2] += 1
-        graph[you][0] += 1
-
+    # 내가 지는 경우
+    if home[2] >= 1 and away[0] >= 1:
+    
+        home[2] -= 1
+        away[0] -= 1
+        dfs(depth + 1)
+        home[2] += 1
+        away[0] += 1
+    
     return
+
+match_list = []
+
+visited = [0 for i in range(6)]
+def make_combi(idx, arr: list):
+    if len(arr) == 2:
+        match_list.append(arr[:])
+        return
+    
+    for i in range(idx + 1, 6):
+        if visited[i] == 0: 
+            arr.append(i)
+            visited[i] = 1 
+            make_combi(i, arr)
+            visited[i] = 0
+            arr.pop()
+
+
+
+make_combi(-1, [])
 
 for i in range(4):
     tmp = list(map(int, si().split()))
-    graph = [[tmp[3*j], tmp[3*j + 1], tmp[3*j + 2]] for j in range(6)]
-    
-    match_list = list(combinations(range(6), 2))
     flag = 0
+    result_list = [[tmp[3*j], tmp[3*j + 1], tmp[3*j + 2]] for j in range(6)]
     dfs(0)
-    if flag:
-        answer.append(1)
-    else:
-        answer.append(0)
-
-print(*answer)
+    print(flag, end = " ")
