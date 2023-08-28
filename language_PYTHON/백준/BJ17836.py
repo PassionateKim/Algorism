1076170a3bd5a28f9181e6d8f2f9b1cbf513c04c
codekim97 <kimtaeu4616@gmail.com>
2023-08-28 18:45:41 +0900

    BJ17836, 복습 횟수:1, 01:00:00, 복습필요3

--- language_PYTHON/백준/BJ17836.py
+++ language_PYTHON/백준/BJ17836.py
@@ -1,48 +1,62 @@
 # 공주님을 구해라.
-# 복습 횟수:0, 01:00:00, 복습필요O
+# 복습 횟수:1, 01:00:00, 복습필요3
+from collections import deque
+import math
 import sys
-from collections import deque   
-si = sys.stdin.readline
+si = sys.stdin.readline 
+N, M, T = map(int, si().split())
+vistited = [[[0, 0] for i in range(M)] for i in range(N)]
+graph = []
 
-N, M, K = map(int, si().split())
-graph = [list(map(int, si().split())) for _ in range(N)]
+for i in range(N):
+    tmp = list(map(int, si().split()))
+    graph.append(tmp)
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs( time):
-    q = deque()
-    visited = [[[0, 0] for i in range(M)] for j in range(N)]
+flag = 0
+def bfs():
+    global flag
 
+    q = deque()
+    # x, y, count, isGramGet
     q.append([0, 0, 0, 0])
-    visited[0][0][0] = 1 # 방문처리
     while q:
-        x, y, knife, time = q.popleft()
-        if x == N-1 and y == M-1:
-            if time <= K:
-                print(time)
+        x, y, count, isGramGet = q.popleft()
+
+        if x == N - 1 and y == M - 1:
+            if count <= T:
+                flag = 1
+                return count
             else:
-                print('Fail')
-            return
-        
+                return
+
         for idx in range(4):
             nx, ny = x + dx[idx], y + dy[idx]
-            if not (0 <= nx < N and 0 <= ny < M): continue
-            
-            if knife == 1:
-                if visited[nx][ny][1]: continue
+            if not ( 0 <= nx < N and 0 <= ny < M): continue
 
-                q.append([nx, ny, 1, time + 1])
-                visited[nx][ny][1] = 1 # 방문처리
+            if isGramGet:
+                if vistited[nx][ny][1] == 1: continue
+
+                q.append([nx, ny, count + 1, isGramGet])
+                vistited[nx][ny][1] = 1
             else:
-                if visited[nx][ny][0]: continue
+                if vistited[nx][ny][0] == 1: continue
                 if graph[nx][ny] == 1: continue
 
                 if graph[nx][ny] == 2:
-                    q.append([nx, ny, 1, time + 1])
-                else:
-                    q.append([nx, ny, 0, time + 1])
+                    q.append([nx, ny, count + 1, 1])
 
-                visited[nx][ny][0] = 1 # 방문처리
+                else:
+                    q.append([nx, ny, count + 1, 0])
     
-    print('Fail')
-bfs(0)
+                vistited[nx][ny][0] = 1
+                    
+    return
+
+candidate = bfs()
+if flag:
+    print(candidate)
+else:
+    print("Fail")
