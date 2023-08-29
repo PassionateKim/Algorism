26ed7934441f7be03866d57521c62e9b0cdba6cf
codekim97 <kimtaeu4616@gmail.com>
2023-08-29 11:15:37 +0900

    BJ11725, 복습횟수:2, 00:30:00, 복습필요X

--- language_PYTHON/백준/BJ11725.py
+++ language_PYTHON/백준/BJ11725.py
@@ -1,41 +1,34 @@
 # 트리의 부모 찾기
-# 복습 횟수:1, 00:30:00, 복습필요O
-import sys
+# 복습 횟수:2, 00:30:00, 복습필요X
 from collections import deque
-si = sys.stdin.readline
+import sys
+
+si = sys.stdin.readline 
 N = int(si())
 
+graph = [[] for i in range(N + 1)]
+parent = [0 for i in range(N + 1)]
 
-graph = [[] for i in range(N+1)]
-answer = [0] * (N+1)
-visited = [0] * (N+1)
-
-for i in range(N-1):
+for i in range(N - 1):
     x, y = map(int, si().split())
     graph[x].append(y)
     graph[y].append(x)
 
-
-                 
-def bfs(start):
+def bfs():
     q = deque()
-    visited[start] = 1 # 방문처리
-    q.append(start)
+    q.append(1)
+    parent[1] = 1
 
     while q:
-        idx = q.popleft()
-
-        for val in graph[idx]:
-            if visited[val]: continue
-
-            q.append(val)
-            answer[val] = idx
-            visited[val] = 1
+        start_index = q.popleft()
+        for end_index in graph[start_index]:
+            
+            if parent[end_index] == 0:
+                parent[end_index] = start_index
+                q.append(end_index)
 
     return
 
-bfs(1)
-
-for i in range(2, len(answer)):
-    print(answer[i])
-
+bfs()
+for i in range(2, len(parent)):
+    print(parent[i])
