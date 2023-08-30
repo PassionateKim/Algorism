b20eb1909816a0956f2d8f608c6dbf982d86a50f
codekim97 <kimtaeu4616@gmail.com>
2023-08-30 14:12:15 +0900

    BJ16973, 복습횟수:1, 01:00:00, 복습필요2

--- language_PYTHON/백준/BJ16973.py
+++ language_PYTHON/백준/BJ16973.py
@@ -1,69 +1,72 @@
 # 직사각형 탈출
-# 복습 횟수:0, 01:00:00, 복습필요O
-import sys
+# 복습 횟수:1, 01:00:00, 복습필요2
 from collections import deque
-si = sys.stdin.readline
-N, M = map(int, si().split())
+import sys
+si = sys.stdin.readline 
 
-graph = [list(map(int, si().split())) for _ in range(N)]
-h, w, sr, sc, fr, fc = map(int, si().split())
-sr, sc, fr, fc = sr - 1, sc - 1, fr - 1, fc - 1
-visited = [[0 for __ in range(M)] for _ in range(N)]
+N, M = map(int, si().split())
+graph = []
+
+for i in range(N):
+    tmp = list(map(int, si().split()))
+    graph.append(tmp)
+
+H, W, Sr, Sc, Fr, Fc = map(int ,si().split())
+# index 1 -> 0 
+
+Sr = Sr - 1
+Sc = Sc - 1
+Fr = Fr - 1
+Fc = Fc - 1
+
+visited = [[0 for i in range(M)] for i in range(N)]
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-wall = []
+
+wall_list = []
+
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 1:
-            wall.append([i, j])
+            wall_list.append([i, j])
 
-def isEdgeOk(nx, ny, h, w):
-    # # 가장 왼쪽 위
-    # if not (0 <= nx < N and 0 <= ny < M): return False
-    # # 가장 왼쪽 아래
-    # if not(0 <= nx+h-1 < N and 0 <= ny < M): return False
-    # # 가장 오른쪽 위
-    # if not (0 <= nx < N and 0 <= ny+w-1 < M): return False
-    # # 가장 오른쪽 아래
-    # if not (0 <= nx+h-1 < N and 0 <= ny+w-1 < M): return False
+def isEdgeOk(nx ,ny):
+    if not (0 <= nx < N and 0 <= ny < M): return False
+    if not (0 <= nx + H - 1 < N and 0 <= ny + W - 1 < M): return False
 
-    if 0 <= nx < N and 0 <= ny < M and 0 <= nx+h-1 < N and 0 <= ny+w-1 < M: return True
-
-    return False
-
-def isWallOk(nx, ny, h, w):
-    # for x in range(nx, nx + h):
-    #     for y in range(ny, ny + w):
-    #         if graph[x][y] == 1: return False
-    for x, y in wall:
-        if nx <= x < nx + h and ny <= y < ny + w:
-            return False
-    
     return True
 
-def bfs(x, y, cnt):
+def isWallOk(nx, ny):
+    for wall_x, wall_y in wall_list:
+        if (nx <= wall_x < nx + H) and (ny <= wall_y < ny + W): return False
+
+    return True
+
+def bfs():
     q = deque()
-    q.append([x, y, cnt])
-    visited[x][y] = 1  # 방문처리
+    # 왼쪽 상부 꼭지점 x, y 좌표
+    q.append([Sr, Sc, 0])
+    visited[Sr][Sc] = 1 # 방문 처리
 
     while q:
-        x, y, cnt = q.popleft()
-
-        if x == fr and y == fc:
-            return cnt
+        x, y, count = q.popleft()
         
+        if x == Fr and y == Fc:
+            print(count)
+            return
+
         for idx in range(4):
-            nx, ny = x + dx[idx], y + dy[idx]
-            
-            # 꼭지점 체크
-            if isEdgeOk(nx, ny, h, w):
-                # 직사각형 안에 1이 존재하는지 체크
-                if isWallOk(nx, ny, h, w):
-                    if visited[nx][ny] == 0: 
-                        q.append([nx, ny, cnt + 1])
-                        visited[nx][ny] = 1 # 방문처리
+            nx,  ny = x + dx[idx], y + dy[idx]
 
-    return -1 
+            if isEdgeOk(nx, ny) and isWallOk(nx, ny):
+                if visited[nx][ny] == 1: continue
 
-print(bfs(sr, sc, 0))
+                q.append([nx, ny, count + 1])
+                visited[nx][ny] = 1 # 방문 처리
+
+    print(-1)
+    return
+
+bfs()
