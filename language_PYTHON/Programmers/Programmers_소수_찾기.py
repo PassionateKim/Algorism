d5c696848ef5bf44b8f7e679b1b51192f824cf27
codekim97 <kimtaeu4616@gmail.com>
2023-08-30 12:55:14 +0900

    Programmers_소수_찾기, 복습횟수:5, 00:45:00, 복습필요X

--- language_PYTHON/Programmers/Programmers_소수_찾기.py
+++ language_PYTHON/Programmers/Programmers_소수_찾기.py
@@ -1,54 +1,39 @@
 # 소수 찾기
-# 복습 횟수:4, 00:45:00, 복습필요O
-from copy import deepcopy
-def dfs(numbers, li : list, cnt, number_list: set):
-    # 탈출조건
-    if len(li) == cnt:
-        tmp = deepcopy(li)
-        number_list.add(tuple(tmp))
-        return
+# 복습 횟수:5, 00:45:00, 복습필요X
 
-    for i in range(len(numbers)):
-        if visited[i] == 1: continue
-
-        li.append(numbers[i])
-        visited[i] = 1 # 방문처리
-        dfs(numbers, li, cnt, number_list)
-        li.pop()
-        visited[i] = 0 # 원상 복구
-
-    return
-
-def isPrime(val):
-    if val < 2:
+import math
+from itertools import permutations
+def isPrime(input):
+    if input == 0 or input == 1:
         return False
+        
+    if input == 2:
+        return True
     
-    for i in range(2, val):
-        if val % i == 0:
+    for i in range(2, int(math.sqrt(input)) + 1):
+        if input % i == 0:
             return False
-    
+
     return True
 
+def solution(number_string):
+    answer = set()
 
-def solution(numbers):
-    global visited
-    
-    number_list = set()
-    numbers = list(map(str, numbers))
-
-    for i in range(1, len(numbers)+1):
-        visited = [0 for _ in range(len(numbers))]
-        dfs(numbers, [], i,  number_list)
-    
-    # 구하기
-    result = set()
-    for number in number_list:
-        number = int("".join(number))
+    for i in range(1, len(number_string) + 1):
+        permutation_list = list(permutations(range(len(number_string)), i))
         
-        
-        if isPrime(number):
-            result.add(number)
-    
-    return len(result)
+        for case in permutation_list:
+            tmp = ""
+            for index in case:
+                tmp = tmp + number_string[index]
+            
 
-print(solution("17"))
+            tmp_int = int(tmp)
+
+            if isPrime(tmp_int):
+                answer.add(tmp_int)
+
+    return len(answer)
+
+
+solution("17")
