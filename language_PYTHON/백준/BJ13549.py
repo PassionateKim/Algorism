703fbae87840204f253876c13c3c83b34895ee59
codekim97 <kimtaeu4616@gmail.com>
2023-08-30 11:19:32 +0900

    BJ13549, 복습횟수:2, 00:30:00, 복습필요2

--- language_PYTHON/백준/BJ13549.py
+++ language_PYTHON/백준/BJ13549.py
@@ -1,37 +1,35 @@
 # 숨바꼭질 3
-# 복습 횟수:0, 01:30:00, 복습필요O
+# 복습 횟수:2, 00:30:00, 복습필요2
 
-# =====오답코드=====#
-import sys
 from collections import deque
-si = sys.stdin.readline
-N, K = map(int, si().split())
-visited = [sys.maxsize for _ in range(10**5 + 1)]
+import sys
+
+si = sys.stdin.readline 
+N, M = map(int, si().split())
+
+visited = [0 for i in range(100000 + 1)]
 
 def bfs():
     q = deque()
     q.append([N, 0])
-    visited[N] = 1
+    visited[N] = 1  # 방문 처리
 
     while q:
-        current, cnt = q.popleft()
+        x, count = q.popleft()
+        if x == M:
+            print(count)
+            break
         
-        if current == K:
-            print(cnt)
-            return
+        if 0 <= 2 * x <= 100000 and visited[2 * x] == 0:
+            q.appendleft([2 * x , count])
+            visited[2*x] = 1
+        if 0 <= x - 1 <= 100000 and visited[x-1] == 0:
+            q.append([x - 1, count + 1])
+            visited[x-1] = 1
+        if 0 <= x + 1 <= 100000 and visited[x+1] == 0:
+            q.append([x + 1, count + 1])
+            visited[x+1] = 1
 
-        if 0 < current * 2 <= 10 ** 5 and visited[current * 2] == 0:
-            q.appendleft([current * 2, cnt])
-            visited[current * 2] = 1 # 방문처리
-
-        if 0 < current + 1 <= 10 ** 5 and visited[current + 1] == 0:
-            q.append([current + 1, cnt + 1])
-            visited[current + 1] = 1 # 방문처리
-
-        if 0 < current - 1 <= 10 ** 5 and visited[current - 1] == 0:
-            q.append([current- 1, cnt + 1])
-            visited[current - 1] = 1 # 방문처리 
-        
     return
 
 bfs()
