6b140a94d1c3ec6b801121bf185134cd6bdd2467
codekim97 <kimtaeu4616@gmail.com>
2023-08-31 16:40:36 +0900

    BJ14940, 복습 횟수:1, 00:30:00, 복습필요X

--- language_PYTHON/백준/BJ14940.py
+++ language_PYTHON/백준/BJ14940.py
@@ -1,101 +1,51 @@
 # 쉬운 최단 거리
-# 복습 횟수:0, 00:45:00, 복습필요O
+# 복습 횟수:1, 00:30:00, 복습필요X
 import sys
 from collections import deque
-si = sys.stdin.readline
 
+si = sys.stdin.readline 
 N, M = map(int, si().split())
 
-graph = [list(map(int, si().split())) for _ in range(N)]
-answer = [[0 for _ in range(M)] for _ in range(N)]
-visited = [[0 for _ in range(M)] for _ in range(N)]
 
+graph = []
+for i in range(N):
+    tmp = list(map(int, si().split()))
+    graph.append(tmp)
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 2:
+            destination = [i, j]
+
+visited = [[0 for i in range(M)] for i in range(N)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs():
+    q= deque()
+    q.append([destination[0], destination[1], 0])
 
-    q = deque()
-    q.append([n, m, 0])
-    visited[n][m] = 1 # 방문처리
-    answer[n][m] = 0
-    
     while q:
-        x, y, cnt = q.popleft()
-        
+        x, y, distance = q.popleft()
         for idx in range(4):
             nx, ny = x + dx[idx], y + dy[idx]
             if not (0 <= nx < N and 0 <= ny < M): continue
-            if visited[nx][ny] == 1: continue
+            if visited[nx][ny] != 0: continue
+            if nx == destination[0] and ny == destination[1]: continue
             if graph[nx][ny] == 0: continue
 
-            q.append([nx, ny, cnt + 1])
-            answer[nx][ny] = cnt + 1
-            visited[nx][ny] = 1 # 방문처리
+            q.append([nx, ny, distance + 1])
+            visited[nx][ny] = distance + 1
 
-    return 
-
-# 첫 위치 체크하기
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 2:
-            n, m = i, j
-
-        if graph[i][j] == 0:
-            visited[i][j] = 1 # 방문처리 
-
+    return
 bfs()
+
 for i in range(N):
     for j in range(M):
-        if visited[i][j] == 0:
-            answer[i][j] = -1
+        if i == destination[0] and j == destination[1]: continue
+        # 갈 수 있는데 도달하지 못하는 경우
+        if graph[i][j] == 1 and visited[i][j] == 0:
+            visited[i][j] = -1
 
-for i in answer:
+for i in visited:
     print(*i)
-
-# from collections import deque
-# n, m = map(int, input().split())
-
-# graph = []
-# for _ in range(n):
-#     graph.append(list(map(int, input().split())))
-    
-# visited = [[False for _ in range(m)] for _ in range(n)]
-
-# for i in range(n):
-#     for j in range(m):
-#         if graph[i][j] == 2:
-#             fx, fy = i, j
-#             graph[fx][fy] = 0
-#         if graph[i][j] == 0:
-#             visited[i][j] = True
-
-# def bfs(x, y, cnt):
-#     queue = deque([(x, y, cnt)])
-#     visited[x][y] = True
-
-#     dx = [1, -1, 0, 0]
-#     dy = [0, 0, 1, -1]
-
-#     while queue:
-#         x, y, cnt = queue.popleft()
-#         for i in range(4):
-#             nx = dx[i] + x
-#             ny = dy[i] + y
-
-#             if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-#                 if graph[nx][ny] == 1:
-#                     queue.append((nx, ny, cnt + 1))
-#                     graph[nx][ny] = cnt + 1
-#                     visited[nx][ny] = True
-#     return
-
-# bfs(fx, fy, 0)
-
-# for i in range(n):
-#     for j in range(m):
-#         if visited[i][j] == False:
-#             graph[i][j] = -1
-
-# for i in range(n):
-#     print(*graph[i])
