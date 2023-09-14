27a04ddcb7af901b953d6a2f2ca604ab118a42ab
codekim97 <kimtaeu4616@gmail.com>
2023-09-14 15:28:42 +0900

    BJ15649, 복습횟수:2, 00:30:00, 복습필요1

--- language_PYTHON/백준/BJ15649.py
+++ language_PYTHON/백준/BJ15649.py
@@ -1,26 +1,19 @@
 # N과 M (1)
 import sys
-si = sys.stdin.readline
+si = sys.stdin.readline 
 N, M = map(int, si().split())
-visited = [0 for i in range(N+1)]
-answer = []
 
-
-def dfs(cnt):
-    
-    if cnt == M:
-        print(*answer)
+def dfs(depth, answer_list: list):
+    if depth == M:
+        print(*answer_list)
         return
-
-    for val in range(1, N+1):
-        if visited[val] == 0: # 방문하지 않은 경우만
-            answer.append(val)
-            visited[val] = 1 # 방문처리
-            dfs(cnt + 1)
-
-            answer.pop() # 초기화
-            visited[val] = 0 # 초기화
     
+    for i in range(1, N + 1):
+        if i not in answer_list:
+            answer_list.append(i)
+            dfs(depth + 1, answer_list)
+            answer_list.pop()
+
     return
 
-dfs(0)
+dfs(0, [])
