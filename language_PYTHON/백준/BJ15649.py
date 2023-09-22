fc615505621e284f8f741e5fabf4c39983233f19
codekim97 <kimtaeu4616@gmail.com>
2023-09-22 16:58:52 +0900

    BJ15649, 복습횟수:3, 00:30:00, 복습필요X

--- language_PYTHON/백준/BJ15649.py
+++ language_PYTHON/백준/BJ15649.py
@@ -1,18 +1,20 @@
 # N과 M (1)
+# 복습횟수:3, 00:30:00, 복습필요1
 import sys
 si = sys.stdin.readline 
 N, M = map(int, si().split())
 
-def dfs(depth, answer_list: list):
+
+def dfs(depth, candidate_list: list):
     if depth == M:
-        print(*answer_list)
+        print(*candidate_list)
         return
     
-    for i in range(1, N + 1):
-        if i not in answer_list:
-            answer_list.append(i)
-            dfs(depth + 1, answer_list)
-            answer_list.pop()
+    for i in range(1, N+1):
+        if i not in candidate_list:
+            candidate_list.append(i)
+            dfs(depth + 1, candidate_list)
+            candidate_list.pop()
 
     return
 
