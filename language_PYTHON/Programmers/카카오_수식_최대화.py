b923f60a846a4195b1ffd7266084f1c3414414c9
codekim97 <kimtaeu4616@gmail.com>
2023-10-31 19:08:33 +0900

    복습횟수:4, 02:00:00, 복습필요O

--- language_PYTHON/Programmers/카카오_수식_최대화.py
+++ language_PYTHON/Programmers/카카오_수식_최대화.py
@@ -1,39 +1,54 @@
 # 2022-08-03
 # 2022-08-05
 # 2022-08-11
+# 2023-10-31
 # 카카오 수식 최대화
-from collections import deque
+
 from itertools import permutations
 import re
+
 def solution(expression):
     answer = 0
     # 1. 나누기
-    expression = re.split(r'(\+|\-|\*)', expression)
-    expression = deque(expression)
-    tmp_expression = deque()
-    # 2.우선순위 결정해 계산하기
-    for p in permutations(["*","-","+"], 3):
-        tmp_expression.extend(expression)
-        for op in p: # *
-            print("op", op)
-            new_expression = deque()
-            while tmp_expression:
-                tmp = tmp_expression.popleft()
-                if tmp != op:
-                    new_expression.append(tmp)
-                else:
-                    left = new_expression.pop()
-                    right = tmp_expression.popleft()
+    expression = re.split(r'(\+|\*|\-)', expression)
 
-                    print("left: ",left,"right: ",right)
-                
-                    new_expression.append(str(eval(left+op+right)))
+    operation_list = list(permutations(["*", "-", "+"])) 
+    
+    for operations in operation_list:
+        tmp_expression_list = expression[:]
+        
+        # - + * 등으로 순서 체크 하면서 바꾸기
+        for oper in operations:
+            combine_list = []
+            for index, val in enumerate(tmp_expression_list):
+
+                if(val == oper):
+                    combine_list.append(index)
+            
+            
             # 초기화
-            tmp_expression.extend(new_expression) 
-        #/ 우선순위 계산 끝
-        answer = max(answer, abs(int(tmp_expression.pop())))
-        print(answer)
+            new_expression_list = []
+            
+            cursor = 0
+            while cursor < len(tmp_expression_list):
+                if cursor in combine_list:
+                    left = new_expression_list.pop()                     
+                    right = tmp_expression_list[cursor + 1]
+
+                    new_val = str(eval(left + oper + right))
+                    
+                    new_expression_list.append(str(new_val))
+                    cursor += 2
+                else:
+                    new_expression_list.append(tmp_expression_list[cursor])
+                    cursor += 1
+
+            tmp_expression_list = new_expression_list[:]
+
+        answer = max(answer, abs(int(tmp_expression_list[0])))
+
     
     return answer
 
-solution("100-200*300-500+20")
+
+print(solution("100-200*300-500+20"))
