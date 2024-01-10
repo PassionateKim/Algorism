32c7e67d4d8a15aa9987e1203653194daf8ca126
codekim97 <kimtaeu4616@gmail.com>
2024-01-10 14:02:33 +0900

    Programmers 피보나치수, 복습횟수:0, 00:30:00, 복습필요X

---
+++ language_PYTHON/Programmers/Programmers_피보나치_수.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+
+    dp = [0 for i in range(n+1)]
+    dp[1] = 1
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        dp[i] = dp[i] % 1234567
+
+    answer = dp[n]
+    
+    return answer
+
+print(solution(33))
+
