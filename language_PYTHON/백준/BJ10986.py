8a78b6ec40ccfd0097f092383d3f4ade49ccd2de
codekim97 <kimtaeu4616@gmail.com>
2024-03-27 11:30:17 +0900

    BJ10986, 복습횟수:0, 01:00:00, 복습필요X

---
+++ language_PYTHON/백준/BJ10986.py
@@ -0,0 +1,20 @@
+import sys
+si = sys.stdin.readline 
+N, M = map(int, si().split())
+
+arr = list(map(int, si().split()))
+
+prefix_sum = [0 for i in range(N + 1)]
+
+for i in range(1, N + 1):
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1]
+
+my_dict = dict()
+answer = 0
+
+for i in range(N + 1):
+    answer = answer + my_dict.get(prefix_sum[i] % M, 0) 
+
+    my_dict[prefix_sum[i] % M] = my_dict.get(prefix_sum[i] % M, 0) + 1
+
+print(answer)
