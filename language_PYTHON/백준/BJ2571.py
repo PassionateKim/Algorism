77692e69ecd759c3e3b314bd22f80d8709490e46
codekim97 <kimtaeu4616@gmail.com>
2024-04-02 11:11:41 +0900

    BJ2571, 복습횟수:1, 00:30:00, 복습필요O

--- language_PYTHON/백준/BJ2571.py
+++ language_PYTHON/백준/BJ2571.py
@@ -1,33 +1,27 @@
-import sys 
+import sys
 si = sys.stdin.readline 
-
 N = int(si())
-color_paper_list = []
+
+graph = [[0 for i in range(101)] for i in range(101)]
 
 for i in range(N):
-    color_paper_list.append(list(map(int, si().split())))
+    x, y = map(int, si().split())
 
-graph = [[0 for i in range(101)] for i in range(101)] # 이따가 101으로 바꿔야함
+    for z in range(x, x + 10):
+        for k in range(y, y + 10):
+            graph[z][k] = 1 # 색종이가 있는 부분
 
-for i in range(len(graph)):
-    for j in range(len(graph)):
-
-        for x, y in color_paper_list:
-            if (x <= i <= x + 10 - 1) and (y <= j <= y + 10 - 1):
-                graph[i][j] = 1 
-                break
-
-for i in range(len(graph) - 1):
-    for j in range(len(graph)):
-        if graph[i][j] != 0 and graph[i+1][j] != 0: 
+for i in range(100):
+    for j in range(101):
+        if graph[i][j] != 0 and graph[i+1][j] != 0:
             graph[i+1][j] = graph[i][j] + 1
 
 answer = 0
-for i in range(len(graph)):
-    for j in range(len(graph)):
+for i in range(101):
+    for j in range(101):
         h = 100
-        for k in range(j, len(graph)):
-            h = min(graph[i][k], h)
+        for k in range(j, 101):
+            h = min(h, graph[i][k])
 
             if h == 0:
                 break
