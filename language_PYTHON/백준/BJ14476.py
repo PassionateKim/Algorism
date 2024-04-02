8847e11c32a703d014630e51292a83479b2ddfe9
codekim97 <kimtaeu4616@gmail.com>
2024-04-02 17:13:26 +0900

    BJ14476, 복습횟수:0, 01:30:00, 복습필요O

---
+++ language_PYTHON/백준/BJ14476.py
@@ -0,0 +1,53 @@
+import sys
+si = sys.stdin.readline 
+N = int(si())
+arr = list(map(int, si().split()))
+
+answer = -1
+answer2 = -1
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+
+    return x
+
+left_prefix_list = [arr[0]]
+right_prefix_list = [arr[-1]]
+
+# left prefix
+left = left_prefix_list[0]
+for i in range(1, N):
+    left = gcd(left, arr[i])
+    left_prefix_list.append(left)
+
+# right prefix
+right = right_prefix_list[0]
+for i in range(N-2, -1, -1):
+    right = gcd(right, arr[i])    
+    right_prefix_list.append(right)
+
+right_prefix_list.reverse()
+
+for index in range(N):
+    if index == 0:
+        val = right_prefix_list[index + 1]
+        # 이때, 최대공약수는 K의 약수가 되면 안 된다.
+        
+    if 0 < index < N - 1:
+        left_val = left_prefix_list[index - 1]
+        right_val = right_prefix_list[index + 1]
+        val = gcd(left_val, right_val)
+
+    if index == N-1:
+        val = left_prefix_list[index - 1]
+    
+    if arr[index] > val or (val <= arr[index] and val % arr[index] == 0): continue
+    
+    if answer < val:
+        answer = val
+        answer2 = arr[index]
+
+if answer == -1:
+    print(-1)
+else:
+    print(answer, answer2)
