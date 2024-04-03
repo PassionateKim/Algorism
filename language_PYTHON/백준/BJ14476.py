0731bfea1fee4ab873a013eecebaafcce8d7c2a8
codekim97 <kimtaeu4616@gmail.com>
2024-04-03 22:52:36 +0900

    BJ14476, 복습횟수:1, 01:00:00, 복습필요O

--- language_PYTHON/백준/BJ14476.py
+++ language_PYTHON/백준/BJ14476.py
@@ -1,3 +1,4 @@
+import math
 import sys
 si = sys.stdin.readline 
 N = int(si())
@@ -5,25 +6,26 @@ arr = list(map(int, si().split()))
 
 answer = -1
 answer2 = -1
-def gcd(x, y):
-    while y:
-        x, y = y, x % y
+# def gcd(x, y):
+#     while y:
+#         x, y = y, x % y
 
-    return x
+#     return x
 
 left_prefix_list = [arr[0]]
 right_prefix_list = [arr[-1]]
 
+
 # left prefix
 left = left_prefix_list[0]
 for i in range(1, N):
-    left = gcd(left, arr[i])
+    left = math.gcd(left, arr[i])
     left_prefix_list.append(left)
 
 # right prefix
 right = right_prefix_list[0]
 for i in range(N-2, -1, -1):
-    right = gcd(right, arr[i])    
+    right = math.gcd(right, arr[i])    
     right_prefix_list.append(right)
 
 right_prefix_list.reverse()
@@ -36,13 +38,13 @@ for index in range(N):
     if 0 < index < N - 1:
         left_val = left_prefix_list[index - 1]
         right_val = right_prefix_list[index + 1]
-        val = gcd(left_val, right_val)
+        val = math.gcd(left_val, right_val)
 
     if index == N-1:
         val = left_prefix_list[index - 1]
     
-    if arr[index] > val or (val <= arr[index] and val % arr[index] == 0): continue
-    
+    if (arr[index] % val == 0) : continue 
+
     if answer < val:
         answer = val
         answer2 = arr[index]
