84485186352a5ada07026f1b49e8bd8e57a7ebb6
codekim97 <kimtaeu4616@gmail.com>
2024-05-09 21:24:15 +0900

    Programmers 스킬트리 복습횟수:1, 00:30:00, 복습필요X

--- language_PYTHON/Programmers/써머윈터인턴_스킬트리.py
+++ language_PYTHON/Programmers/써머윈터인턴_스킬트리.py
@@ -1,30 +1,25 @@
 # 2022-09-25
+# 2024-05-09
 # 스킬 트리
 def solution(skill, skill_trees):
     answer = 0
-    skill_list = list(skill)
-    tmp_list = []
-    # 완전탐색 순서가 있어야하므로 list
+    skill_sequence = skill
+    
     for skill in skill_trees:
-        skill = list(skill)
-        tmp = []
-        # skill_list에 있는 원소만 추출하기
-        for s in skill:
-            if s in skill_list:
-                tmp.append(s)
-        tmp_list.append(tmp)
-    
-    for tmp in tmp_list:
-        flag = 1
-        for i in range(len(tmp)):
-            if tmp[i] != skill_list[i]: 
-                flag = 0
-        if flag:
-            answer += 1
-    print(answer)
-         
+        cursor = 0
+        is_true = True
+        for val in skill:
 
-    
+            if val not in skill_sequence:
+                continue
+            if val == skill_sequence[cursor]:
+                cursor += 1
+            else:
+                is_true = False
+                break
+        
+        if is_true:
+            answer += 1
 
     return answer
 
